bccf38a47c47e4612969fb5f5ac7c771ae9c0b67
sdk-team <sdk-team@alibabacloud.com>
2021-03-08 15:45:57 +0000

    Generated cpp 2020-03-20 for videoseg.

--- videoseg-20200320/include/alibabacloud/videoseg_20200320.hpp
+++ videoseg-20200320/include/alibabacloud/videoseg_20200320.hpp
@@ -198,6 +198,203 @@ public:
 
   virtual ~GetAsyncJobResultResponse() = default;
 };
+class SegmentGreenScreenVideoRequest : public Darabonba::Model {
+public:
+  shared_ptr<string> videoURL{};
+  shared_ptr<bool> async{};
+
+  SegmentGreenScreenVideoRequest() {}
+
+  explicit SegmentGreenScreenVideoRequest(const std::map<string, boost::any> &config) : Darabonba::Model(config) {
+    fromMap(config);
+  };
+
+  void validate() override {}
+
+  map<string, boost::any> toMap() override {
+    map<string, boost::any> res;
+    if (videoURL) {
+      res["VideoURL"] = boost::any(*videoURL);
+    }
+    if (async) {
+      res["Async"] = boost::any(*async);
+    }
+    return res;
+  }
+
+  void fromMap(map<string, boost::any> m) override {
+    if (m.find("VideoURL") != m.end() && !m["VideoURL"].empty()) {
+      videoURL = make_shared<string>(boost::any_cast<string>(m["VideoURL"]));
+    }
+    if (m.find("Async") != m.end() && !m["Async"].empty()) {
+      async = make_shared<bool>(boost::any_cast<bool>(m["Async"]));
+    }
+  }
+
+
+  virtual ~SegmentGreenScreenVideoRequest() = default;
+};
+class SegmentGreenScreenVideoAdvanceRequest : public Darabonba::Model {
+public:
+  shared_ptr<Darabonba::Stream> videoURLObject{};
+  shared_ptr<bool> async{};
+
+  SegmentGreenScreenVideoAdvanceRequest() {}
+
+  explicit SegmentGreenScreenVideoAdvanceRequest(const std::map<string, boost::any> &config) : Darabonba::Model(config) {
+    fromMap(config);
+  };
+
+  void validate() override {
+    if (!videoURLObject) {
+      BOOST_THROW_EXCEPTION(boost::enable_error_info(std::runtime_error("videoURLObject is required.")));
+    }
+  }
+
+  map<string, boost::any> toMap() override {
+    map<string, boost::any> res;
+    if (videoURLObject) {
+      res["VideoURLObject"] = boost::any(*videoURLObject);
+    }
+    if (async) {
+      res["Async"] = boost::any(*async);
+    }
+    return res;
+  }
+
+  void fromMap(map<string, boost::any> m) override {
+    if (m.find("VideoURLObject") != m.end() && !m["VideoURLObject"].empty()) {
+      videoURLObject = make_shared<Darabonba::Stream>(boost::any_cast<Darabonba::Stream>(m["VideoURLObject"]));
+    }
+    if (m.find("Async") != m.end() && !m["Async"].empty()) {
+      async = make_shared<bool>(boost::any_cast<bool>(m["Async"]));
+    }
+  }
+
+
+  virtual ~SegmentGreenScreenVideoAdvanceRequest() = default;
+};
+class SegmentGreenScreenVideoResponseBodyData : public Darabonba::Model {
+public:
+  shared_ptr<string> videoURL{};
+
+  SegmentGreenScreenVideoResponseBodyData() {}
+
+  explicit SegmentGreenScreenVideoResponseBodyData(const std::map<string, boost::any> &config) : Darabonba::Model(config) {
+    fromMap(config);
+  };
+
+  void validate() override {}
+
+  map<string, boost::any> toMap() override {
+    map<string, boost::any> res;
+    if (videoURL) {
+      res["VideoURL"] = boost::any(*videoURL);
+    }
+    return res;
+  }
+
+  void fromMap(map<string, boost::any> m) override {
+    if (m.find("VideoURL") != m.end() && !m["VideoURL"].empty()) {
+      videoURL = make_shared<string>(boost::any_cast<string>(m["VideoURL"]));
+    }
+  }
+
+
+  virtual ~SegmentGreenScreenVideoResponseBodyData() = default;
+};
+class SegmentGreenScreenVideoResponseBody : public Darabonba::Model {
+public:
+  shared_ptr<string> requestId{};
+  shared_ptr<SegmentGreenScreenVideoResponseBodyData> data{};
+
+  SegmentGreenScreenVideoResponseBody() {}
+
+  explicit SegmentGreenScreenVideoResponseBody(const std::map<string, boost::any> &config) : Darabonba::Model(config) {
+    fromMap(config);
+  };
+
+  void validate() override {}
+
+  map<string, boost::any> toMap() override {
+    map<string, boost::any> res;
+    if (requestId) {
+      res["RequestId"] = boost::any(*requestId);
+    }
+    if (data) {
+      res["Data"] = data ? boost::any(data->toMap()) : boost::any(map<string,boost::any>({}));
+    }
+    return res;
+  }
+
+  void fromMap(map<string, boost::any> m) override {
+    if (m.find("RequestId") != m.end() && !m["RequestId"].empty()) {
+      requestId = make_shared<string>(boost::any_cast<string>(m["RequestId"]));
+    }
+    if (m.find("Data") != m.end() && !m["Data"].empty()) {
+      if (typeid(map<string, boost::any>) == m["Data"].type()) {
+        SegmentGreenScreenVideoResponseBodyData model1;
+        model1.fromMap(boost::any_cast<map<string, boost::any>>(m["Data"]));
+        data = make_shared<SegmentGreenScreenVideoResponseBodyData>(model1);
+      }
+    }
+  }
+
+
+  virtual ~SegmentGreenScreenVideoResponseBody() = default;
+};
+class SegmentGreenScreenVideoResponse : public Darabonba::Model {
+public:
+  shared_ptr<map<string, string>> headers{};
+  shared_ptr<SegmentGreenScreenVideoResponseBody> body{};
+
+  SegmentGreenScreenVideoResponse() {}
+
+  explicit SegmentGreenScreenVideoResponse(const std::map<string, boost::any> &config) : Darabonba::Model(config) {
+    fromMap(config);
+  };
+
+  void validate() override {
+    if (!headers) {
+      BOOST_THROW_EXCEPTION(boost::enable_error_info(std::runtime_error("headers is required.")));
+    }
+    if (!body) {
+      BOOST_THROW_EXCEPTION(boost::enable_error_info(std::runtime_error("body is required.")));
+    }
+  }
+
+  map<string, boost::any> toMap() override {
+    map<string, boost::any> res;
+    if (headers) {
+      res["headers"] = boost::any(*headers);
+    }
+    if (body) {
+      res["body"] = body ? boost::any(body->toMap()) : boost::any(map<string,boost::any>({}));
+    }
+    return res;
+  }
+
+  void fromMap(map<string, boost::any> m) override {
+    if (m.find("headers") != m.end() && !m["headers"].empty()) {
+      map<string, string> map1 = boost::any_cast<map<string, string>>(m["headers"]);
+      map<string, string> toMap1;
+      for (auto item:map1) {
+         toMap1[item.first] = item.second;
+      }
+      headers = make_shared<map<string, string>>(toMap1);
+    }
+    if (m.find("body") != m.end() && !m["body"].empty()) {
+      if (typeid(map<string, boost::any>) == m["body"].type()) {
+        SegmentGreenScreenVideoResponseBody model1;
+        model1.fromMap(boost::any_cast<map<string, boost::any>>(m["body"]));
+        body = make_shared<SegmentGreenScreenVideoResponseBody>(model1);
+      }
+    }
+  }
+
+
+  virtual ~SegmentGreenScreenVideoResponse() = default;
+};
 class SegmentHalfBodyRequest : public Darabonba::Model {
 public:
   shared_ptr<string> videoUrl{};
@@ -592,203 +789,6 @@ public:
 
   virtual ~SegmentVideoBodyResponse() = default;
 };
-class SegmentGreenScreenVideoRequest : public Darabonba::Model {
-public:
-  shared_ptr<string> videoURL{};
-  shared_ptr<bool> async{};
-
-  SegmentGreenScreenVideoRequest() {}
-
-  explicit SegmentGreenScreenVideoRequest(const std::map<string, boost::any> &config) : Darabonba::Model(config) {
-    fromMap(config);
-  };
-
-  void validate() override {}
-
-  map<string, boost::any> toMap() override {
-    map<string, boost::any> res;
-    if (videoURL) {
-      res["VideoURL"] = boost::any(*videoURL);
-    }
-    if (async) {
-      res["Async"] = boost::any(*async);
-    }
-    return res;
-  }
-
-  void fromMap(map<string, boost::any> m) override {
-    if (m.find("VideoURL") != m.end() && !m["VideoURL"].empty()) {
-      videoURL = make_shared<string>(boost::any_cast<string>(m["VideoURL"]));
-    }
-    if (m.find("Async") != m.end() && !m["Async"].empty()) {
-      async = make_shared<bool>(boost::any_cast<bool>(m["Async"]));
-    }
-  }
-
-
-  virtual ~SegmentGreenScreenVideoRequest() = default;
-};
-class SegmentGreenScreenVideoAdvanceRequest : public Darabonba::Model {
-public:
-  shared_ptr<Darabonba::Stream> videoURLObject{};
-  shared_ptr<bool> async{};
-
-  SegmentGreenScreenVideoAdvanceRequest() {}
-
-  explicit SegmentGreenScreenVideoAdvanceRequest(const std::map<string, boost::any> &config) : Darabonba::Model(config) {
-    fromMap(config);
-  };
-
-  void validate() override {
-    if (!videoURLObject) {
-      BOOST_THROW_EXCEPTION(boost::enable_error_info(std::runtime_error("videoURLObject is required.")));
-    }
-  }
-
-  map<string, boost::any> toMap() override {
-    map<string, boost::any> res;
-    if (videoURLObject) {
-      res["VideoURLObject"] = boost::any(*videoURLObject);
-    }
-    if (async) {
-      res["Async"] = boost::any(*async);
-    }
-    return res;
-  }
-
-  void fromMap(map<string, boost::any> m) override {
-    if (m.find("VideoURLObject") != m.end() && !m["VideoURLObject"].empty()) {
-      videoURLObject = make_shared<Darabonba::Stream>(boost::any_cast<Darabonba::Stream>(m["VideoURLObject"]));
-    }
-    if (m.find("Async") != m.end() && !m["Async"].empty()) {
-      async = make_shared<bool>(boost::any_cast<bool>(m["Async"]));
-    }
-  }
-
-
-  virtual ~SegmentGreenScreenVideoAdvanceRequest() = default;
-};
-class SegmentGreenScreenVideoResponseBodyData : public Darabonba::Model {
-public:
-  shared_ptr<string> videoURL{};
-
-  SegmentGreenScreenVideoResponseBodyData() {}
-
-  explicit SegmentGreenScreenVideoResponseBodyData(const std::map<string, boost::any> &config) : Darabonba::Model(config) {
-    fromMap(config);
-  };
-
-  void validate() override {}
-
-  map<string, boost::any> toMap() override {
-    map<string, boost::any> res;
-    if (videoURL) {
-      res["VideoURL"] = boost::any(*videoURL);
-    }
-    return res;
-  }
-
-  void fromMap(map<string, boost::any> m) override {
-    if (m.find("VideoURL") != m.end() && !m["VideoURL"].empty()) {
-      videoURL = make_shared<string>(boost::any_cast<string>(m["VideoURL"]));
-    }
-  }
-
-
-  virtual ~SegmentGreenScreenVideoResponseBodyData() = default;
-};
-class SegmentGreenScreenVideoResponseBody : public Darabonba::Model {
-public:
-  shared_ptr<string> requestId{};
-  shared_ptr<SegmentGreenScreenVideoResponseBodyData> data{};
-
-  SegmentGreenScreenVideoResponseBody() {}
-
-  explicit SegmentGreenScreenVideoResponseBody(const std::map<string, boost::any> &config) : Darabonba::Model(config) {
-    fromMap(config);
-  };
-
-  void validate() override {}
-
-  map<string, boost::any> toMap() override {
-    map<string, boost::any> res;
-    if (requestId) {
-      res["RequestId"] = boost::any(*requestId);
-    }
-    if (data) {
-      res["Data"] = data ? boost::any(data->toMap()) : boost::any(map<string,boost::any>({}));
-    }
-    return res;
-  }
-
-  void fromMap(map<string, boost::any> m) override {
-    if (m.find("RequestId") != m.end() && !m["RequestId"].empty()) {
-      requestId = make_shared<string>(boost::any_cast<string>(m["RequestId"]));
-    }
-    if (m.find("Data") != m.end() && !m["Data"].empty()) {
-      if (typeid(map<string, boost::any>) == m["Data"].type()) {
-        SegmentGreenScreenVideoResponseBodyData model1;
-        model1.fromMap(boost::any_cast<map<string, boost::any>>(m["Data"]));
-        data = make_shared<SegmentGreenScreenVideoResponseBodyData>(model1);
-      }
-    }
-  }
-
-
-  virtual ~SegmentGreenScreenVideoResponseBody() = default;
-};
-class SegmentGreenScreenVideoResponse : public Darabonba::Model {
-public:
-  shared_ptr<map<string, string>> headers{};
-  shared_ptr<SegmentGreenScreenVideoResponseBody> body{};
-
-  SegmentGreenScreenVideoResponse() {}
-
-  explicit SegmentGreenScreenVideoResponse(const std::map<string, boost::any> &config) : Darabonba::Model(config) {
-    fromMap(config);
-  };
-
-  void validate() override {
-    if (!headers) {
-      BOOST_THROW_EXCEPTION(boost::enable_error_info(std::runtime_error("headers is required.")));
-    }
-    if (!body) {
-      BOOST_THROW_EXCEPTION(boost::enable_error_info(std::runtime_error("body is required.")));
-    }
-  }
-
-  map<string, boost::any> toMap() override {
-    map<string, boost::any> res;
-    if (headers) {
-      res["headers"] = boost::any(*headers);
-    }
-    if (body) {
-      res["body"] = body ? boost::any(body->toMap()) : boost::any(map<string,boost::any>({}));
-    }
-    return res;
-  }
-
-  void fromMap(map<string, boost::any> m) override {
-    if (m.find("headers") != m.end() && !m["headers"].empty()) {
-      map<string, string> map1 = boost::any_cast<map<string, string>>(m["headers"]);
-      map<string, string> toMap1;
-      for (auto item:map1) {
-         toMap1[item.first] = item.second;
-      }
-      headers = make_shared<map<string, string>>(toMap1);
-    }
-    if (m.find("body") != m.end() && !m["body"].empty()) {
-      if (typeid(map<string, boost::any>) == m["body"].type()) {
-        SegmentGreenScreenVideoResponseBody model1;
-        model1.fromMap(boost::any_cast<map<string, boost::any>>(m["body"]));
-        body = make_shared<SegmentGreenScreenVideoResponseBody>(model1);
-      }
-    }
-  }
-
-
-  virtual ~SegmentGreenScreenVideoResponse() = default;
-};
 class Client : Alibabacloud_OpenApi::Client {
 public:
   explicit Client(const shared_ptr<Alibabacloud_OpenApi::Config>& config);
@@ -801,15 +801,15 @@ public:
                      shared_ptr<string> endpoint);
   GetAsyncJobResultResponse getAsyncJobResultWithOptions(shared_ptr<GetAsyncJobResultRequest> request, shared_ptr<Darabonba_Util::RuntimeOptions> runtime);
   GetAsyncJobResultResponse getAsyncJobResult(shared_ptr<GetAsyncJobResultRequest> request);
+  SegmentGreenScreenVideoResponse segmentGreenScreenVideoWithOptions(shared_ptr<SegmentGreenScreenVideoRequest> request, shared_ptr<Darabonba_Util::RuntimeOptions> runtime);
+  SegmentGreenScreenVideoResponse segmentGreenScreenVideo(shared_ptr<SegmentGreenScreenVideoRequest> request);
+  SegmentGreenScreenVideoResponse segmentGreenScreenVideoAdvance(shared_ptr<SegmentGreenScreenVideoAdvanceRequest> request, shared_ptr<Darabonba_Util::RuntimeOptions> runtime);
   SegmentHalfBodyResponse segmentHalfBodyWithOptions(shared_ptr<SegmentHalfBodyRequest> request, shared_ptr<Darabonba_Util::RuntimeOptions> runtime);
   SegmentHalfBodyResponse segmentHalfBody(shared_ptr<SegmentHalfBodyRequest> request);
   SegmentHalfBodyResponse segmentHalfBodyAdvance(shared_ptr<SegmentHalfBodyAdvanceRequest> request, shared_ptr<Darabonba_Util::RuntimeOptions> runtime);
   SegmentVideoBodyResponse segmentVideoBodyWithOptions(shared_ptr<SegmentVideoBodyRequest> request, shared_ptr<Darabonba_Util::RuntimeOptions> runtime);
   SegmentVideoBodyResponse segmentVideoBody(shared_ptr<SegmentVideoBodyRequest> request);
   SegmentVideoBodyResponse segmentVideoBodyAdvance(shared_ptr<SegmentVideoBodyAdvanceRequest> request, shared_ptr<Darabonba_Util::RuntimeOptions> runtime);
-  SegmentGreenScreenVideoResponse segmentGreenScreenVideoWithOptions(shared_ptr<SegmentGreenScreenVideoRequest> request, shared_ptr<Darabonba_Util::RuntimeOptions> runtime);
-  SegmentGreenScreenVideoResponse segmentGreenScreenVideo(shared_ptr<SegmentGreenScreenVideoRequest> request);
-  SegmentGreenScreenVideoResponse segmentGreenScreenVideoAdvance(shared_ptr<SegmentGreenScreenVideoAdvanceRequest> request, shared_ptr<Darabonba_Util::RuntimeOptions> runtime);
 
   virtual ~Client() = default;
 };
