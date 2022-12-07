49dab4afafd8fb3e6fe1b0a9d5afd420a7c4506c
sdk-team <sdk-team@alibabacloud.com>
2022-12-07 09:33:53 +0000

    Update Endpoint .

--- alikafka-20190916/src/alikafka_20190916.cpp
+++ alikafka-20190916/src/alikafka_20190916.cpp
@@ -18,44 +18,6 @@ using namespace Alibabacloud_Alikafka20190916;
 
 Alibabacloud_Alikafka20190916::Client::Client(const shared_ptr<Alibabacloud_OpenApi::Config>& config) : Alibabacloud_OpenApi::Client(config) {
   _endpointRule = make_shared<string>("regional");
-  _endpointMap = make_shared<map<string, string>>(map<string, string>({
-    {"ap-northeast-2-pop", "alikafka.aliyuncs.com"},
-    {"ap-southeast-2", "alikafka.aliyuncs.com"},
-    {"cn-beijing-finance-1", "alikafka.aliyuncs.com"},
-    {"cn-beijing-finance-pop", "alikafka.aliyuncs.com"},
-    {"cn-beijing-gov-1", "alikafka.aliyuncs.com"},
-    {"cn-beijing-nu16-b01", "alikafka.aliyuncs.com"},
-    {"cn-edge-1", "alikafka.aliyuncs.com"},
-    {"cn-fujian", "alikafka.aliyuncs.com"},
-    {"cn-haidian-cm12-c01", "alikafka.aliyuncs.com"},
-    {"cn-hangzhou-bj-b01", "alikafka.aliyuncs.com"},
-    {"cn-hangzhou-internal-prod-1", "alikafka.aliyuncs.com"},
-    {"cn-hangzhou-internal-test-1", "alikafka.aliyuncs.com"},
-    {"cn-hangzhou-internal-test-2", "alikafka.aliyuncs.com"},
-    {"cn-hangzhou-internal-test-3", "alikafka.aliyuncs.com"},
-    {"cn-hangzhou-test-306", "alikafka.aliyuncs.com"},
-    {"cn-hongkong-finance-pop", "alikafka.aliyuncs.com"},
-    {"cn-huhehaote-nebula-1", "alikafka.aliyuncs.com"},
-    {"cn-qingdao-nebula", "alikafka.aliyuncs.com"},
-    {"cn-shanghai-et15-b01", "alikafka.aliyuncs.com"},
-    {"cn-shanghai-et2-b01", "alikafka.aliyuncs.com"},
-    {"cn-shanghai-inner", "alikafka.aliyuncs.com"},
-    {"cn-shanghai-internal-test-1", "alikafka.aliyuncs.com"},
-    {"cn-shenzhen-inner", "alikafka.aliyuncs.com"},
-    {"cn-shenzhen-st4-d01", "alikafka.aliyuncs.com"},
-    {"cn-shenzhen-su18-b01", "alikafka.aliyuncs.com"},
-    {"cn-wuhan", "alikafka.aliyuncs.com"},
-    {"cn-wulanchabu", "alikafka.aliyuncs.com"},
-    {"cn-yushanfang", "alikafka.aliyuncs.com"},
-    {"cn-zhangbei", "alikafka.aliyuncs.com"},
-    {"cn-zhangbei-na61-b01", "alikafka.aliyuncs.com"},
-    {"cn-zhangjiakou-na62-a01", "alikafka.aliyuncs.com"},
-    {"cn-zhengzhou-nebula-1", "alikafka.aliyuncs.com"},
-    {"eu-west-1-oxs", "alikafka.aliyuncs.com"},
-    {"me-east-1", "alikafka.aliyuncs.com"},
-    {"rus-west-1-pop", "alikafka.aliyuncs.com"}
-  })
-);
   checkConfig(config);
   _endpoint = make_shared<string>(getEndpoint(make_shared<string>("alikafka"), _regionId, _endpointRule, _network, _suffix, _endpointMap, _endpoint));
 };
