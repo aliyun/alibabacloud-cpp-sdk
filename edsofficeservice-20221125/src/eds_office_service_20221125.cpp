2d806fc08b5ece568c991524ca923968482e278e
sdk-team <sdk-team@alibabacloud.com>
2022-12-14 08:08:28 +0000

    AuthCode and StsToken.

---
+++ edsofficeservice-20221125/src/eds_office_service_20221125.cpp
@@ -0,0 +1,59 @@
+// This file is auto-generated, don't edit it. Thanks.
+
+#include <alibabacloud/eds_office_service_20221125.hpp>
+#include <alibabacloud/endpoint_util.hpp>
+#include <alibabacloud/open_api.hpp>
+#include <boost/any.hpp>
+#include <boost/throw_exception.hpp>
+#include <darabonba/core.hpp>
+#include <darabonba/util.hpp>
+#include <iostream>
+#include <map>
+
+using namespace std;
+
+using namespace Alibabacloud_EdsOfficeService20221125;
+
+Alibabacloud_EdsOfficeService20221125::Client::Client(const shared_ptr<Alibabacloud_OpenApi::Config>& config) : Alibabacloud_OpenApi::Client(config) {
+  _endpointRule = make_shared<string>("");
+  checkConfig(config);
+  _endpoint = make_shared<string>(getEndpoint(make_shared<string>("edsofficeservice"), _regionId, _endpointRule, _network, _suffix, _endpointMap, _endpoint));
+};
+
+string Alibabacloud_EdsOfficeService20221125::Client::getEndpoint(shared_ptr<string> productId,
+                                                                  shared_ptr<string> regionId,
+                                                                  shared_ptr<string> endpointRule,
+                                                                  shared_ptr<string> network,
+                                                                  shared_ptr<string> suffix,
+                                                                  shared_ptr<map<string, string>> endpointMap,
+                                                                  shared_ptr<string> endpoint) {
+  if (!Darabonba_Util::Client::empty(endpoint)) {
+    return *endpoint;
+  }
+  if (!Darabonba_Util::Client::isUnset<map<string, string>>(endpointMap) && !Darabonba_Util::Client::empty(make_shared<string>((*endpointMap)[regionId]))) {
+    return (*endpointMap)[regionId];
+  }
+  return Alibabacloud_EndpointUtil::Client::getEndpointRules(productId, regionId, endpointRule, network, suffix);
+}
+
+CreateDesktopPoolResponse Alibabacloud_EdsOfficeService20221125::Client::createDesktopPoolWithOptions(shared_ptr<Darabonba_Util::RuntimeOptions> runtime) {
+  shared_ptr<Alibabacloud_OpenApi::OpenApiRequest> req = make_shared<Alibabacloud_OpenApi::OpenApiRequest>();
+  shared_ptr<Alibabacloud_OpenApi::Params> params = make_shared<Alibabacloud_OpenApi::Params>(map<string, boost::any>({
+    {"action", boost::any(string("CreateDesktopPool"))},
+    {"version", boost::any(string("2022-11-25"))},
+    {"protocol", boost::any(string("HTTPS"))},
+    {"pathname", boost::any(string("/"))},
+    {"method", boost::any(string("POST"))},
+    {"authType", boost::any(string("AK"))},
+    {"style", boost::any(string("RPC"))},
+    {"reqBodyType", boost::any(string("formData"))},
+    {"bodyType", boost::any(string("json"))}
+  }));
+  return CreateDesktopPoolResponse(callApi(params, req, runtime));
+}
+
+CreateDesktopPoolResponse Alibabacloud_EdsOfficeService20221125::Client::createDesktopPool() {
+  shared_ptr<Darabonba_Util::RuntimeOptions> runtime = make_shared<Darabonba_Util::RuntimeOptions>();
+  return createDesktopPoolWithOptions(runtime);
+}
+
