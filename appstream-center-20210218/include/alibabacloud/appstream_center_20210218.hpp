d009c0fadf8ed173417beffd1c5228ca77edcafa
sdk-team <sdk-team@alibabacloud.com>
2023-06-05 03:36:48 +0000

    Auto create user when GetAuthCode.

--- appstream-center-20210218/include/alibabacloud/appstream_center_20210218.hpp
+++ appstream-center-20210218/include/alibabacloud/appstream_center_20210218.hpp
@@ -13,8 +13,150 @@
 using namespace std;
 
 namespace Alibabacloud_Appstream-center20210218 {
+class ExpireLoginTokenRequest : public Darabonba::Model {
+public:
+  shared_ptr<string> endUserId{};
+  shared_ptr<string> loginToken{};
+  shared_ptr<string> officeSiteId{};
+  shared_ptr<string> sessionId{};
+
+  ExpireLoginTokenRequest() {}
+
+  explicit ExpireLoginTokenRequest(const std::map<string, boost::any> &config) : Darabonba::Model(config) {
+    fromMap(config);
+  };
+
+  void validate() override {}
+
+  map<string, boost::any> toMap() override {
+    map<string, boost::any> res;
+    if (endUserId) {
+      res["EndUserId"] = boost::any(*endUserId);
+    }
+    if (loginToken) {
+      res["LoginToken"] = boost::any(*loginToken);
+    }
+    if (officeSiteId) {
+      res["OfficeSiteId"] = boost::any(*officeSiteId);
+    }
+    if (sessionId) {
+      res["SessionId"] = boost::any(*sessionId);
+    }
+    return res;
+  }
+
+  void fromMap(map<string, boost::any> m) override {
+    if (m.find("EndUserId") != m.end() && !m["EndUserId"].empty()) {
+      endUserId = make_shared<string>(boost::any_cast<string>(m["EndUserId"]));
+    }
+    if (m.find("LoginToken") != m.end() && !m["LoginToken"].empty()) {
+      loginToken = make_shared<string>(boost::any_cast<string>(m["LoginToken"]));
+    }
+    if (m.find("OfficeSiteId") != m.end() && !m["OfficeSiteId"].empty()) {
+      officeSiteId = make_shared<string>(boost::any_cast<string>(m["OfficeSiteId"]));
+    }
+    if (m.find("SessionId") != m.end() && !m["SessionId"].empty()) {
+      sessionId = make_shared<string>(boost::any_cast<string>(m["SessionId"]));
+    }
+  }
+
+
+  virtual ~ExpireLoginTokenRequest() = default;
+};
+class ExpireLoginTokenResponseBody : public Darabonba::Model {
+public:
+  shared_ptr<string> requestId{};
+
+  ExpireLoginTokenResponseBody() {}
+
+  explicit ExpireLoginTokenResponseBody(const std::map<string, boost::any> &config) : Darabonba::Model(config) {
+    fromMap(config);
+  };
+
+  void validate() override {}
+
+  map<string, boost::any> toMap() override {
+    map<string, boost::any> res;
+    if (requestId) {
+      res["RequestId"] = boost::any(*requestId);
+    }
+    return res;
+  }
+
+  void fromMap(map<string, boost::any> m) override {
+    if (m.find("RequestId") != m.end() && !m["RequestId"].empty()) {
+      requestId = make_shared<string>(boost::any_cast<string>(m["RequestId"]));
+    }
+  }
+
+
+  virtual ~ExpireLoginTokenResponseBody() = default;
+};
+class ExpireLoginTokenResponse : public Darabonba::Model {
+public:
+  shared_ptr<map<string, string>> headers{};
+  shared_ptr<long> statusCode{};
+  shared_ptr<ExpireLoginTokenResponseBody> body{};
+
+  ExpireLoginTokenResponse() {}
+
+  explicit ExpireLoginTokenResponse(const std::map<string, boost::any> &config) : Darabonba::Model(config) {
+    fromMap(config);
+  };
+
+  void validate() override {
+    if (!headers) {
+      BOOST_THROW_EXCEPTION(boost::enable_error_info(std::runtime_error("headers is required.")));
+    }
+    if (!statusCode) {
+      BOOST_THROW_EXCEPTION(boost::enable_error_info(std::runtime_error("statusCode is required.")));
+    }
+    if (!body) {
+      BOOST_THROW_EXCEPTION(boost::enable_error_info(std::runtime_error("body is required.")));
+    }
+  }
+
+  map<string, boost::any> toMap() override {
+    map<string, boost::any> res;
+    if (headers) {
+      res["headers"] = boost::any(*headers);
+    }
+    if (statusCode) {
+      res["statusCode"] = boost::any(*statusCode);
+    }
+    if (body) {
+      res["body"] = body ? boost::any(body->toMap()) : boost::any(map<string,boost::any>({}));
+    }
+    return res;
+  }
+
+  void fromMap(map<string, boost::any> m) override {
+    if (m.find("headers") != m.end() && !m["headers"].empty()) {
+      map<string, string> map1 = boost::any_cast<map<string, string>>(m["headers"]);
+      map<string, string> toMap1;
+      for (auto item:map1) {
+         toMap1[item.first] = item.second;
+      }
+      headers = make_shared<map<string, string>>(toMap1);
+    }
+    if (m.find("statusCode") != m.end() && !m["statusCode"].empty()) {
+      statusCode = make_shared<long>(boost::any_cast<long>(m["statusCode"]));
+    }
+    if (m.find("body") != m.end() && !m["body"].empty()) {
+      if (typeid(map<string, boost::any>) == m["body"].type()) {
+        ExpireLoginTokenResponseBody model1;
+        model1.fromMap(boost::any_cast<map<string, boost::any>>(m["body"]));
+        body = make_shared<ExpireLoginTokenResponseBody>(model1);
+      }
+    }
+  }
+
+
+  virtual ~ExpireLoginTokenResponse() = default;
+};
 class GetAuthCodeRequest : public Darabonba::Model {
 public:
+  shared_ptr<bool> autoCreateUser{};
   shared_ptr<string> endUserId{};
   shared_ptr<string> externalUserId{};
   shared_ptr<string> policy{};
@@ -29,6 +171,9 @@ public:
 
   map<string, boost::any> toMap() override {
     map<string, boost::any> res;
+    if (autoCreateUser) {
+      res["AutoCreateUser"] = boost::any(*autoCreateUser);
+    }
     if (endUserId) {
       res["EndUserId"] = boost::any(*endUserId);
     }
@@ -42,6 +187,9 @@ public:
   }
 
   void fromMap(map<string, boost::any> m) override {
+    if (m.find("AutoCreateUser") != m.end() && !m["AutoCreateUser"].empty()) {
+      autoCreateUser = make_shared<bool>(boost::any_cast<bool>(m["AutoCreateUser"]));
+    }
     if (m.find("EndUserId") != m.end() && !m["EndUserId"].empty()) {
       endUserId = make_shared<string>(boost::any_cast<string>(m["EndUserId"]));
     }
@@ -211,6 +359,8 @@ public:
                      shared_ptr<string> suffix,
                      shared_ptr<map<string, string>> endpointMap,
                      shared_ptr<string> endpoint);
+  ExpireLoginTokenResponse expireLoginTokenWithOptions(shared_ptr<ExpireLoginTokenRequest> request, shared_ptr<Darabonba_Util::RuntimeOptions> runtime);
+  ExpireLoginTokenResponse expireLoginToken(shared_ptr<ExpireLoginTokenRequest> request);
   GetAuthCodeResponse getAuthCodeWithOptions(shared_ptr<GetAuthCodeRequest> request, shared_ptr<Darabonba_Util::RuntimeOptions> runtime);
   GetAuthCodeResponse getAuthCode(shared_ptr<GetAuthCodeRequest> request);
 
--- appstream-center-20210218/src/appstream_center_20210218.cpp
+++ appstream-center-20210218/src/appstream_center_20210218.cpp
@@ -37,9 +37,49 @@ string Alibabacloud_Appstream-center20210218::Client::getEndpoint(shared_ptr<str
   return Alibabacloud_EndpointUtil::Client::getEndpointRules(productId, regionId, endpointRule, network, suffix);
 }
 
+ExpireLoginTokenResponse Alibabacloud_Appstream-center20210218::Client::expireLoginTokenWithOptions(shared_ptr<ExpireLoginTokenRequest> request, shared_ptr<Darabonba_Util::RuntimeOptions> runtime) {
+  Darabonba_Util::Client::validateModel(request);
+  shared_ptr<map<string, boost::any>> body = make_shared<map<string, boost::any>>(map<string, boost::any>());
+  if (!Darabonba_Util::Client::isUnset<string>(request->endUserId)) {
+    body->insert(pair<string, string>("EndUserId", *request->endUserId));
+  }
+  if (!Darabonba_Util::Client::isUnset<string>(request->loginToken)) {
+    body->insert(pair<string, string>("LoginToken", *request->loginToken));
+  }
+  if (!Darabonba_Util::Client::isUnset<string>(request->officeSiteId)) {
+    body->insert(pair<string, string>("OfficeSiteId", *request->officeSiteId));
+  }
+  if (!Darabonba_Util::Client::isUnset<string>(request->sessionId)) {
+    body->insert(pair<string, string>("SessionId", *request->sessionId));
+  }
+  shared_ptr<Alibabacloud_OpenApi::OpenApiRequest> req = make_shared<Alibabacloud_OpenApi::OpenApiRequest>(map<string, boost::any>({
+    {"body", boost::any(Alibabacloud_OpenApiUtil::Client::parseToMap(body))}
+  }));
+  shared_ptr<Alibabacloud_OpenApi::Params> params = make_shared<Alibabacloud_OpenApi::Params>(map<string, boost::any>({
+    {"action", boost::any(string("ExpireLoginToken"))},
+    {"version", boost::any(string("2021-02-18"))},
+    {"protocol", boost::any(string("HTTPS"))},
+    {"pathname", boost::any(string("/"))},
+    {"method", boost::any(string("POST"))},
+    {"authType", boost::any(string("AK"))},
+    {"style", boost::any(string("RPC"))},
+    {"reqBodyType", boost::any(string("formData"))},
+    {"bodyType", boost::any(string("json"))}
+  }));
+  return ExpireLoginTokenResponse(callApi(params, req, runtime));
+}
+
+ExpireLoginTokenResponse Alibabacloud_Appstream-center20210218::Client::expireLoginToken(shared_ptr<ExpireLoginTokenRequest> request) {
+  shared_ptr<Darabonba_Util::RuntimeOptions> runtime = make_shared<Darabonba_Util::RuntimeOptions>();
+  return expireLoginTokenWithOptions(request, runtime);
+}
+
 GetAuthCodeResponse Alibabacloud_Appstream-center20210218::Client::getAuthCodeWithOptions(shared_ptr<GetAuthCodeRequest> request, shared_ptr<Darabonba_Util::RuntimeOptions> runtime) {
   Darabonba_Util::Client::validateModel(request);
   shared_ptr<map<string, boost::any>> body = make_shared<map<string, boost::any>>(map<string, boost::any>());
+  if (!Darabonba_Util::Client::isUnset<bool>(request->autoCreateUser)) {
+    body->insert(pair<string, bool>("AutoCreateUser", *request->autoCreateUser));
+  }
   if (!Darabonba_Util::Client::isUnset<string>(request->endUserId)) {
     body->insert(pair<string, string>("EndUserId", *request->endUserId));
   }
