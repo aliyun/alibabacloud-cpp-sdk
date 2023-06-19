5deb5074994fbaf3fbe0a3d7b1c6a9753ec06ebf
sdk-team <sdk-team@alibabacloud.com>
2023-06-19 06:08:06 +0000

    Republish.

--- cloudapi-20160714/include/alibabacloud/cloud_api20160714.hpp
+++ cloudapi-20160714/include/alibabacloud/cloud_api20160714.hpp
@@ -33483,6 +33483,161 @@ public:
 
   virtual ~DescribeSignaturesByApiResponse() = default;
 };
+class DescribeSummaryDataRequest : public Darabonba::Model {
+public:
+  shared_ptr<string> securityToken{};
+
+  DescribeSummaryDataRequest() {}
+
+  explicit DescribeSummaryDataRequest(const std::map<string, boost::any> &config) : Darabonba::Model(config) {
+    fromMap(config);
+  };
+
+  void validate() override {}
+
+  map<string, boost::any> toMap() override {
+    map<string, boost::any> res;
+    if (securityToken) {
+      res["SecurityToken"] = boost::any(*securityToken);
+    }
+    return res;
+  }
+
+  void fromMap(map<string, boost::any> m) override {
+    if (m.find("SecurityToken") != m.end() && !m["SecurityToken"].empty()) {
+      securityToken = make_shared<string>(boost::any_cast<string>(m["SecurityToken"]));
+    }
+  }
+
+
+  virtual ~DescribeSummaryDataRequest() = default;
+};
+class DescribeSummaryDataResponseBody : public Darabonba::Model {
+public:
+  shared_ptr<long> expireInstanceNum{};
+  shared_ptr<string> region{};
+  shared_ptr<string> requestId{};
+  shared_ptr<long> usageApiNum{};
+  shared_ptr<long> usageGroupNum{};
+  shared_ptr<long> usageInstanceNum{};
+
+  DescribeSummaryDataResponseBody() {}
+
+  explicit DescribeSummaryDataResponseBody(const std::map<string, boost::any> &config) : Darabonba::Model(config) {
+    fromMap(config);
+  };
+
+  void validate() override {}
+
+  map<string, boost::any> toMap() override {
+    map<string, boost::any> res;
+    if (expireInstanceNum) {
+      res["ExpireInstanceNum"] = boost::any(*expireInstanceNum);
+    }
+    if (region) {
+      res["Region"] = boost::any(*region);
+    }
+    if (requestId) {
+      res["RequestId"] = boost::any(*requestId);
+    }
+    if (usageApiNum) {
+      res["UsageApiNum"] = boost::any(*usageApiNum);
+    }
+    if (usageGroupNum) {
+      res["UsageGroupNum"] = boost::any(*usageGroupNum);
+    }
+    if (usageInstanceNum) {
+      res["UsageInstanceNum"] = boost::any(*usageInstanceNum);
+    }
+    return res;
+  }
+
+  void fromMap(map<string, boost::any> m) override {
+    if (m.find("ExpireInstanceNum") != m.end() && !m["ExpireInstanceNum"].empty()) {
+      expireInstanceNum = make_shared<long>(boost::any_cast<long>(m["ExpireInstanceNum"]));
+    }
+    if (m.find("Region") != m.end() && !m["Region"].empty()) {
+      region = make_shared<string>(boost::any_cast<string>(m["Region"]));
+    }
+    if (m.find("RequestId") != m.end() && !m["RequestId"].empty()) {
+      requestId = make_shared<string>(boost::any_cast<string>(m["RequestId"]));
+    }
+    if (m.find("UsageApiNum") != m.end() && !m["UsageApiNum"].empty()) {
+      usageApiNum = make_shared<long>(boost::any_cast<long>(m["UsageApiNum"]));
+    }
+    if (m.find("UsageGroupNum") != m.end() && !m["UsageGroupNum"].empty()) {
+      usageGroupNum = make_shared<long>(boost::any_cast<long>(m["UsageGroupNum"]));
+    }
+    if (m.find("UsageInstanceNum") != m.end() && !m["UsageInstanceNum"].empty()) {
+      usageInstanceNum = make_shared<long>(boost::any_cast<long>(m["UsageInstanceNum"]));
+    }
+  }
+
+
+  virtual ~DescribeSummaryDataResponseBody() = default;
+};
+class DescribeSummaryDataResponse : public Darabonba::Model {
+public:
+  shared_ptr<map<string, string>> headers{};
+  shared_ptr<long> statusCode{};
+  shared_ptr<DescribeSummaryDataResponseBody> body{};
+
+  DescribeSummaryDataResponse() {}
+
+  explicit DescribeSummaryDataResponse(const std::map<string, boost::any> &config) : Darabonba::Model(config) {
+    fromMap(config);
+  };
+
+  void validate() override {
+    if (!headers) {
+      BOOST_THROW_EXCEPTION(boost::enable_error_info(std::runtime_error("headers is required.")));
+    }
+    if (!statusCode) {
+      BOOST_THROW_EXCEPTION(boost::enable_error_info(std::runtime_error("statusCode is required.")));
+    }
+    if (!body) {
+      BOOST_THROW_EXCEPTION(boost::enable_error_info(std::runtime_error("body is required.")));
+    }
+  }
+
+  map<string, boost::any> toMap() override {
+    map<string, boost::any> res;
+    if (headers) {
+      res["headers"] = boost::any(*headers);
+    }
+    if (statusCode) {
+      res["statusCode"] = boost::any(*statusCode);
+    }
+    if (body) {
+      res["body"] = body ? boost::any(body->toMap()) : boost::any(map<string,boost::any>({}));
+    }
+    return res;
+  }
+
+  void fromMap(map<string, boost::any> m) override {
+    if (m.find("headers") != m.end() && !m["headers"].empty()) {
+      map<string, string> map1 = boost::any_cast<map<string, string>>(m["headers"]);
+      map<string, string> toMap1;
+      for (auto item:map1) {
+         toMap1[item.first] = item.second;
+      }
+      headers = make_shared<map<string, string>>(toMap1);
+    }
+    if (m.find("statusCode") != m.end() && !m["statusCode"].empty()) {
+      statusCode = make_shared<long>(boost::any_cast<long>(m["statusCode"]));
+    }
+    if (m.find("body") != m.end() && !m["body"].empty()) {
+      if (typeid(map<string, boost::any>) == m["body"].type()) {
+        DescribeSummaryDataResponseBody model1;
+        model1.fromMap(boost::any_cast<map<string, boost::any>>(m["body"]));
+        body = make_shared<DescribeSummaryDataResponseBody>(model1);
+      }
+    }
+  }
+
+
+  virtual ~DescribeSummaryDataResponse() = default;
+};
 class DescribeSystemParametersRequest : public Darabonba::Model {
 public:
   shared_ptr<string> securityToken{};
@@ -47200,6 +47355,8 @@ public:
   DescribeSignaturesResponse describeSignatures(shared_ptr<DescribeSignaturesRequest> request);
   DescribeSignaturesByApiResponse describeSignaturesByApiWithOptions(shared_ptr<DescribeSignaturesByApiRequest> request, shared_ptr<Darabonba_Util::RuntimeOptions> runtime);
   DescribeSignaturesByApiResponse describeSignaturesByApi(shared_ptr<DescribeSignaturesByApiRequest> request);
+  DescribeSummaryDataResponse describeSummaryDataWithOptions(shared_ptr<DescribeSummaryDataRequest> request, shared_ptr<Darabonba_Util::RuntimeOptions> runtime);
+  DescribeSummaryDataResponse describeSummaryData(shared_ptr<DescribeSummaryDataRequest> request);
   DescribeSystemParametersResponse describeSystemParametersWithOptions(shared_ptr<DescribeSystemParametersRequest> request, shared_ptr<Darabonba_Util::RuntimeOptions> runtime);
   DescribeSystemParametersResponse describeSystemParameters(shared_ptr<DescribeSystemParametersRequest> request);
   DescribeTrafficControlsResponse describeTrafficControlsWithOptions(shared_ptr<DescribeTrafficControlsRequest> request, shared_ptr<Darabonba_Util::RuntimeOptions> runtime);
