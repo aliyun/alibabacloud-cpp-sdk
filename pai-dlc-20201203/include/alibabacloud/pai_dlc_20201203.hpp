66e36436fabef38d3a0fc398ed81226454079b86
sdk-team <sdk-team@alibabacloud.com>
2023-08-11 08:37:20 +0000

    Generated cpp 2020-12-03 for pai-dlc.

--- pai-dlc-20201203/include/alibabacloud/pai_dlc_20201203.hpp
+++ pai-dlc-20201203/include/alibabacloud/pai_dlc_20201203.hpp
@@ -5407,6 +5407,7 @@ public:
 class GetTensorboardRequest : public Darabonba::Model {
 public:
   shared_ptr<string> jodId{};
+  shared_ptr<string> token{};
   shared_ptr<string> workspaceId{};
 
   GetTensorboardRequest() {}
@@ -5422,6 +5423,9 @@ public:
     if (jodId) {
       res["JodId"] = boost::any(*jodId);
     }
+    if (token) {
+      res["Token"] = boost::any(*token);
+    }
     if (workspaceId) {
       res["WorkspaceId"] = boost::any(*workspaceId);
     }
@@ -5431,6 +5435,9 @@ public:
   void fromMap(map<string, boost::any> m) override {
     if (m.find("JodId") != m.end() && !m["JodId"].empty()) {
       jodId = make_shared<string>(boost::any_cast<string>(m["JodId"]));
+    }
+    if (m.find("Token") != m.end() && !m["Token"].empty()) {
+      token = make_shared<string>(boost::any_cast<string>(m["Token"]));
     }
     if (m.find("WorkspaceId") != m.end() && !m["WorkspaceId"].empty()) {
       workspaceId = make_shared<string>(boost::any_cast<string>(m["WorkspaceId"]));
@@ -5501,6 +5508,133 @@ public:
 
 
   virtual ~GetTensorboardResponse() = default;
+};
+class GetTensorboardSharedUrlRequest : public Darabonba::Model {
+public:
+  shared_ptr<string> expireTimeSeconds{};
+
+  GetTensorboardSharedUrlRequest() {}
+
+  explicit GetTensorboardSharedUrlRequest(const std::map<string, boost::any> &config) : Darabonba::Model(config) {
+    fromMap(config);
+  };
+
+  void validate() override {}
+
+  map<string, boost::any> toMap() override {
+    map<string, boost::any> res;
+    if (expireTimeSeconds) {
+      res["ExpireTimeSeconds"] = boost::any(*expireTimeSeconds);
+    }
+    return res;
+  }
+
+  void fromMap(map<string, boost::any> m) override {
+    if (m.find("ExpireTimeSeconds") != m.end() && !m["ExpireTimeSeconds"].empty()) {
+      expireTimeSeconds = make_shared<string>(boost::any_cast<string>(m["ExpireTimeSeconds"]));
+    }
+  }
+
+
+  virtual ~GetTensorboardSharedUrlRequest() = default;
+};
+class GetTensorboardSharedUrlResponseBody : public Darabonba::Model {
+public:
+  shared_ptr<string> requestId{};
+  shared_ptr<string> tensorboardSharedUrl{};
+
+  GetTensorboardSharedUrlResponseBody() {}
+
+  explicit GetTensorboardSharedUrlResponseBody(const std::map<string, boost::any> &config) : Darabonba::Model(config) {
+    fromMap(config);
+  };
+
+  void validate() override {}
+
+  map<string, boost::any> toMap() override {
+    map<string, boost::any> res;
+    if (requestId) {
+      res["RequestId"] = boost::any(*requestId);
+    }
+    if (tensorboardSharedUrl) {
+      res["TensorboardSharedUrl"] = boost::any(*tensorboardSharedUrl);
+    }
+    return res;
+  }
+
+  void fromMap(map<string, boost::any> m) override {
+    if (m.find("RequestId") != m.end() && !m["RequestId"].empty()) {
+      requestId = make_shared<string>(boost::any_cast<string>(m["RequestId"]));
+    }
+    if (m.find("TensorboardSharedUrl") != m.end() && !m["TensorboardSharedUrl"].empty()) {
+      tensorboardSharedUrl = make_shared<string>(boost::any_cast<string>(m["TensorboardSharedUrl"]));
+    }
+  }
+
+
+  virtual ~GetTensorboardSharedUrlResponseBody() = default;
+};
+class GetTensorboardSharedUrlResponse : public Darabonba::Model {
+public:
+  shared_ptr<map<string, string>> headers{};
+  shared_ptr<long> statusCode{};
+  shared_ptr<GetTensorboardSharedUrlResponseBody> body{};
+
+  GetTensorboardSharedUrlResponse() {}
+
+  explicit GetTensorboardSharedUrlResponse(const std::map<string, boost::any> &config) : Darabonba::Model(config) {
+    fromMap(config);
+  };
+
+  void validate() override {
+    if (!headers) {
+      BOOST_THROW_EXCEPTION(boost::enable_error_info(std::runtime_error("headers is required.")));
+    }
+    if (!statusCode) {
+      BOOST_THROW_EXCEPTION(boost::enable_error_info(std::runtime_error("statusCode is required.")));
+    }
+    if (!body) {
+      BOOST_THROW_EXCEPTION(boost::enable_error_info(std::runtime_error("body is required.")));
+    }
+  }
+
+  map<string, boost::any> toMap() override {
+    map<string, boost::any> res;
+    if (headers) {
+      res["headers"] = boost::any(*headers);
+    }
+    if (statusCode) {
+      res["statusCode"] = boost::any(*statusCode);
+    }
+    if (body) {
+      res["body"] = body ? boost::any(body->toMap()) : boost::any(map<string,boost::any>({}));
+    }
+    return res;
+  }
+
+  void fromMap(map<string, boost::any> m) override {
+    if (m.find("headers") != m.end() && !m["headers"].empty()) {
+      map<string, string> map1 = boost::any_cast<map<string, string>>(m["headers"]);
+      map<string, string> toMap1;
+      for (auto item:map1) {
+         toMap1[item.first] = item.second;
+      }
+      headers = make_shared<map<string, string>>(toMap1);
+    }
+    if (m.find("statusCode") != m.end() && !m["statusCode"].empty()) {
+      statusCode = make_shared<long>(boost::any_cast<long>(m["statusCode"]));
+    }
+    if (m.find("body") != m.end() && !m["body"].empty()) {
+      if (typeid(map<string, boost::any>) == m["body"].type()) {
+        GetTensorboardSharedUrlResponseBody model1;
+        model1.fromMap(boost::any_cast<map<string, boost::any>>(m["body"]));
+        body = make_shared<GetTensorboardSharedUrlResponseBody>(model1);
+      }
+    }
+  }
+
+
+  virtual ~GetTensorboardSharedUrlResponse() = default;
 };
 class GetTokenRequest : public Darabonba::Model {
 public:
@@ -7278,6 +7412,11 @@ public:
                                                    shared_ptr<map<string, string>> headers,
                                                    shared_ptr<Darabonba_Util::RuntimeOptions> runtime);
   GetTensorboardResponse getTensorboard(shared_ptr<string> TensorboardId, shared_ptr<GetTensorboardRequest> request);
+  GetTensorboardSharedUrlResponse getTensorboardSharedUrlWithOptions(shared_ptr<string> TensorboardId,
+                                                                     shared_ptr<GetTensorboardSharedUrlRequest> request,
+                                                                     shared_ptr<map<string, string>> headers,
+                                                                     shared_ptr<Darabonba_Util::RuntimeOptions> runtime);
+  GetTensorboardSharedUrlResponse getTensorboardSharedUrl(shared_ptr<string> TensorboardId, shared_ptr<GetTensorboardSharedUrlRequest> request);
   GetTokenResponse getTokenWithOptions(shared_ptr<GetTokenRequest> request, shared_ptr<map<string, string>> headers, shared_ptr<Darabonba_Util::RuntimeOptions> runtime);
   GetTokenResponse getToken(shared_ptr<GetTokenRequest> request);
   GetWebTerminalResponse getWebTerminalWithOptions(shared_ptr<string> JobId,
--- pai-dlc-20201203/src/pai_dlc_20201203.cpp
+++ pai-dlc-20201203/src/pai_dlc_20201203.cpp
@@ -519,6 +519,9 @@ GetTensorboardResponse Alibabacloud_Pai-dlc20201203::Client::getTensorboardWithO
   if (!Darabonba_Util::Client::isUnset<string>(request->jodId)) {
     query->insert(pair<string, string>("JodId", *request->jodId));
   }
+  if (!Darabonba_Util::Client::isUnset<string>(request->token)) {
+    query->insert(pair<string, string>("Token", *request->token));
+  }
   if (!Darabonba_Util::Client::isUnset<string>(request->workspaceId)) {
     query->insert(pair<string, string>("WorkspaceId", *request->workspaceId));
   }
@@ -544,6 +547,39 @@ GetTensorboardResponse Alibabacloud_Pai-dlc20201203::Client::getTensorboard(shar
   shared_ptr<Darabonba_Util::RuntimeOptions> runtime = make_shared<Darabonba_Util::RuntimeOptions>();
   shared_ptr<map<string, string>> headers = make_shared<map<string, string>>(map<string, string>());
   return getTensorboardWithOptions(TensorboardId, request, headers, runtime);
+}
+
+GetTensorboardSharedUrlResponse Alibabacloud_Pai-dlc20201203::Client::getTensorboardSharedUrlWithOptions(shared_ptr<string> TensorboardId,
+                                                                                                         shared_ptr<GetTensorboardSharedUrlRequest> request,
+                                                                                                         shared_ptr<map<string, string>> headers,
+                                                                                                         shared_ptr<Darabonba_Util::RuntimeOptions> runtime) {
+  Darabonba_Util::Client::validateModel(request);
+  shared_ptr<map<string, boost::any>> query = make_shared<map<string, boost::any>>(map<string, boost::any>());
+  if (!Darabonba_Util::Client::isUnset<string>(request->expireTimeSeconds)) {
+    query->insert(pair<string, string>("ExpireTimeSeconds", *request->expireTimeSeconds));
+  }
+  shared_ptr<Alibabacloud_OpenApi::OpenApiRequest> req = make_shared<Alibabacloud_OpenApi::OpenApiRequest>(map<string, boost::any>({
+    {"headers", !headers ? boost::any() : boost::any(*headers)},
+    {"query", boost::any(Alibabacloud_OpenApiUtil::Client::query(query))}
+  }));
+  shared_ptr<Alibabacloud_OpenApi::Params> params = make_shared<Alibabacloud_OpenApi::Params>(map<string, boost::any>({
+    {"action", boost::any(string("GetTensorboardSharedUrl"))},
+    {"version", boost::any(string("2020-12-03"))},
+    {"protocol", boost::any(string("HTTPS"))},
+    {"pathname", boost::any(string("/api/v1/tensorboards/") + string(Alibabacloud_OpenApiUtil::Client::getEncodeParam(TensorboardId)) + string("/sharedurl"))},
+    {"method", boost::any(string("GET"))},
+    {"authType", boost::any(string("AK"))},
+    {"style", boost::any(string("ROA"))},
+    {"reqBodyType", boost::any(string("json"))},
+    {"bodyType", boost::any(string("json"))}
+  }));
+  return GetTensorboardSharedUrlResponse(callApi(params, req, runtime));
+}
+
+GetTensorboardSharedUrlResponse Alibabacloud_Pai-dlc20201203::Client::getTensorboardSharedUrl(shared_ptr<string> TensorboardId, shared_ptr<GetTensorboardSharedUrlRequest> request) {
+  shared_ptr<Darabonba_Util::RuntimeOptions> runtime = make_shared<Darabonba_Util::RuntimeOptions>();
+  shared_ptr<map<string, string>> headers = make_shared<map<string, string>>(map<string, string>());
+  return getTensorboardSharedUrlWithOptions(TensorboardId, request, headers, runtime);
 }
 
 GetTokenResponse Alibabacloud_Pai-dlc20201203::Client::getTokenWithOptions(shared_ptr<GetTokenRequest> request, shared_ptr<map<string, string>> headers, shared_ptr<Darabonba_Util::RuntimeOptions> runtime) {
