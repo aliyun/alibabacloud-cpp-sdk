8374914295e50503573b7bd2f2bfcb800281c0c4
sdk-team <sdk-team@alibabacloud.com>
2023-08-21 08:22:38 +0000

    Generated cpp 2020-05-12 for Sasti.

--- sasti-20200512/src/sasti_20200512.cpp
+++ sasti-20200512/src/sasti_20200512.cpp
@@ -10,7 +10,6 @@
 #include <darabonba/util.hpp>
 #include <iostream>
 #include <map>
-#include <vector>
 
 using namespace std;
 
@@ -138,36 +137,5 @@ DescribeIpReportResponse Alibabacloud_Sasti20200512::Client::describeIpReportWit
 DescribeIpReportResponse Alibabacloud_Sasti20200512::Client::describeIpReport(shared_ptr<DescribeIpReportRequest> request) {
   shared_ptr<Darabonba_Util::RuntimeOptions> runtime = make_shared<Darabonba_Util::RuntimeOptions>();
   return describeIpReportWithOptions(request, runtime);
-}
-
-GetGraphQueryTemplatesResponse Alibabacloud_Sasti20200512::Client::getGraphQueryTemplatesWithOptions(shared_ptr<GetGraphQueryTemplatesRequest> request, shared_ptr<Darabonba_Util::RuntimeOptions> runtime) {
-  Darabonba_Util::Client::validateModel(request);
-  shared_ptr<map<string, boost::any>> body = make_shared<map<string, boost::any>>(map<string, boost::any>());
-  if (!Darabonba_Util::Client::isUnset<string>(request->env)) {
-    body->insert(pair<string, string>("Env", *request->env));
-  }
-  if (!Darabonba_Util::Client::isUnset<string>(request->serviceUnit)) {
-    body->insert(pair<string, string>("ServiceUnit", *request->serviceUnit));
-  }
-  shared_ptr<Alibabacloud_OpenApi::OpenApiRequest> req = make_shared<Alibabacloud_OpenApi::OpenApiRequest>(map<string, boost::any>({
-    {"body", boost::any(Alibabacloud_OpenApiUtil::Client::parseToMap(body))}
-  }));
-  shared_ptr<Alibabacloud_OpenApi::Params> params = make_shared<Alibabacloud_OpenApi::Params>(map<string, boost::any>({
-    {"action", boost::any(string("GetGraphQueryTemplates"))},
-    {"version", boost::any(string("2020-05-12"))},
-    {"protocol", boost::any(string("HTTPS"))},
-    {"pathname", boost::any(string("/"))},
-    {"method", boost::any(string("POST"))},
-    {"authType", boost::any(string("AK"))},
-    {"style", boost::any(string("RPC"))},
-    {"reqBodyType", boost::any(string("formData"))},
-    {"bodyType", boost::any(string("json"))}
-  }));
-  return GetGraphQueryTemplatesResponse(callApi(params, req, runtime));
-}
-
-GetGraphQueryTemplatesResponse Alibabacloud_Sasti20200512::Client::getGraphQueryTemplates(shared_ptr<GetGraphQueryTemplatesRequest> request) {
-  shared_ptr<Darabonba_Util::RuntimeOptions> runtime = make_shared<Darabonba_Util::RuntimeOptions>();
-  return getGraphQueryTemplatesWithOptions(request, runtime);
 }
 
