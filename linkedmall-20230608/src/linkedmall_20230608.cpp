fb22ae2f37f45cd9a529700eae39881a7d539909
sdk-team <sdk-team@alibabacloud.com>
2023-08-23 03:17:53 +0000

    Generated cpp 2023-06-08 for linkedmall.

---
+++ linkedmall-20230608/src/linkedmall_20230608.cpp
@@ -0,0 +1,92 @@
+// This file is auto-generated, don't edit it. Thanks.
+
+#include <alibabacloud/linkedmall_20230608.hpp>
+#include <alibabacloud/endpoint_util.hpp>
+#include <alibabacloud/open_api.hpp>
+#include <darabonba/util.hpp>
+#include <iostream>
+#include <map>
+
+using namespace std;
+
+using namespace Alibabacloud_Linkedmall20230608;
+
+Alibabacloud_Linkedmall20230608::Client::Client(const shared_ptr<Alibabacloud_OpenApi::Config>& config) : Alibabacloud_OpenApi::Client(config) {
+  _endpointRule = make_shared<string>("regional");
+  _endpointMap = make_shared<map<string, string>>(map<string, string>({
+    {"cn-hangzhou", "linkedmall.aliyuncs.com"},
+    {"cn-shanghai", "linkedmall.aliyuncs.com"},
+    {"ap-northeast-1", "linkedmall.aliyuncs.com"},
+    {"ap-northeast-2-pop", "linkedmall.aliyuncs.com"},
+    {"ap-south-1", "linkedmall.aliyuncs.com"},
+    {"ap-southeast-1", "linkedmall.aliyuncs.com"},
+    {"ap-southeast-2", "linkedmall.aliyuncs.com"},
+    {"ap-southeast-3", "linkedmall.aliyuncs.com"},
+    {"ap-southeast-5", "linkedmall.aliyuncs.com"},
+    {"cn-beijing", "linkedmall.aliyuncs.com"},
+    {"cn-beijing-finance-1", "linkedmall.aliyuncs.com"},
+    {"cn-beijing-finance-pop", "linkedmall.aliyuncs.com"},
+    {"cn-beijing-gov-1", "linkedmall.aliyuncs.com"},
+    {"cn-beijing-nu16-b01", "linkedmall.aliyuncs.com"},
+    {"cn-chengdu", "linkedmall.aliyuncs.com"},
+    {"cn-edge-1", "linkedmall.aliyuncs.com"},
+    {"cn-fujian", "linkedmall.aliyuncs.com"},
+    {"cn-haidian-cm12-c01", "linkedmall.aliyuncs.com"},
+    {"cn-hangzhou-bj-b01", "linkedmall.aliyuncs.com"},
+    {"cn-hangzhou-finance", "linkedmall.aliyuncs.com"},
+    {"cn-hangzhou-internal-prod-1", "linkedmall.aliyuncs.com"},
+    {"cn-hangzhou-internal-test-1", "linkedmall.aliyuncs.com"},
+    {"cn-hangzhou-internal-test-2", "linkedmall.aliyuncs.com"},
+    {"cn-hangzhou-internal-test-3", "linkedmall.aliyuncs.com"},
+    {"cn-hangzhou-test-306", "linkedmall.aliyuncs.com"},
+    {"cn-hongkong", "linkedmall.aliyuncs.com"},
+    {"cn-hongkong-finance-pop", "linkedmall.aliyuncs.com"},
+    {"cn-huhehaote", "linkedmall.aliyuncs.com"},
+    {"cn-north-2-gov-1", "linkedmall.aliyuncs.com"},
+    {"cn-qingdao", "linkedmall.aliyuncs.com"},
+    {"cn-qingdao-nebula", "linkedmall.aliyuncs.com"},
+    {"cn-shanghai-et15-b01", "linkedmall.aliyuncs.com"},
+    {"cn-shanghai-et2-b01", "linkedmall.aliyuncs.com"},
+    {"cn-shanghai-finance-1", "linkedmall.aliyuncs.com"},
+    {"cn-shanghai-inner", "linkedmall.aliyuncs.com"},
+    {"cn-shanghai-internal-test-1", "linkedmall.aliyuncs.com"},
+    {"cn-shenzhen", "linkedmall.aliyuncs.com"},
+    {"cn-shenzhen-finance-1", "linkedmall.aliyuncs.com"},
+    {"cn-shenzhen-inner", "linkedmall.aliyuncs.com"},
+    {"cn-shenzhen-st4-d01", "linkedmall.aliyuncs.com"},
+    {"cn-shenzhen-su18-b01", "linkedmall.aliyuncs.com"},
+    {"cn-wuhan", "linkedmall.aliyuncs.com"},
+    {"cn-yushanfang", "linkedmall.aliyuncs.com"},
+    {"cn-zhangbei-na61-b01", "linkedmall.aliyuncs.com"},
+    {"cn-zhangjiakou", "linkedmall.aliyuncs.com"},
+    {"cn-zhangjiakou-na62-a01", "linkedmall.aliyuncs.com"},
+    {"cn-zhengzhou-nebula-1", "linkedmall.aliyuncs.com"},
+    {"eu-central-1", "linkedmall.aliyuncs.com"},
+    {"eu-west-1", "linkedmall.aliyuncs.com"},
+    {"eu-west-1-oxs", "linkedmall.aliyuncs.com"},
+    {"me-east-1", "linkedmall.aliyuncs.com"},
+    {"rus-west-1-pop", "linkedmall.aliyuncs.com"},
+    {"us-east-1", "linkedmall.aliyuncs.com"},
+    {"us-west-1", "linkedmall.aliyuncs.com"}
+  })
+);
+  checkConfig(config);
+  _endpoint = make_shared<string>(getEndpoint(make_shared<string>("linkedmall"), _regionId, _endpointRule, _network, _suffix, _endpointMap, _endpoint));
+};
+
+string Alibabacloud_Linkedmall20230608::Client::getEndpoint(shared_ptr<string> productId,
+                                                            shared_ptr<string> regionId,
+                                                            shared_ptr<string> endpointRule,
+                                                            shared_ptr<string> network,
+                                                            shared_ptr<string> suffix,
+                                                            shared_ptr<map<string, string>> endpointMap,
+                                                            shared_ptr<string> endpoint) {
+  if (!Darabonba_Util::Client::empty(endpoint)) {
+    return *endpoint;
+  }
+  if (!Darabonba_Util::Client::isUnset<map<string, string>>(endpointMap) && !Darabonba_Util::Client::empty(make_shared<string>((*endpointMap)[regionId]))) {
+    return (*endpointMap)[regionId];
+  }
+  return Alibabacloud_EndpointUtil::Client::getEndpointRules(productId, regionId, endpointRule, network, suffix);
+}
+
