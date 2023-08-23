521d213f9a4c2048ec9f1768b55fad5e7315f80a
sdk-team <sdk-team@alibabacloud.com>
2023-08-23 02:21:55 +0000

    Generated cpp 2022-04-19 for ResourceDirectoryMaster.

--- resourcedirectorymaster-20220419/include/alibabacloud/resource_directory_master_20220419.hpp
+++ resourcedirectorymaster-20220419/include/alibabacloud/resource_directory_master_20220419.hpp
@@ -3009,126 +3009,6 @@ public:
 
   virtual ~DeleteFolderResponse() = default;
 };
-class DeleteInvalidCloudAccountRecordRequest : public Darabonba::Model {
-public:
-  shared_ptr<string> recordId{};
-
-  DeleteInvalidCloudAccountRecordRequest() {}
-
-  explicit DeleteInvalidCloudAccountRecordRequest(const std::map<string, boost::any> &config) : Darabonba::Model(config) {
-    fromMap(config);
-  };
-
-  void validate() override {}
-
-  map<string, boost::any> toMap() override {
-    map<string, boost::any> res;
-    if (recordId) {
-      res["RecordId"] = boost::any(*recordId);
-    }
-    return res;
-  }
-
-  void fromMap(map<string, boost::any> m) override {
-    if (m.find("RecordId") != m.end() && !m["RecordId"].empty()) {
-      recordId = make_shared<string>(boost::any_cast<string>(m["RecordId"]));
-    }
-  }
-
-
-  virtual ~DeleteInvalidCloudAccountRecordRequest() = default;
-};
-class DeleteInvalidCloudAccountRecordResponseBody : public Darabonba::Model {
-public:
-  shared_ptr<string> requestId{};
-
-  DeleteInvalidCloudAccountRecordResponseBody() {}
-
-  explicit DeleteInvalidCloudAccountRecordResponseBody(const std::map<string, boost::any> &config) : Darabonba::Model(config) {
-    fromMap(config);
-  };
-
-  void validate() override {}
-
-  map<string, boost::any> toMap() override {
-    map<string, boost::any> res;
-    if (requestId) {
-      res["RequestId"] = boost::any(*requestId);
-    }
-    return res;
-  }
-
-  void fromMap(map<string, boost::any> m) override {
-    if (m.find("RequestId") != m.end() && !m["RequestId"].empty()) {
-      requestId = make_shared<string>(boost::any_cast<string>(m["RequestId"]));
-    }
-  }
-
-
-  virtual ~DeleteInvalidCloudAccountRecordResponseBody() = default;
-};
-class DeleteInvalidCloudAccountRecordResponse : public Darabonba::Model {
-public:
-  shared_ptr<map<string, string>> headers{};
-  shared_ptr<long> statusCode{};
-  shared_ptr<DeleteInvalidCloudAccountRecordResponseBody> body{};
-
-  DeleteInvalidCloudAccountRecordResponse() {}
-
-  explicit DeleteInvalidCloudAccountRecordResponse(const std::map<string, boost::any> &config) : Darabonba::Model(config) {
-    fromMap(config);
-  };
-
-  void validate() override {
-    if (!headers) {
-      BOOST_THROW_EXCEPTION(boost::enable_error_info(std::runtime_error("headers is required.")));
-    }
-    if (!statusCode) {
-      BOOST_THROW_EXCEPTION(boost::enable_error_info(std::runtime_error("statusCode is required.")));
-    }
-    if (!body) {
-      BOOST_THROW_EXCEPTION(boost::enable_error_info(std::runtime_error("body is required.")));
-    }
-  }
-
-  map<string, boost::any> toMap() override {
-    map<string, boost::any> res;
-    if (headers) {
-      res["headers"] = boost::any(*headers);
-    }
-    if (statusCode) {
-      res["statusCode"] = boost::any(*statusCode);
-    }
-    if (body) {
-      res["body"] = body ? boost::any(body->toMap()) : boost::any(map<string,boost::any>({}));
-    }
-    return res;
-  }
-
-  void fromMap(map<string, boost::any> m) override {
-    if (m.find("headers") != m.end() && !m["headers"].empty()) {
-      map<string, string> map1 = boost::any_cast<map<string, string>>(m["headers"]);
-      map<string, string> toMap1;
-      for (auto item:map1) {
-         toMap1[item.first] = item.second;
-      }
-      headers = make_shared<map<string, string>>(toMap1);
-    }
-    if (m.find("statusCode") != m.end() && !m["statusCode"].empty()) {
-      statusCode = make_shared<long>(boost::any_cast<long>(m["statusCode"]));
-    }
-    if (m.find("body") != m.end() && !m["body"].empty()) {
-      if (typeid(map<string, boost::any>) == m["body"].type()) {
-        DeleteInvalidCloudAccountRecordResponseBody model1;
-        model1.fromMap(boost::any_cast<map<string, boost::any>>(m["body"]));
-        body = make_shared<DeleteInvalidCloudAccountRecordResponseBody>(model1);
-      }
-    }
-  }
-
-
-  virtual ~DeleteInvalidCloudAccountRecordResponse() = default;
-};
 class DeleteMessageContactRequest : public Darabonba::Model {
 public:
   shared_ptr<string> contactId{};
@@ -14151,8 +14031,6 @@ public:
   DeleteControlPolicyResponse deleteControlPolicy(shared_ptr<DeleteControlPolicyRequest> request);
   DeleteFolderResponse deleteFolderWithOptions(shared_ptr<DeleteFolderRequest> request, shared_ptr<Darabonba_Util::RuntimeOptions> runtime);
   DeleteFolderResponse deleteFolder(shared_ptr<DeleteFolderRequest> request);
-  DeleteInvalidCloudAccountRecordResponse deleteInvalidCloudAccountRecordWithOptions(shared_ptr<DeleteInvalidCloudAccountRecordRequest> request, shared_ptr<Darabonba_Util::RuntimeOptions> runtime);
-  DeleteInvalidCloudAccountRecordResponse deleteInvalidCloudAccountRecord(shared_ptr<DeleteInvalidCloudAccountRecordRequest> request);
   DeleteMessageContactResponse deleteMessageContactWithOptions(shared_ptr<DeleteMessageContactRequest> request, shared_ptr<Darabonba_Util::RuntimeOptions> runtime);
   DeleteMessageContactResponse deleteMessageContact(shared_ptr<DeleteMessageContactRequest> request);
   DeregisterDelegatedAdministratorResponse deregisterDelegatedAdministratorWithOptions(shared_ptr<DeregisterDelegatedAdministratorRequest> request, shared_ptr<Darabonba_Util::RuntimeOptions> runtime);
