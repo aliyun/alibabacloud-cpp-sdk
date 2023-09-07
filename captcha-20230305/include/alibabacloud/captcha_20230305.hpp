407aded41d3f54c2cd22297fbf306e2b171867d5
sdk-team <sdk-team@alibabacloud.com>
2023-09-07 08:54:12 +0000

    Generated cpp 2023-03-05 for captcha.

--- captcha-20230305/include/alibabacloud/captcha_20230305.hpp
+++ captcha-20230305/include/alibabacloud/captcha_20230305.hpp
@@ -194,6 +194,187 @@ public:
 
   virtual ~VerifyCaptchaResponse() = default;
 };
+class VerifyIntelligentCaptchaRequest : public Darabonba::Model {
+public:
+  shared_ptr<string> captchaVerifyParam{};
+
+  VerifyIntelligentCaptchaRequest() {}
+
+  explicit VerifyIntelligentCaptchaRequest(const std::map<string, boost::any> &config) : Darabonba::Model(config) {
+    fromMap(config);
+  };
+
+  void validate() override {}
+
+  map<string, boost::any> toMap() override {
+    map<string, boost::any> res;
+    if (captchaVerifyParam) {
+      res["CaptchaVerifyParam"] = boost::any(*captchaVerifyParam);
+    }
+    return res;
+  }
+
+  void fromMap(map<string, boost::any> m) override {
+    if (m.find("CaptchaVerifyParam") != m.end() && !m["CaptchaVerifyParam"].empty()) {
+      captchaVerifyParam = make_shared<string>(boost::any_cast<string>(m["CaptchaVerifyParam"]));
+    }
+  }
+
+
+  virtual ~VerifyIntelligentCaptchaRequest() = default;
+};
+class VerifyIntelligentCaptchaResponseBodyResult : public Darabonba::Model {
+public:
+  shared_ptr<bool> verifyResult{};
+
+  VerifyIntelligentCaptchaResponseBodyResult() {}
+
+  explicit VerifyIntelligentCaptchaResponseBodyResult(const std::map<string, boost::any> &config) : Darabonba::Model(config) {
+    fromMap(config);
+  };
+
+  void validate() override {}
+
+  map<string, boost::any> toMap() override {
+    map<string, boost::any> res;
+    if (verifyResult) {
+      res["VerifyResult"] = boost::any(*verifyResult);
+    }
+    return res;
+  }
+
+  void fromMap(map<string, boost::any> m) override {
+    if (m.find("VerifyResult") != m.end() && !m["VerifyResult"].empty()) {
+      verifyResult = make_shared<bool>(boost::any_cast<bool>(m["VerifyResult"]));
+    }
+  }
+
+
+  virtual ~VerifyIntelligentCaptchaResponseBodyResult() = default;
+};
+class VerifyIntelligentCaptchaResponseBody : public Darabonba::Model {
+public:
+  shared_ptr<string> code{};
+  shared_ptr<string> message{};
+  shared_ptr<string> requestId{};
+  shared_ptr<VerifyIntelligentCaptchaResponseBodyResult> result{};
+  shared_ptr<bool> success{};
+
+  VerifyIntelligentCaptchaResponseBody() {}
+
+  explicit VerifyIntelligentCaptchaResponseBody(const std::map<string, boost::any> &config) : Darabonba::Model(config) {
+    fromMap(config);
+  };
+
+  void validate() override {}
+
+  map<string, boost::any> toMap() override {
+    map<string, boost::any> res;
+    if (code) {
+      res["Code"] = boost::any(*code);
+    }
+    if (message) {
+      res["Message"] = boost::any(*message);
+    }
+    if (requestId) {
+      res["RequestId"] = boost::any(*requestId);
+    }
+    if (result) {
+      res["Result"] = result ? boost::any(result->toMap()) : boost::any(map<string,boost::any>({}));
+    }
+    if (success) {
+      res["Success"] = boost::any(*success);
+    }
+    return res;
+  }
+
+  void fromMap(map<string, boost::any> m) override {
+    if (m.find("Code") != m.end() && !m["Code"].empty()) {
+      code = make_shared<string>(boost::any_cast<string>(m["Code"]));
+    }
+    if (m.find("Message") != m.end() && !m["Message"].empty()) {
+      message = make_shared<string>(boost::any_cast<string>(m["Message"]));
+    }
+    if (m.find("RequestId") != m.end() && !m["RequestId"].empty()) {
+      requestId = make_shared<string>(boost::any_cast<string>(m["RequestId"]));
+    }
+    if (m.find("Result") != m.end() && !m["Result"].empty()) {
+      if (typeid(map<string, boost::any>) == m["Result"].type()) {
+        VerifyIntelligentCaptchaResponseBodyResult model1;
+        model1.fromMap(boost::any_cast<map<string, boost::any>>(m["Result"]));
+        result = make_shared<VerifyIntelligentCaptchaResponseBodyResult>(model1);
+      }
+    }
+    if (m.find("Success") != m.end() && !m["Success"].empty()) {
+      success = make_shared<bool>(boost::any_cast<bool>(m["Success"]));
+    }
+  }
+
+
+  virtual ~VerifyIntelligentCaptchaResponseBody() = default;
+};
+class VerifyIntelligentCaptchaResponse : public Darabonba::Model {
+public:
+  shared_ptr<map<string, string>> headers{};
+  shared_ptr<long> statusCode{};
+  shared_ptr<VerifyIntelligentCaptchaResponseBody> body{};
+
+  VerifyIntelligentCaptchaResponse() {}
+
+  explicit VerifyIntelligentCaptchaResponse(const std::map<string, boost::any> &config) : Darabonba::Model(config) {
+    fromMap(config);
+  };
+
+  void validate() override {
+    if (!headers) {
+      BOOST_THROW_EXCEPTION(boost::enable_error_info(std::runtime_error("headers is required.")));
+    }
+    if (!statusCode) {
+      BOOST_THROW_EXCEPTION(boost::enable_error_info(std::runtime_error("statusCode is required.")));
+    }
+    if (!body) {
+      BOOST_THROW_EXCEPTION(boost::enable_error_info(std::runtime_error("body is required.")));
+    }
+  }
+
+  map<string, boost::any> toMap() override {
+    map<string, boost::any> res;
+    if (headers) {
+      res["headers"] = boost::any(*headers);
+    }
+    if (statusCode) {
+      res["statusCode"] = boost::any(*statusCode);
+    }
+    if (body) {
+      res["body"] = body ? boost::any(body->toMap()) : boost::any(map<string,boost::any>({}));
+    }
+    return res;
+  }
+
+  void fromMap(map<string, boost::any> m) override {
+    if (m.find("headers") != m.end() && !m["headers"].empty()) {
+      map<string, string> map1 = boost::any_cast<map<string, string>>(m["headers"]);
+      map<string, string> toMap1;
+      for (auto item:map1) {
+         toMap1[item.first] = item.second;
+      }
+      headers = make_shared<map<string, string>>(toMap1);
+    }
+    if (m.find("statusCode") != m.end() && !m["statusCode"].empty()) {
+      statusCode = make_shared<long>(boost::any_cast<long>(m["statusCode"]));
+    }
+    if (m.find("body") != m.end() && !m["body"].empty()) {
+      if (typeid(map<string, boost::any>) == m["body"].type()) {
+        VerifyIntelligentCaptchaResponseBody model1;
+        model1.fromMap(boost::any_cast<map<string, boost::any>>(m["body"]));
+        body = make_shared<VerifyIntelligentCaptchaResponseBody>(model1);
+      }
+    }
+  }
+
+
+  virtual ~VerifyIntelligentCaptchaResponse() = default;
+};
 class Client : Alibabacloud_OpenApi::Client {
 public:
   explicit Client(const shared_ptr<Alibabacloud_OpenApi::Config>& config);
@@ -206,6 +387,8 @@ public:
                      shared_ptr<string> endpoint);
   VerifyCaptchaResponse verifyCaptchaWithOptions(shared_ptr<VerifyCaptchaRequest> request, shared_ptr<Darabonba_Util::RuntimeOptions> runtime);
   VerifyCaptchaResponse verifyCaptcha(shared_ptr<VerifyCaptchaRequest> request);
+  VerifyIntelligentCaptchaResponse verifyIntelligentCaptchaWithOptions(shared_ptr<VerifyIntelligentCaptchaRequest> request, shared_ptr<Darabonba_Util::RuntimeOptions> runtime);
+  VerifyIntelligentCaptchaResponse verifyIntelligentCaptcha(shared_ptr<VerifyIntelligentCaptchaRequest> request);
 
   virtual ~Client() = default;
 };
--- captcha-20230305/src/captcha_20230305.cpp
+++ captcha-20230305/src/captcha_20230305.cpp
@@ -65,3 +65,31 @@ VerifyCaptchaResponse Alibabacloud_Captcha20230305::Client::verifyCaptcha(shared
   return verifyCaptchaWithOptions(request, runtime);
 }
 
+VerifyIntelligentCaptchaResponse Alibabacloud_Captcha20230305::Client::verifyIntelligentCaptchaWithOptions(shared_ptr<VerifyIntelligentCaptchaRequest> request, shared_ptr<Darabonba_Util::RuntimeOptions> runtime) {
+  Darabonba_Util::Client::validateModel(request);
+  shared_ptr<map<string, boost::any>> body = make_shared<map<string, boost::any>>(map<string, boost::any>());
+  if (!Darabonba_Util::Client::isUnset<string>(request->captchaVerifyParam)) {
+    body->insert(pair<string, string>("CaptchaVerifyParam", *request->captchaVerifyParam));
+  }
+  shared_ptr<Alibabacloud_OpenApi::OpenApiRequest> req = make_shared<Alibabacloud_OpenApi::OpenApiRequest>(map<string, boost::any>({
+    {"body", boost::any(Alibabacloud_OpenApiUtil::Client::parseToMap(body))}
+  }));
+  shared_ptr<Alibabacloud_OpenApi::Params> params = make_shared<Alibabacloud_OpenApi::Params>(map<string, boost::any>({
+    {"action", boost::any(string("VerifyIntelligentCaptcha"))},
+    {"version", boost::any(string("2023-03-05"))},
+    {"protocol", boost::any(string("HTTPS"))},
+    {"pathname", boost::any(string("/"))},
+    {"method", boost::any(string("POST"))},
+    {"authType", boost::any(string("AK"))},
+    {"style", boost::any(string("RPC"))},
+    {"reqBodyType", boost::any(string("formData"))},
+    {"bodyType", boost::any(string("json"))}
+  }));
+  return VerifyIntelligentCaptchaResponse(callApi(params, req, runtime));
+}
+
+VerifyIntelligentCaptchaResponse Alibabacloud_Captcha20230305::Client::verifyIntelligentCaptcha(shared_ptr<VerifyIntelligentCaptchaRequest> request) {
+  shared_ptr<Darabonba_Util::RuntimeOptions> runtime = make_shared<Darabonba_Util::RuntimeOptions>();
+  return verifyIntelligentCaptchaWithOptions(request, runtime);
+}
+
