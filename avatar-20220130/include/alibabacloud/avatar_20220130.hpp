4bf4b4cff374e5134f2e8d14056ce3928631366f
sdk-team <sdk-team@alibabacloud.com>
2024-01-17 03:25:57 +0000

    Generated cpp 2022-01-30 for avatar.

--- avatar-20220130/include/alibabacloud/avatar_20220130.hpp
+++ avatar-20220130/include/alibabacloud/avatar_20220130.hpp
@@ -664,6 +664,154 @@ public:
 
   virtual ~ClientStartResponse() = default;
 };
+class ClientUnbindDeviceRequest : public Darabonba::Model {
+public:
+  shared_ptr<string> deviceId{};
+  shared_ptr<long> tenantId{};
+
+  ClientUnbindDeviceRequest() {}
+
+  explicit ClientUnbindDeviceRequest(const std::map<string, boost::any> &config) : Darabonba::Model(config) {
+    fromMap(config);
+  };
+
+  void validate() override {}
+
+  map<string, boost::any> toMap() override {
+    map<string, boost::any> res;
+    if (deviceId) {
+      res["DeviceId"] = boost::any(*deviceId);
+    }
+    if (tenantId) {
+      res["TenantId"] = boost::any(*tenantId);
+    }
+    return res;
+  }
+
+  void fromMap(map<string, boost::any> m) override {
+    if (m.find("DeviceId") != m.end() && !m["DeviceId"].empty()) {
+      deviceId = make_shared<string>(boost::any_cast<string>(m["DeviceId"]));
+    }
+    if (m.find("TenantId") != m.end() && !m["TenantId"].empty()) {
+      tenantId = make_shared<long>(boost::any_cast<long>(m["TenantId"]));
+    }
+  }
+
+
+  virtual ~ClientUnbindDeviceRequest() = default;
+};
+class ClientUnbindDeviceResponseBody : public Darabonba::Model {
+public:
+  shared_ptr<string> code{};
+  shared_ptr<string> message{};
+  shared_ptr<string> requestId{};
+  shared_ptr<bool> success{};
+
+  ClientUnbindDeviceResponseBody() {}
+
+  explicit ClientUnbindDeviceResponseBody(const std::map<string, boost::any> &config) : Darabonba::Model(config) {
+    fromMap(config);
+  };
+
+  void validate() override {}
+
+  map<string, boost::any> toMap() override {
+    map<string, boost::any> res;
+    if (code) {
+      res["Code"] = boost::any(*code);
+    }
+    if (message) {
+      res["Message"] = boost::any(*message);
+    }
+    if (requestId) {
+      res["RequestId"] = boost::any(*requestId);
+    }
+    if (success) {
+      res["Success"] = boost::any(*success);
+    }
+    return res;
+  }
+
+  void fromMap(map<string, boost::any> m) override {
+    if (m.find("Code") != m.end() && !m["Code"].empty()) {
+      code = make_shared<string>(boost::any_cast<string>(m["Code"]));
+    }
+    if (m.find("Message") != m.end() && !m["Message"].empty()) {
+      message = make_shared<string>(boost::any_cast<string>(m["Message"]));
+    }
+    if (m.find("RequestId") != m.end() && !m["RequestId"].empty()) {
+      requestId = make_shared<string>(boost::any_cast<string>(m["RequestId"]));
+    }
+    if (m.find("Success") != m.end() && !m["Success"].empty()) {
+      success = make_shared<bool>(boost::any_cast<bool>(m["Success"]));
+    }
+  }
+
+
+  virtual ~ClientUnbindDeviceResponseBody() = default;
+};
+class ClientUnbindDeviceResponse : public Darabonba::Model {
+public:
+  shared_ptr<map<string, string>> headers{};
+  shared_ptr<long> statusCode{};
+  shared_ptr<ClientUnbindDeviceResponseBody> body{};
+
+  ClientUnbindDeviceResponse() {}
+
+  explicit ClientUnbindDeviceResponse(const std::map<string, boost::any> &config) : Darabonba::Model(config) {
+    fromMap(config);
+  };
+
+  void validate() override {
+    if (!headers) {
+      BOOST_THROW_EXCEPTION(boost::enable_error_info(std::runtime_error("headers is required.")));
+    }
+    if (!statusCode) {
+      BOOST_THROW_EXCEPTION(boost::enable_error_info(std::runtime_error("statusCode is required.")));
+    }
+    if (!body) {
+      BOOST_THROW_EXCEPTION(boost::enable_error_info(std::runtime_error("body is required.")));
+    }
+  }
+
+  map<string, boost::any> toMap() override {
+    map<string, boost::any> res;
+    if (headers) {
+      res["headers"] = boost::any(*headers);
+    }
+    if (statusCode) {
+      res["statusCode"] = boost::any(*statusCode);
+    }
+    if (body) {
+      res["body"] = body ? boost::any(body->toMap()) : boost::any(map<string,boost::any>({}));
+    }
+    return res;
+  }
+
+  void fromMap(map<string, boost::any> m) override {
+    if (m.find("headers") != m.end() && !m["headers"].empty()) {
+      map<string, string> map1 = boost::any_cast<map<string, string>>(m["headers"]);
+      map<string, string> toMap1;
+      for (auto item:map1) {
+         toMap1[item.first] = item.second;
+      }
+      headers = make_shared<map<string, string>>(toMap1);
+    }
+    if (m.find("statusCode") != m.end() && !m["statusCode"].empty()) {
+      statusCode = make_shared<long>(boost::any_cast<long>(m["statusCode"]));
+    }
+    if (m.find("body") != m.end() && !m["body"].empty()) {
+      if (typeid(map<string, boost::any>) == m["body"].type()) {
+        ClientUnbindDeviceResponseBody model1;
+        model1.fromMap(boost::any_cast<map<string, boost::any>>(m["body"]));
+        body = make_shared<ClientUnbindDeviceResponseBody>(model1);
+      }
+    }
+  }
+
+
+  virtual ~ClientUnbindDeviceResponse() = default;
+};
 class CloseTimedResetOperateRequest : public Darabonba::Model {
 public:
   shared_ptr<string> instanceId{};
@@ -9769,6 +9917,8 @@ public:
   ClientAuthResponse clientAuth(shared_ptr<ClientAuthRequest> request);
   ClientStartResponse clientStartWithOptions(shared_ptr<ClientStartRequest> request, shared_ptr<Darabonba_Util::RuntimeOptions> runtime);
   ClientStartResponse clientStart(shared_ptr<ClientStartRequest> request);
+  ClientUnbindDeviceResponse clientUnbindDeviceWithOptions(shared_ptr<ClientUnbindDeviceRequest> request, shared_ptr<Darabonba_Util::RuntimeOptions> runtime);
+  ClientUnbindDeviceResponse clientUnbindDevice(shared_ptr<ClientUnbindDeviceRequest> request);
   CloseTimedResetOperateResponse closeTimedResetOperateWithOptions(shared_ptr<CloseTimedResetOperateRequest> request, shared_ptr<Darabonba_Util::RuntimeOptions> runtime);
   CloseTimedResetOperateResponse closeTimedResetOperate(shared_ptr<CloseTimedResetOperateRequest> request);
   ConfirmAvatar2dTrainResponse confirmAvatar2dTrainWithOptions(shared_ptr<ConfirmAvatar2dTrainRequest> request, shared_ptr<Darabonba_Util::RuntimeOptions> runtime);
--- avatar-20220130/src/avatar_20220130.cpp
+++ avatar-20220130/src/avatar_20220130.cpp
@@ -151,6 +151,37 @@ ClientStartResponse Alibabacloud_Avatar20220130::Client::clientStart(shared_ptr<
   return clientStartWithOptions(request, runtime);
 }
 
+ClientUnbindDeviceResponse Alibabacloud_Avatar20220130::Client::clientUnbindDeviceWithOptions(shared_ptr<ClientUnbindDeviceRequest> request, shared_ptr<Darabonba_Util::RuntimeOptions> runtime) {
+  Darabonba_Util::Client::validateModel(request);
+  shared_ptr<map<string, boost::any>> query = make_shared<map<string, boost::any>>(map<string, boost::any>());
+  if (!Darabonba_Util::Client::isUnset<string>(request->deviceId)) {
+    query->insert(pair<string, string>("DeviceId", *request->deviceId));
+  }
+  if (!Darabonba_Util::Client::isUnset<long>(request->tenantId)) {
+    query->insert(pair<string, long>("TenantId", *request->tenantId));
+  }
+  shared_ptr<Alibabacloud_OpenApi::OpenApiRequest> req = make_shared<Alibabacloud_OpenApi::OpenApiRequest>(map<string, boost::any>({
+    {"query", boost::any(Alibabacloud_OpenApiUtil::Client::query(query))}
+  }));
+  shared_ptr<Alibabacloud_OpenApi::Params> params = make_shared<Alibabacloud_OpenApi::Params>(map<string, boost::any>({
+    {"action", boost::any(string("ClientUnbindDevice"))},
+    {"version", boost::any(string("2022-01-30"))},
+    {"protocol", boost::any(string("HTTPS"))},
+    {"pathname", boost::any(string("/"))},
+    {"method", boost::any(string("POST"))},
+    {"authType", boost::any(string("AK"))},
+    {"style", boost::any(string("RPC"))},
+    {"reqBodyType", boost::any(string("formData"))},
+    {"bodyType", boost::any(string("json"))}
+  }));
+  return ClientUnbindDeviceResponse(callApi(params, req, runtime));
+}
+
+ClientUnbindDeviceResponse Alibabacloud_Avatar20220130::Client::clientUnbindDevice(shared_ptr<ClientUnbindDeviceRequest> request) {
+  shared_ptr<Darabonba_Util::RuntimeOptions> runtime = make_shared<Darabonba_Util::RuntimeOptions>();
+  return clientUnbindDeviceWithOptions(request, runtime);
+}
+
 CloseTimedResetOperateResponse Alibabacloud_Avatar20220130::Client::closeTimedResetOperateWithOptions(shared_ptr<CloseTimedResetOperateRequest> request, shared_ptr<Darabonba_Util::RuntimeOptions> runtime) {
   Darabonba_Util::Client::validateModel(request);
   shared_ptr<map<string, boost::any>> query = make_shared<map<string, boost::any>>(map<string, boost::any>());
