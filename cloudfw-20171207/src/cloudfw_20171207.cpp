a52184a0b79d6d5f8a1d33d2f68f6d47adacf4a7
sdk-team <sdk-team@alibabacloud.com>
2024-01-17 17:10:20 +0000

    Generated cpp 2017-12-07 for Cloudfw.

--- cloudfw-20171207/src/cloudfw_20171207.cpp
+++ cloudfw-20171207/src/cloudfw_20171207.cpp
@@ -3741,6 +3741,34 @@ PutEnableFwSwitchResponse Alibabacloud_Cloudfw20171207::Client::putEnableFwSwitc
   return putEnableFwSwitchWithOptions(request, runtime);
 }
 
+ReleasePostInstanceResponse Alibabacloud_Cloudfw20171207::Client::releasePostInstanceWithOptions(shared_ptr<ReleasePostInstanceRequest> request, shared_ptr<Darabonba_Util::RuntimeOptions> runtime) {
+  Darabonba_Util::Client::validateModel(request);
+  shared_ptr<map<string, boost::any>> query = make_shared<map<string, boost::any>>(map<string, boost::any>());
+  if (!Darabonba_Util::Client::isUnset<string>(request->instanceId)) {
+    query->insert(pair<string, string>("InstanceId", *request->instanceId));
+  }
+  shared_ptr<Alibabacloud_OpenApi::OpenApiRequest> req = make_shared<Alibabacloud_OpenApi::OpenApiRequest>(map<string, boost::any>({
+    {"query", boost::any(Alibabacloud_OpenApiUtil::Client::query(query))}
+  }));
+  shared_ptr<Alibabacloud_OpenApi::Params> params = make_shared<Alibabacloud_OpenApi::Params>(map<string, boost::any>({
+    {"action", boost::any(string("ReleasePostInstance"))},
+    {"version", boost::any(string("2017-12-07"))},
+    {"protocol", boost::any(string("HTTPS"))},
+    {"pathname", boost::any(string("/"))},
+    {"method", boost::any(string("POST"))},
+    {"authType", boost::any(string("AK"))},
+    {"style", boost::any(string("RPC"))},
+    {"reqBodyType", boost::any(string("formData"))},
+    {"bodyType", boost::any(string("json"))}
+  }));
+  return ReleasePostInstanceResponse(callApi(params, req, runtime));
+}
+
+ReleasePostInstanceResponse Alibabacloud_Cloudfw20171207::Client::releasePostInstance(shared_ptr<ReleasePostInstanceRequest> request) {
+  shared_ptr<Darabonba_Util::RuntimeOptions> runtime = make_shared<Darabonba_Util::RuntimeOptions>();
+  return releasePostInstanceWithOptions(request, runtime);
+}
+
 ResetVpcFirewallRuleHitCountResponse Alibabacloud_Cloudfw20171207::Client::resetVpcFirewallRuleHitCountWithOptions(shared_ptr<ResetVpcFirewallRuleHitCountRequest> request, shared_ptr<Darabonba_Util::RuntimeOptions> runtime) {
   Darabonba_Util::Client::validateModel(request);
   shared_ptr<map<string, boost::any>> query = make_shared<map<string, boost::any>>(map<string, boost::any>());
