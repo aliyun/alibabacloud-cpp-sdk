0101f84c35c8d16b6ea8a4fc9ecdf9e2b5ed301e
sdk-team <sdk-team@alibabacloud.com>
2024-02-22 02:37:39 +0000

    Support API DisconnectDesktopSessions.

--- ecd-20200930/include/alibabacloud/ecd_20200930.hpp
+++ ecd-20200930/include/alibabacloud/ecd_20200930.hpp
@@ -28583,6 +28583,237 @@ public:
 
   virtual ~DisableDesktopsInGroupResponse() = default;
 };
+class DisconnectDesktopSessionsRequestSessions : public Darabonba::Model {
+public:
+  shared_ptr<string> desktopId{};
+  shared_ptr<string> endUserId{};
+
+  DisconnectDesktopSessionsRequestSessions() {}
+
+  explicit DisconnectDesktopSessionsRequestSessions(const std::map<string, boost::any> &config) : Darabonba::Model(config) {
+    fromMap(config);
+  };
+
+  void validate() override {}
+
+  map<string, boost::any> toMap() override {
+    map<string, boost::any> res;
+    if (desktopId) {
+      res["DesktopId"] = boost::any(*desktopId);
+    }
+    if (endUserId) {
+      res["EndUserId"] = boost::any(*endUserId);
+    }
+    return res;
+  }
+
+  void fromMap(map<string, boost::any> m) override {
+    if (m.find("DesktopId") != m.end() && !m["DesktopId"].empty()) {
+      desktopId = make_shared<string>(boost::any_cast<string>(m["DesktopId"]));
+    }
+    if (m.find("EndUserId") != m.end() && !m["EndUserId"].empty()) {
+      endUserId = make_shared<string>(boost::any_cast<string>(m["EndUserId"]));
+    }
+  }
+
+
+  virtual ~DisconnectDesktopSessionsRequestSessions() = default;
+};
+class DisconnectDesktopSessionsRequest : public Darabonba::Model {
+public:
+  shared_ptr<bool> preCheck{};
+  shared_ptr<string> regionId{};
+  shared_ptr<vector<DisconnectDesktopSessionsRequestSessions>> sessions{};
+
+  DisconnectDesktopSessionsRequest() {}
+
+  explicit DisconnectDesktopSessionsRequest(const std::map<string, boost::any> &config) : Darabonba::Model(config) {
+    fromMap(config);
+  };
+
+  void validate() override {}
+
+  map<string, boost::any> toMap() override {
+    map<string, boost::any> res;
+    if (preCheck) {
+      res["PreCheck"] = boost::any(*preCheck);
+    }
+    if (regionId) {
+      res["RegionId"] = boost::any(*regionId);
+    }
+    if (sessions) {
+      vector<boost::any> temp1;
+      for(auto item1:*sessions){
+        temp1.push_back(boost::any(item1.toMap()));
+      }
+      res["Sessions"] = boost::any(temp1);
+    }
+    return res;
+  }
+
+  void fromMap(map<string, boost::any> m) override {
+    if (m.find("PreCheck") != m.end() && !m["PreCheck"].empty()) {
+      preCheck = make_shared<bool>(boost::any_cast<bool>(m["PreCheck"]));
+    }
+    if (m.find("RegionId") != m.end() && !m["RegionId"].empty()) {
+      regionId = make_shared<string>(boost::any_cast<string>(m["RegionId"]));
+    }
+    if (m.find("Sessions") != m.end() && !m["Sessions"].empty()) {
+      if (typeid(vector<boost::any>) == m["Sessions"].type()) {
+        vector<DisconnectDesktopSessionsRequestSessions> expect1;
+        for(auto item1:boost::any_cast<vector<boost::any>>(m["Sessions"])){
+          if (typeid(map<string, boost::any>) == item1.type()) {
+            DisconnectDesktopSessionsRequestSessions model2;
+            model2.fromMap(boost::any_cast<map<string, boost::any>>(item1));
+            expect1.push_back(model2);
+          }
+        }
+        sessions = make_shared<vector<DisconnectDesktopSessionsRequestSessions>>(expect1);
+      }
+    }
+  }
+
+
+  virtual ~DisconnectDesktopSessionsRequest() = default;
+};
+class DisconnectDesktopSessionsResponseBodyInvalidSessions : public Darabonba::Model {
+public:
+  shared_ptr<string> desktopId{};
+  shared_ptr<string> endUserId{};
+
+  DisconnectDesktopSessionsResponseBodyInvalidSessions() {}
+
+  explicit DisconnectDesktopSessionsResponseBodyInvalidSessions(const std::map<string, boost::any> &config) : Darabonba::Model(config) {
+    fromMap(config);
+  };
+
+  void validate() override {}
+
+  map<string, boost::any> toMap() override {
+    map<string, boost::any> res;
+    if (desktopId) {
+      res["DesktopId"] = boost::any(*desktopId);
+    }
+    if (endUserId) {
+      res["EndUserId"] = boost::any(*endUserId);
+    }
+    return res;
+  }
+
+  void fromMap(map<string, boost::any> m) override {
+    if (m.find("DesktopId") != m.end() && !m["DesktopId"].empty()) {
+      desktopId = make_shared<string>(boost::any_cast<string>(m["DesktopId"]));
+    }
+    if (m.find("EndUserId") != m.end() && !m["EndUserId"].empty()) {
+      endUserId = make_shared<string>(boost::any_cast<string>(m["EndUserId"]));
+    }
+  }
+
+
+  virtual ~DisconnectDesktopSessionsResponseBodyInvalidSessions() = default;
+};
+class DisconnectDesktopSessionsResponseBody : public Darabonba::Model {
+public:
+  shared_ptr<vector<DisconnectDesktopSessionsResponseBodyInvalidSessions>> invalidSessions{};
+  shared_ptr<string> requestId{};
+
+  DisconnectDesktopSessionsResponseBody() {}
+
+  explicit DisconnectDesktopSessionsResponseBody(const std::map<string, boost::any> &config) : Darabonba::Model(config) {
+    fromMap(config);
+  };
+
+  void validate() override {}
+
+  map<string, boost::any> toMap() override {
+    map<string, boost::any> res;
+    if (invalidSessions) {
+      vector<boost::any> temp1;
+      for(auto item1:*invalidSessions){
+        temp1.push_back(boost::any(item1.toMap()));
+      }
+      res["InvalidSessions"] = boost::any(temp1);
+    }
+    if (requestId) {
+      res["RequestId"] = boost::any(*requestId);
+    }
+    return res;
+  }
+
+  void fromMap(map<string, boost::any> m) override {
+    if (m.find("InvalidSessions") != m.end() && !m["InvalidSessions"].empty()) {
+      if (typeid(vector<boost::any>) == m["InvalidSessions"].type()) {
+        vector<DisconnectDesktopSessionsResponseBodyInvalidSessions> expect1;
+        for(auto item1:boost::any_cast<vector<boost::any>>(m["InvalidSessions"])){
+          if (typeid(map<string, boost::any>) == item1.type()) {
+            DisconnectDesktopSessionsResponseBodyInvalidSessions model2;
+            model2.fromMap(boost::any_cast<map<string, boost::any>>(item1));
+            expect1.push_back(model2);
+          }
+        }
+        invalidSessions = make_shared<vector<DisconnectDesktopSessionsResponseBodyInvalidSessions>>(expect1);
+      }
+    }
+    if (m.find("RequestId") != m.end() && !m["RequestId"].empty()) {
+      requestId = make_shared<string>(boost::any_cast<string>(m["RequestId"]));
+    }
+  }
+
+
+  virtual ~DisconnectDesktopSessionsResponseBody() = default;
+};
+class DisconnectDesktopSessionsResponse : public Darabonba::Model {
+public:
+  shared_ptr<map<string, string>> headers{};
+  shared_ptr<long> statusCode{};
+  shared_ptr<DisconnectDesktopSessionsResponseBody> body{};
+
+  DisconnectDesktopSessionsResponse() {}
+
+  explicit DisconnectDesktopSessionsResponse(const std::map<string, boost::any> &config) : Darabonba::Model(config) {
+    fromMap(config);
+  };
+
+  void validate() override {}
+
+  map<string, boost::any> toMap() override {
+    map<string, boost::any> res;
+    if (headers) {
+      res["headers"] = boost::any(*headers);
+    }
+    if (statusCode) {
+      res["statusCode"] = boost::any(*statusCode);
+    }
+    if (body) {
+      res["body"] = body ? boost::any(body->toMap()) : boost::any(map<string,boost::any>({}));
+    }
+    return res;
+  }
+
+  void fromMap(map<string, boost::any> m) override {
+    if (m.find("headers") != m.end() && !m["headers"].empty()) {
+      map<string, string> map1 = boost::any_cast<map<string, string>>(m["headers"]);
+      map<string, string> toMap1;
+      for (auto item:map1) {
+         toMap1[item.first] = item.second;
+      }
+      headers = make_shared<map<string, string>>(toMap1);
+    }
+    if (m.find("statusCode") != m.end() && !m["statusCode"].empty()) {
+      statusCode = make_shared<long>(boost::any_cast<long>(m["statusCode"]));
+    }
+    if (m.find("body") != m.end() && !m["body"].empty()) {
+      if (typeid(map<string, boost::any>) == m["body"].type()) {
+        DisconnectDesktopSessionsResponseBody model1;
+        model1.fromMap(boost::any_cast<map<string, boost::any>>(m["body"]));
+        body = make_shared<DisconnectDesktopSessionsResponseBody>(model1);
+      }
+    }
+  }
+
+
+  virtual ~DisconnectDesktopSessionsResponse() = default;
+};
 class DissociateNetworkPackageRequest : public Darabonba::Model {
 public:
   shared_ptr<string> networkPackageId{};
@@ -45039,6 +45270,8 @@ public:
   DetachEndUserResponse detachEndUser(shared_ptr<DetachEndUserRequest> request);
   DisableDesktopsInGroupResponse disableDesktopsInGroupWithOptions(shared_ptr<DisableDesktopsInGroupRequest> request, shared_ptr<Darabonba_Util::RuntimeOptions> runtime);
   DisableDesktopsInGroupResponse disableDesktopsInGroup(shared_ptr<DisableDesktopsInGroupRequest> request);
+  DisconnectDesktopSessionsResponse disconnectDesktopSessionsWithOptions(shared_ptr<DisconnectDesktopSessionsRequest> request, shared_ptr<Darabonba_Util::RuntimeOptions> runtime);
+  DisconnectDesktopSessionsResponse disconnectDesktopSessions(shared_ptr<DisconnectDesktopSessionsRequest> request);
   DissociateNetworkPackageResponse dissociateNetworkPackageWithOptions(shared_ptr<DissociateNetworkPackageRequest> request, shared_ptr<Darabonba_Util::RuntimeOptions> runtime);
   DissociateNetworkPackageResponse dissociateNetworkPackage(shared_ptr<DissociateNetworkPackageRequest> request);
   ExportClientEventsResponse exportClientEventsWithOptions(shared_ptr<ExportClientEventsRequest> request, shared_ptr<Darabonba_Util::RuntimeOptions> runtime);
--- ecd-20200930/src/ecd_20200930.cpp
+++ ecd-20200930/src/ecd_20200930.cpp
@@ -16,6 +16,7 @@ using namespace std;
 using namespace Alibabacloud_Ecd20200930;
 
 Alibabacloud_Ecd20200930::Client::Client(const shared_ptr<Alibabacloud_OpenApi::Config>& config) : Alibabacloud_OpenApi::Client(config) {
+  _signatureAlgorithm = make_shared<string>("v2");
   _endpointRule = make_shared<string>("regional");
   checkConfig(config);
   _endpoint = make_shared<string>(getEndpoint(make_shared<string>("ecd"), _regionId, _endpointRule, _network, _suffix, _endpointMap, _endpoint));
@@ -5213,6 +5214,40 @@ DisableDesktopsInGroupResponse Alibabacloud_Ecd20200930::Client::disableDesktops
 DisableDesktopsInGroupResponse Alibabacloud_Ecd20200930::Client::disableDesktopsInGroup(shared_ptr<DisableDesktopsInGroupRequest> request) {
   shared_ptr<Darabonba_Util::RuntimeOptions> runtime = make_shared<Darabonba_Util::RuntimeOptions>();
   return disableDesktopsInGroupWithOptions(request, runtime);
+}
+
+DisconnectDesktopSessionsResponse Alibabacloud_Ecd20200930::Client::disconnectDesktopSessionsWithOptions(shared_ptr<DisconnectDesktopSessionsRequest> request, shared_ptr<Darabonba_Util::RuntimeOptions> runtime) {
+  Darabonba_Util::Client::validateModel(request);
+  shared_ptr<map<string, boost::any>> query = make_shared<map<string, boost::any>>(map<string, boost::any>());
+  if (!Darabonba_Util::Client::isUnset<bool>(request->preCheck)) {
+    query->insert(pair<string, bool>("PreCheck", *request->preCheck));
+  }
+  if (!Darabonba_Util::Client::isUnset<string>(request->regionId)) {
+    query->insert(pair<string, string>("RegionId", *request->regionId));
+  }
+  if (!Darabonba_Util::Client::isUnset<vector<DisconnectDesktopSessionsRequestSessions>>(request->sessions)) {
+    query->insert(pair<string, vector<DisconnectDesktopSessionsRequestSessions>>("Sessions", *request->sessions));
+  }
+  shared_ptr<Alibabacloud_OpenApi::OpenApiRequest> req = make_shared<Alibabacloud_OpenApi::OpenApiRequest>(map<string, boost::any>({
+    {"query", boost::any(Alibabacloud_OpenApiUtil::Client::query(query))}
+  }));
+  shared_ptr<Alibabacloud_OpenApi::Params> params = make_shared<Alibabacloud_OpenApi::Params>(map<string, boost::any>({
+    {"action", boost::any(string("DisconnectDesktopSessions"))},
+    {"version", boost::any(string("2020-09-30"))},
+    {"protocol", boost::any(string("HTTPS"))},
+    {"pathname", boost::any(string("/"))},
+    {"method", boost::any(string("POST"))},
+    {"authType", boost::any(string("AK"))},
+    {"style", boost::any(string("RPC"))},
+    {"reqBodyType", boost::any(string("formData"))},
+    {"bodyType", boost::any(string("json"))}
+  }));
+  return DisconnectDesktopSessionsResponse(callApi(params, req, runtime));
+}
+
+DisconnectDesktopSessionsResponse Alibabacloud_Ecd20200930::Client::disconnectDesktopSessions(shared_ptr<DisconnectDesktopSessionsRequest> request) {
+  shared_ptr<Darabonba_Util::RuntimeOptions> runtime = make_shared<Darabonba_Util::RuntimeOptions>();
+  return disconnectDesktopSessionsWithOptions(request, runtime);
 }
 
 DissociateNetworkPackageResponse Alibabacloud_Ecd20200930::Client::dissociateNetworkPackageWithOptions(shared_ptr<DissociateNetworkPackageRequest> request, shared_ptr<Darabonba_Util::RuntimeOptions> runtime) {
