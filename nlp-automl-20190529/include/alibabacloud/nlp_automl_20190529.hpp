5a8c2696801cff6bf2610e732848dee038e4ed89
sdk-team <sdk-team@alibabacloud.com>
2024-07-04 17:09:55 +0000

    Generated cpp 2019-05-29 for nlp-automl.

---
+++ nlp-automl-20190529/include/alibabacloud/nlp_automl_20190529.hpp
@@ -0,0 +1,177 @@
+// This file is auto-generated, don't edit it. Thanks.
+
+#ifndef ALIBABACLOUD_NLP-AUTOML20190529_H_
+#define ALIBABACLOUD_NLP-AUTOML20190529_H_
+
+#include <alibabacloud/open_api.hpp>
+#include <darabonba/core.hpp>
+#include <darabonba/util.hpp>
+#include <iostream>
+#include <map>
+
+using namespace std;
+
+namespace Alibabacloud_Nlp-automl20190529 {
+class PredictModelRequest : public Darabonba::Model {
+public:
+  shared_ptr<string> content{};
+  shared_ptr<long> modelId{};
+
+  PredictModelRequest() {}
+
+  explicit PredictModelRequest(const std::map<string, boost::any> &config) : Darabonba::Model(config) {
+    fromMap(config);
+  };
+
+  void validate() override {}
+
+  map<string, boost::any> toMap() override {
+    map<string, boost::any> res;
+    if (content) {
+      res["Content"] = boost::any(*content);
+    }
+    if (modelId) {
+      res["ModelId"] = boost::any(*modelId);
+    }
+    return res;
+  }
+
+  void fromMap(map<string, boost::any> m) override {
+    if (m.find("Content") != m.end() && !m["Content"].empty()) {
+      content = make_shared<string>(boost::any_cast<string>(m["Content"]));
+    }
+    if (m.find("ModelId") != m.end() && !m["ModelId"].empty()) {
+      modelId = make_shared<long>(boost::any_cast<long>(m["ModelId"]));
+    }
+  }
+
+
+  virtual ~PredictModelRequest() = default;
+};
+class PredictModelResponseBody : public Darabonba::Model {
+public:
+  shared_ptr<string> code{};
+  shared_ptr<string> data{};
+  shared_ptr<string> message{};
+  shared_ptr<string> requestId{};
+  shared_ptr<string> success{};
+
+  PredictModelResponseBody() {}
+
+  explicit PredictModelResponseBody(const std::map<string, boost::any> &config) : Darabonba::Model(config) {
+    fromMap(config);
+  };
+
+  void validate() override {}
+
+  map<string, boost::any> toMap() override {
+    map<string, boost::any> res;
+    if (code) {
+      res["Code"] = boost::any(*code);
+    }
+    if (data) {
+      res["Data"] = boost::any(*data);
+    }
+    if (message) {
+      res["Message"] = boost::any(*message);
+    }
+    if (requestId) {
+      res["RequestId"] = boost::any(*requestId);
+    }
+    if (success) {
+      res["Success"] = boost::any(*success);
+    }
+    return res;
+  }
+
+  void fromMap(map<string, boost::any> m) override {
+    if (m.find("Code") != m.end() && !m["Code"].empty()) {
+      code = make_shared<string>(boost::any_cast<string>(m["Code"]));
+    }
+    if (m.find("Data") != m.end() && !m["Data"].empty()) {
+      data = make_shared<string>(boost::any_cast<string>(m["Data"]));
+    }
+    if (m.find("Message") != m.end() && !m["Message"].empty()) {
+      message = make_shared<string>(boost::any_cast<string>(m["Message"]));
+    }
+    if (m.find("RequestId") != m.end() && !m["RequestId"].empty()) {
+      requestId = make_shared<string>(boost::any_cast<string>(m["RequestId"]));
+    }
+    if (m.find("Success") != m.end() && !m["Success"].empty()) {
+      success = make_shared<string>(boost::any_cast<string>(m["Success"]));
+    }
+  }
+
+
+  virtual ~PredictModelResponseBody() = default;
+};
+class PredictModelResponse : public Darabonba::Model {
+public:
+  shared_ptr<map<string, string>> headers{};
+  shared_ptr<long> statusCode{};
+  shared_ptr<PredictModelResponseBody> body{};
+
+  PredictModelResponse() {}
+
+  explicit PredictModelResponse(const std::map<string, boost::any> &config) : Darabonba::Model(config) {
+    fromMap(config);
+  };
+
+  void validate() override {}
+
+  map<string, boost::any> toMap() override {
+    map<string, boost::any> res;
+    if (headers) {
+      res["headers"] = boost::any(*headers);
+    }
+    if (statusCode) {
+      res["statusCode"] = boost::any(*statusCode);
+    }
+    if (body) {
+      res["body"] = body ? boost::any(body->toMap()) : boost::any(map<string,boost::any>({}));
+    }
+    return res;
+  }
+
+  void fromMap(map<string, boost::any> m) override {
+    if (m.find("headers") != m.end() && !m["headers"].empty()) {
+      map<string, string> map1 = boost::any_cast<map<string, string>>(m["headers"]);
+      map<string, string> toMap1;
+      for (auto item:map1) {
+         toMap1[item.first] = item.second;
+      }
+      headers = make_shared<map<string, string>>(toMap1);
+    }
+    if (m.find("statusCode") != m.end() && !m["statusCode"].empty()) {
+      statusCode = make_shared<long>(boost::any_cast<long>(m["statusCode"]));
+    }
+    if (m.find("body") != m.end() && !m["body"].empty()) {
+      if (typeid(map<string, boost::any>) == m["body"].type()) {
+        PredictModelResponseBody model1;
+        model1.fromMap(boost::any_cast<map<string, boost::any>>(m["body"]));
+        body = make_shared<PredictModelResponseBody>(model1);
+      }
+    }
+  }
+
+
+  virtual ~PredictModelResponse() = default;
+};
+class Client : Alibabacloud_OpenApi::Client {
+public:
+  explicit Client(const shared_ptr<Alibabacloud_OpenApi::Config>& config);
+  string getEndpoint(shared_ptr<string> productId,
+                     shared_ptr<string> regionId,
+                     shared_ptr<string> endpointRule,
+                     shared_ptr<string> network,
+                     shared_ptr<string> suffix,
+                     shared_ptr<map<string, string>> endpointMap,
+                     shared_ptr<string> endpoint);
+  PredictModelResponse predictModelWithOptions(shared_ptr<PredictModelRequest> request, shared_ptr<map<string, string>> headers, shared_ptr<Darabonba_Util::RuntimeOptions> runtime);
+  PredictModelResponse predictModel(shared_ptr<PredictModelRequest> request);
+
+  virtual ~Client() = default;
+};
+} // namespace Alibabacloud_Nlp-automl20190529
+
+#endif
---
+++ nlp-automl-20190529/src/nlp_automl_20190529.cpp
@@ -0,0 +1,73 @@
+// This file is auto-generated, don't edit it. Thanks.
+
+#include <alibabacloud/nlp_automl_20190529.hpp>
+#include <alibabacloud/endpoint_util.hpp>
+#include <alibabacloud/open_api.hpp>
+#include <alibabacloud/open_api_util.hpp>
+#include <boost/any.hpp>
+#include <darabonba/core.hpp>
+#include <darabonba/util.hpp>
+#include <iostream>
+#include <map>
+
+using namespace std;
+
+using namespace Alibabacloud_Nlp-automl20190529;
+
+Alibabacloud_Nlp-automl20190529::Client::Client(const shared_ptr<Alibabacloud_OpenApi::Config>& config) : Alibabacloud_OpenApi::Client(config) {
+  _endpointRule = make_shared<string>("regional");
+  checkConfig(config);
+  _endpoint = make_shared<string>(getEndpoint(make_shared<string>("nlp-automl"), _regionId, _endpointRule, _network, _suffix, _endpointMap, _endpoint));
+};
+
+string Alibabacloud_Nlp-automl20190529::Client::getEndpoint(shared_ptr<string> productId,
+                                                            shared_ptr<string> regionId,
+                                                            shared_ptr<string> endpointRule,
+                                                            shared_ptr<string> network,
+                                                            shared_ptr<string> suffix,
+                                                            shared_ptr<map<string, string>> endpointMap,
+                                                            shared_ptr<string> endpoint) {
+  if (!Darabonba_Util::Client::empty(endpoint)) {
+    return *endpoint;
+  }
+  if (!Darabonba_Util::Client::isUnset<map<string, string>>(endpointMap) && !Darabonba_Util::Client::empty(make_shared<string>((*endpointMap)[regionId]))) {
+    return (*endpointMap)[regionId];
+  }
+  return Alibabacloud_EndpointUtil::Client::getEndpointRules(productId, regionId, endpointRule, network, suffix);
+}
+
+PredictModelResponse Alibabacloud_Nlp-automl20190529::Client::predictModelWithOptions(shared_ptr<PredictModelRequest> request, shared_ptr<map<string, string>> headers, shared_ptr<Darabonba_Util::RuntimeOptions> runtime) {
+  Darabonba_Util::Client::validateModel(request);
+  shared_ptr<map<string, boost::any>> query = make_shared<map<string, boost::any>>(map<string, boost::any>());
+  if (!Darabonba_Util::Client::isUnset<long>(request->modelId)) {
+    query->insert(pair<string, long>("ModelId", *request->modelId));
+  }
+  shared_ptr<map<string, boost::any>> body = make_shared<map<string, boost::any>>(map<string, boost::any>());
+  if (!Darabonba_Util::Client::isUnset<string>(request->content)) {
+    body->insert(pair<string, string>("Content", *request->content));
+  }
+  shared_ptr<Alibabacloud_OpenApi::OpenApiRequest> req = make_shared<Alibabacloud_OpenApi::OpenApiRequest>(map<string, boost::any>({
+    {"headers", !headers ? boost::any() : boost::any(*headers)},
+    {"query", boost::any(Alibabacloud_OpenApiUtil::Client::query(query))},
+    {"body", boost::any(Alibabacloud_OpenApiUtil::Client::parseToMap(body))}
+  }));
+  shared_ptr<Alibabacloud_OpenApi::Params> params = make_shared<Alibabacloud_OpenApi::Params>(map<string, boost::any>({
+    {"action", boost::any(string("PredictModel"))},
+    {"version", boost::any(string("2019-05-29"))},
+    {"protocol", boost::any(string("HTTPS"))},
+    {"pathname", boost::any(string("/api/automl/predict"))},
+    {"method", boost::any(string("POST"))},
+    {"authType", boost::any(string("AK"))},
+    {"style", boost::any(string("ROA"))},
+    {"reqBodyType", boost::any(string("formData"))},
+    {"bodyType", boost::any(string("json"))}
+  }));
+  return PredictModelResponse(callApi(params, req, runtime));
+}
+
+PredictModelResponse Alibabacloud_Nlp-automl20190529::Client::predictModel(shared_ptr<PredictModelRequest> request) {
+  shared_ptr<Darabonba_Util::RuntimeOptions> runtime = make_shared<Darabonba_Util::RuntimeOptions>();
+  shared_ptr<map<string, string>> headers = make_shared<map<string, string>>(map<string, string>());
+  return predictModelWithOptions(request, headers, runtime);
+}
+
