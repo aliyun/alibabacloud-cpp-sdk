6473aad3cbe6b71170f0baa3f50df63cf2beb55f
sdk-team <sdk-team@alibabacloud.com>
2024-08-14 01:56:56 +0000

    Support API DescribeMobileOperatorAttribute.

--- dytnsapi-20200217/include/alibabacloud/dytnsapi_20200217.hpp
+++ dytnsapi-20200217/include/alibabacloud/dytnsapi_20200217.hpp
@@ -1488,6 +1488,254 @@ public:
 
   virtual ~DescribeEmptyNumberResponse() = default;
 };
+class DescribeMobileOperatorAttributeRequest : public Darabonba::Model {
+public:
+  shared_ptr<string> authCode{};
+  shared_ptr<string> inputNumber{};
+  shared_ptr<string> mask{};
+  shared_ptr<long> ownerId{};
+  shared_ptr<string> resourceOwnerAccount{};
+  shared_ptr<long> resourceOwnerId{};
+
+  DescribeMobileOperatorAttributeRequest() {}
+
+  explicit DescribeMobileOperatorAttributeRequest(const std::map<string, boost::any> &config) : Darabonba::Model(config) {
+    fromMap(config);
+  };
+
+  void validate() override {}
+
+  map<string, boost::any> toMap() override {
+    map<string, boost::any> res;
+    if (authCode) {
+      res["AuthCode"] = boost::any(*authCode);
+    }
+    if (inputNumber) {
+      res["InputNumber"] = boost::any(*inputNumber);
+    }
+    if (mask) {
+      res["Mask"] = boost::any(*mask);
+    }
+    if (ownerId) {
+      res["OwnerId"] = boost::any(*ownerId);
+    }
+    if (resourceOwnerAccount) {
+      res["ResourceOwnerAccount"] = boost::any(*resourceOwnerAccount);
+    }
+    if (resourceOwnerId) {
+      res["ResourceOwnerId"] = boost::any(*resourceOwnerId);
+    }
+    return res;
+  }
+
+  void fromMap(map<string, boost::any> m) override {
+    if (m.find("AuthCode") != m.end() && !m["AuthCode"].empty()) {
+      authCode = make_shared<string>(boost::any_cast<string>(m["AuthCode"]));
+    }
+    if (m.find("InputNumber") != m.end() && !m["InputNumber"].empty()) {
+      inputNumber = make_shared<string>(boost::any_cast<string>(m["InputNumber"]));
+    }
+    if (m.find("Mask") != m.end() && !m["Mask"].empty()) {
+      mask = make_shared<string>(boost::any_cast<string>(m["Mask"]));
+    }
+    if (m.find("OwnerId") != m.end() && !m["OwnerId"].empty()) {
+      ownerId = make_shared<long>(boost::any_cast<long>(m["OwnerId"]));
+    }
+    if (m.find("ResourceOwnerAccount") != m.end() && !m["ResourceOwnerAccount"].empty()) {
+      resourceOwnerAccount = make_shared<string>(boost::any_cast<string>(m["ResourceOwnerAccount"]));
+    }
+    if (m.find("ResourceOwnerId") != m.end() && !m["ResourceOwnerId"].empty()) {
+      resourceOwnerId = make_shared<long>(boost::any_cast<long>(m["ResourceOwnerId"]));
+    }
+  }
+
+
+  virtual ~DescribeMobileOperatorAttributeRequest() = default;
+};
+class DescribeMobileOperatorAttributeResponseBodyData : public Darabonba::Model {
+public:
+  shared_ptr<string> basicCarrier{};
+  shared_ptr<string> carrier{};
+  shared_ptr<string> city{};
+  shared_ptr<bool> isNumberPortability{};
+  shared_ptr<string> province{};
+  shared_ptr<string> realNumber{};
+  shared_ptr<string> segmentCarrier{};
+
+  DescribeMobileOperatorAttributeResponseBodyData() {}
+
+  explicit DescribeMobileOperatorAttributeResponseBodyData(const std::map<string, boost::any> &config) : Darabonba::Model(config) {
+    fromMap(config);
+  };
+
+  void validate() override {}
+
+  map<string, boost::any> toMap() override {
+    map<string, boost::any> res;
+    if (basicCarrier) {
+      res["BasicCarrier"] = boost::any(*basicCarrier);
+    }
+    if (carrier) {
+      res["Carrier"] = boost::any(*carrier);
+    }
+    if (city) {
+      res["City"] = boost::any(*city);
+    }
+    if (isNumberPortability) {
+      res["IsNumberPortability"] = boost::any(*isNumberPortability);
+    }
+    if (province) {
+      res["Province"] = boost::any(*province);
+    }
+    if (realNumber) {
+      res["RealNumber"] = boost::any(*realNumber);
+    }
+    if (segmentCarrier) {
+      res["SegmentCarrier"] = boost::any(*segmentCarrier);
+    }
+    return res;
+  }
+
+  void fromMap(map<string, boost::any> m) override {
+    if (m.find("BasicCarrier") != m.end() && !m["BasicCarrier"].empty()) {
+      basicCarrier = make_shared<string>(boost::any_cast<string>(m["BasicCarrier"]));
+    }
+    if (m.find("Carrier") != m.end() && !m["Carrier"].empty()) {
+      carrier = make_shared<string>(boost::any_cast<string>(m["Carrier"]));
+    }
+    if (m.find("City") != m.end() && !m["City"].empty()) {
+      city = make_shared<string>(boost::any_cast<string>(m["City"]));
+    }
+    if (m.find("IsNumberPortability") != m.end() && !m["IsNumberPortability"].empty()) {
+      isNumberPortability = make_shared<bool>(boost::any_cast<bool>(m["IsNumberPortability"]));
+    }
+    if (m.find("Province") != m.end() && !m["Province"].empty()) {
+      province = make_shared<string>(boost::any_cast<string>(m["Province"]));
+    }
+    if (m.find("RealNumber") != m.end() && !m["RealNumber"].empty()) {
+      realNumber = make_shared<string>(boost::any_cast<string>(m["RealNumber"]));
+    }
+    if (m.find("SegmentCarrier") != m.end() && !m["SegmentCarrier"].empty()) {
+      segmentCarrier = make_shared<string>(boost::any_cast<string>(m["SegmentCarrier"]));
+    }
+  }
+
+
+  virtual ~DescribeMobileOperatorAttributeResponseBodyData() = default;
+};
+class DescribeMobileOperatorAttributeResponseBody : public Darabonba::Model {
+public:
+  shared_ptr<string> accessDeniedDetail{};
+  shared_ptr<string> code{};
+  shared_ptr<DescribeMobileOperatorAttributeResponseBodyData> data{};
+  shared_ptr<string> message{};
+  shared_ptr<string> requestId{};
+
+  DescribeMobileOperatorAttributeResponseBody() {}
+
+  explicit DescribeMobileOperatorAttributeResponseBody(const std::map<string, boost::any> &config) : Darabonba::Model(config) {
+    fromMap(config);
+  };
+
+  void validate() override {}
+
+  map<string, boost::any> toMap() override {
+    map<string, boost::any> res;
+    if (accessDeniedDetail) {
+      res["AccessDeniedDetail"] = boost::any(*accessDeniedDetail);
+    }
+    if (code) {
+      res["Code"] = boost::any(*code);
+    }
+    if (data) {
+      res["Data"] = data ? boost::any(data->toMap()) : boost::any(map<string,boost::any>({}));
+    }
+    if (message) {
+      res["Message"] = boost::any(*message);
+    }
+    if (requestId) {
+      res["RequestId"] = boost::any(*requestId);
+    }
+    return res;
+  }
+
+  void fromMap(map<string, boost::any> m) override {
+    if (m.find("AccessDeniedDetail") != m.end() && !m["AccessDeniedDetail"].empty()) {
+      accessDeniedDetail = make_shared<string>(boost::any_cast<string>(m["AccessDeniedDetail"]));
+    }
+    if (m.find("Code") != m.end() && !m["Code"].empty()) {
+      code = make_shared<string>(boost::any_cast<string>(m["Code"]));
+    }
+    if (m.find("Data") != m.end() && !m["Data"].empty()) {
+      if (typeid(map<string, boost::any>) == m["Data"].type()) {
+        DescribeMobileOperatorAttributeResponseBodyData model1;
+        model1.fromMap(boost::any_cast<map<string, boost::any>>(m["Data"]));
+        data = make_shared<DescribeMobileOperatorAttributeResponseBodyData>(model1);
+      }
+    }
+    if (m.find("Message") != m.end() && !m["Message"].empty()) {
+      message = make_shared<string>(boost::any_cast<string>(m["Message"]));
+    }
+    if (m.find("RequestId") != m.end() && !m["RequestId"].empty()) {
+      requestId = make_shared<string>(boost::any_cast<string>(m["RequestId"]));
+    }
+  }
+
+
+  virtual ~DescribeMobileOperatorAttributeResponseBody() = default;
+};
+class DescribeMobileOperatorAttributeResponse : public Darabonba::Model {
+public:
+  shared_ptr<map<string, string>> headers{};
+  shared_ptr<long> statusCode{};
+  shared_ptr<DescribeMobileOperatorAttributeResponseBody> body{};
+
+  DescribeMobileOperatorAttributeResponse() {}
+
+  explicit DescribeMobileOperatorAttributeResponse(const std::map<string, boost::any> &config) : Darabonba::Model(config) {
+    fromMap(config);
+  };
+
+  void validate() override {}
+
+  map<string, boost::any> toMap() override {
+    map<string, boost::any> res;
+    if (headers) {
+      res["headers"] = boost::any(*headers);
+    }
+    if (statusCode) {
+      res["statusCode"] = boost::any(*statusCode);
+    }
+    if (body) {
+      res["body"] = body ? boost::any(body->toMap()) : boost::any(map<string,boost::any>({}));
+    }
+    return res;
+  }
+
+  void fromMap(map<string, boost::any> m) override {
+    if (m.find("headers") != m.end() && !m["headers"].empty()) {
+      map<string, string> map1 = boost::any_cast<map<string, string>>(m["headers"]);
+      map<string, string> toMap1;
+      for (auto item:map1) {
+         toMap1[item.first] = item.second;
+      }
+      headers = make_shared<map<string, string>>(toMap1);
+    }
+    if (m.find("statusCode") != m.end() && !m["statusCode"].empty()) {
+      statusCode = make_shared<long>(boost::any_cast<long>(m["statusCode"]));
+    }
+    if (m.find("body") != m.end() && !m["body"].empty()) {
+      if (typeid(map<string, boost::any>) == m["body"].type()) {
+        DescribeMobileOperatorAttributeResponseBody model1;
+        model1.fromMap(boost::any_cast<map<string, boost::any>>(m["body"]));
+        body = make_shared<DescribeMobileOperatorAttributeResponseBody>(model1);
+      }
+    }
+  }
+
+
+  virtual ~DescribeMobileOperatorAttributeResponse() = default;
+};
 class DescribePhoneNumberAnalysisRequest : public Darabonba::Model {
 public:
   shared_ptr<string> authCode{};
@@ -8792,6 +9040,8 @@ public:
   CompanyTwoElementsVerificationResponse companyTwoElementsVerification(shared_ptr<CompanyTwoElementsVerificationRequest> request);
   DescribeEmptyNumberResponse describeEmptyNumberWithOptions(shared_ptr<DescribeEmptyNumberRequest> request, shared_ptr<Darabonba_Util::RuntimeOptions> runtime);
   DescribeEmptyNumberResponse describeEmptyNumber(shared_ptr<DescribeEmptyNumberRequest> request);
+  DescribeMobileOperatorAttributeResponse describeMobileOperatorAttributeWithOptions(shared_ptr<DescribeMobileOperatorAttributeRequest> request, shared_ptr<Darabonba_Util::RuntimeOptions> runtime);
+  DescribeMobileOperatorAttributeResponse describeMobileOperatorAttribute(shared_ptr<DescribeMobileOperatorAttributeRequest> request);
   DescribePhoneNumberAnalysisResponse describePhoneNumberAnalysisWithOptions(shared_ptr<DescribePhoneNumberAnalysisRequest> request, shared_ptr<Darabonba_Util::RuntimeOptions> runtime);
   DescribePhoneNumberAnalysisResponse describePhoneNumberAnalysis(shared_ptr<DescribePhoneNumberAnalysisRequest> request);
   DescribePhoneNumberAnalysisAIResponse describePhoneNumberAnalysisAIWithOptions(shared_ptr<DescribePhoneNumberAnalysisAIRequest> request, shared_ptr<Darabonba_Util::RuntimeOptions> runtime);
--- dytnsapi-20200217/src/dytnsapi_20200217.cpp
+++ dytnsapi-20200217/src/dytnsapi_20200217.cpp
@@ -310,6 +310,49 @@ DescribeEmptyNumberResponse Alibabacloud_Dytnsapi20200217::Client::describeEmpty
   return describeEmptyNumberWithOptions(request, runtime);
 }
 
+DescribeMobileOperatorAttributeResponse Alibabacloud_Dytnsapi20200217::Client::describeMobileOperatorAttributeWithOptions(shared_ptr<DescribeMobileOperatorAttributeRequest> request, shared_ptr<Darabonba_Util::RuntimeOptions> runtime) {
+  Darabonba_Util::Client::validateModel(request);
+  shared_ptr<map<string, boost::any>> query = make_shared<map<string, boost::any>>(map<string, boost::any>());
+  if (!Darabonba_Util::Client::isUnset<string>(request->authCode)) {
+    query->insert(pair<string, string>("AuthCode", *request->authCode));
+  }
+  if (!Darabonba_Util::Client::isUnset<string>(request->inputNumber)) {
+    query->insert(pair<string, string>("InputNumber", *request->inputNumber));
+  }
+  if (!Darabonba_Util::Client::isUnset<string>(request->mask)) {
+    query->insert(pair<string, string>("Mask", *request->mask));
+  }
+  if (!Darabonba_Util::Client::isUnset<long>(request->ownerId)) {
+    query->insert(pair<string, long>("OwnerId", *request->ownerId));
+  }
+  if (!Darabonba_Util::Client::isUnset<string>(request->resourceOwnerAccount)) {
+    query->insert(pair<string, string>("ResourceOwnerAccount", *request->resourceOwnerAccount));
+  }
+  if (!Darabonba_Util::Client::isUnset<long>(request->resourceOwnerId)) {
+    query->insert(pair<string, long>("ResourceOwnerId", *request->resourceOwnerId));
+  }
+  shared_ptr<Alibabacloud_OpenApi::OpenApiRequest> req = make_shared<Alibabacloud_OpenApi::OpenApiRequest>(map<string, boost::any>({
+    {"query", boost::any(Alibabacloud_OpenApiUtil::Client::query(query))}
+  }));
+  shared_ptr<Alibabacloud_OpenApi::Params> params = make_shared<Alibabacloud_OpenApi::Params>(map<string, boost::any>({
+    {"action", boost::any(string("DescribeMobileOperatorAttribute"))},
+    {"version", boost::any(string("2020-02-17"))},
+    {"protocol", boost::any(string("HTTPS"))},
+    {"pathname", boost::any(string("/"))},
+    {"method", boost::any(string("POST"))},
+    {"authType", boost::any(string("AK"))},
+    {"style", boost::any(string("RPC"))},
+    {"reqBodyType", boost::any(string("formData"))},
+    {"bodyType", boost::any(string("json"))}
+  }));
+  return DescribeMobileOperatorAttributeResponse(callApi(params, req, runtime));
+}
+
+DescribeMobileOperatorAttributeResponse Alibabacloud_Dytnsapi20200217::Client::describeMobileOperatorAttribute(shared_ptr<DescribeMobileOperatorAttributeRequest> request) {
+  shared_ptr<Darabonba_Util::RuntimeOptions> runtime = make_shared<Darabonba_Util::RuntimeOptions>();
+  return describeMobileOperatorAttributeWithOptions(request, runtime);
+}
+
 DescribePhoneNumberAnalysisResponse Alibabacloud_Dytnsapi20200217::Client::describePhoneNumberAnalysisWithOptions(shared_ptr<DescribePhoneNumberAnalysisRequest> request, shared_ptr<Darabonba_Util::RuntimeOptions> runtime) {
   Darabonba_Util::Client::validateModel(request);
   shared_ptr<map<string, boost::any>> query = make_shared<map<string, boost::any>>(map<string, boost::any>());
