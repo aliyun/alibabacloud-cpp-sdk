7cfd05f18294f38f2af77996b98266488e8f747b
sdk-team <sdk-team@alibabacloud.com>
2024-12-24 03:07:51 +0000

    Generated cpp 2016-02-01 for Httpdns.

--- httpdns-20160201/include/alibabacloud/httpdns_20160201.hpp
+++ httpdns-20160201/include/alibabacloud/httpdns_20160201.hpp
@@ -1377,6 +1377,166 @@ public:
 
   virtual ~ListDomainsResponse() = default;
 };
+class RefreshResolveCacheRequest : public Darabonba::Model {
+public:
+  shared_ptr<vector<string>> domains{};
+
+  RefreshResolveCacheRequest() {}
+
+  explicit RefreshResolveCacheRequest(const std::map<string, boost::any> &config) : Darabonba::Model(config) {
+    fromMap(config);
+  };
+
+  void validate() override {}
+
+  map<string, boost::any> toMap() override {
+    map<string, boost::any> res;
+    if (domains) {
+      res["Domains"] = boost::any(*domains);
+    }
+    return res;
+  }
+
+  void fromMap(map<string, boost::any> m) override {
+    if (m.find("Domains") != m.end() && !m["Domains"].empty()) {
+      vector<string> toVec1;
+      if (typeid(vector<boost::any>) == m["Domains"].type()) {
+        vector<boost::any> vec1 = boost::any_cast<vector<boost::any>>(m["Domains"]);
+        for (auto item:vec1) {
+           toVec1.push_back(boost::any_cast<string>(item));
+        }
+      }
+      domains = make_shared<vector<string>>(toVec1);
+    }
+  }
+
+
+  virtual ~RefreshResolveCacheRequest() = default;
+};
+class RefreshResolveCacheShrinkRequest : public Darabonba::Model {
+public:
+  shared_ptr<string> domainsShrink{};
+
+  RefreshResolveCacheShrinkRequest() {}
+
+  explicit RefreshResolveCacheShrinkRequest(const std::map<string, boost::any> &config) : Darabonba::Model(config) {
+    fromMap(config);
+  };
+
+  void validate() override {}
+
+  map<string, boost::any> toMap() override {
+    map<string, boost::any> res;
+    if (domainsShrink) {
+      res["Domains"] = boost::any(*domainsShrink);
+    }
+    return res;
+  }
+
+  void fromMap(map<string, boost::any> m) override {
+    if (m.find("Domains") != m.end() && !m["Domains"].empty()) {
+      domainsShrink = make_shared<string>(boost::any_cast<string>(m["Domains"]));
+    }
+  }
+
+
+  virtual ~RefreshResolveCacheShrinkRequest() = default;
+};
+class RefreshResolveCacheResponseBody : public Darabonba::Model {
+public:
+  shared_ptr<string> code{};
+  shared_ptr<string> message{};
+  shared_ptr<string> requestId{};
+
+  RefreshResolveCacheResponseBody() {}
+
+  explicit RefreshResolveCacheResponseBody(const std::map<string, boost::any> &config) : Darabonba::Model(config) {
+    fromMap(config);
+  };
+
+  void validate() override {}
+
+  map<string, boost::any> toMap() override {
+    map<string, boost::any> res;
+    if (code) {
+      res["Code"] = boost::any(*code);
+    }
+    if (message) {
+      res["Message"] = boost::any(*message);
+    }
+    if (requestId) {
+      res["RequestId"] = boost::any(*requestId);
+    }
+    return res;
+  }
+
+  void fromMap(map<string, boost::any> m) override {
+    if (m.find("Code") != m.end() && !m["Code"].empty()) {
+      code = make_shared<string>(boost::any_cast<string>(m["Code"]));
+    }
+    if (m.find("Message") != m.end() && !m["Message"].empty()) {
+      message = make_shared<string>(boost::any_cast<string>(m["Message"]));
+    }
+    if (m.find("RequestId") != m.end() && !m["RequestId"].empty()) {
+      requestId = make_shared<string>(boost::any_cast<string>(m["RequestId"]));
+    }
+  }
+
+
+  virtual ~RefreshResolveCacheResponseBody() = default;
+};
+class RefreshResolveCacheResponse : public Darabonba::Model {
+public:
+  shared_ptr<map<string, string>> headers{};
+  shared_ptr<long> statusCode{};
+  shared_ptr<RefreshResolveCacheResponseBody> body{};
+
+  RefreshResolveCacheResponse() {}
+
+  explicit RefreshResolveCacheResponse(const std::map<string, boost::any> &config) : Darabonba::Model(config) {
+    fromMap(config);
+  };
+
+  void validate() override {}
+
+  map<string, boost::any> toMap() override {
+    map<string, boost::any> res;
+    if (headers) {
+      res["headers"] = boost::any(*headers);
+    }
+    if (statusCode) {
+      res["statusCode"] = boost::any(*statusCode);
+    }
+    if (body) {
+      res["body"] = body ? boost::any(body->toMap()) : boost::any(map<string,boost::any>({}));
+    }
+    return res;
+  }
+
+  void fromMap(map<string, boost::any> m) override {
+    if (m.find("headers") != m.end() && !m["headers"].empty()) {
+      map<string, string> map1 = boost::any_cast<map<string, string>>(m["headers"]);
+      map<string, string> toMap1;
+      for (auto item:map1) {
+         toMap1[item.first] = item.second;
+      }
+      headers = make_shared<map<string, string>>(toMap1);
+    }
+    if (m.find("statusCode") != m.end() && !m["statusCode"].empty()) {
+      statusCode = make_shared<long>(boost::any_cast<long>(m["statusCode"]));
+    }
+    if (m.find("body") != m.end() && !m["body"].empty()) {
+      if (typeid(map<string, boost::any>) == m["body"].type()) {
+        RefreshResolveCacheResponseBody model1;
+        model1.fromMap(boost::any_cast<map<string, boost::any>>(m["body"]));
+        body = make_shared<RefreshResolveCacheResponseBody>(model1);
+      }
+    }
+  }
+
+
+  virtual ~RefreshResolveCacheResponse() = default;
+};
 class Client : Alibabacloud_OpenApi::Client {
 public:
   explicit Client(const shared_ptr<Alibabacloud_OpenApi::Config>& config);
@@ -1401,6 +1561,8 @@ public:
   GetResolveStatisticsResponse getResolveStatistics(shared_ptr<GetResolveStatisticsRequest> request);
   ListDomainsResponse listDomainsWithOptions(shared_ptr<ListDomainsRequest> request, shared_ptr<Darabonba_Util::RuntimeOptions> runtime);
   ListDomainsResponse listDomains(shared_ptr<ListDomainsRequest> request);
+  RefreshResolveCacheResponse refreshResolveCacheWithOptions(shared_ptr<RefreshResolveCacheRequest> tmpReq, shared_ptr<Darabonba_Util::RuntimeOptions> runtime);
+  RefreshResolveCacheResponse refreshResolveCache(shared_ptr<RefreshResolveCacheRequest> request);
 
   virtual ~Client() = default;
 };
--- httpdns-20160201/src/httpdns_20160201.cpp
+++ httpdns-20160201/src/httpdns_20160201.cpp
@@ -259,3 +259,36 @@ ListDomainsResponse Alibabacloud_Httpdns20160201::Client::listDomains(shared_ptr
   return listDomainsWithOptions(request, runtime);
 }
 
+RefreshResolveCacheResponse Alibabacloud_Httpdns20160201::Client::refreshResolveCacheWithOptions(shared_ptr<RefreshResolveCacheRequest> tmpReq, shared_ptr<Darabonba_Util::RuntimeOptions> runtime) {
+  Darabonba_Util::Client::validateModel(tmpReq);
+  shared_ptr<RefreshResolveCacheShrinkRequest> request = make_shared<RefreshResolveCacheShrinkRequest>();
+  Alibabacloud_OpenApiUtil::Client::convert(tmpReq, request);
+  if (!Darabonba_Util::Client::isUnset<vector<string>>(tmpReq->domains)) {
+    request->domainsShrink = make_shared<string>(Alibabacloud_OpenApiUtil::Client::arrayToStringWithSpecifiedStyle(tmpReq->domains, make_shared<string>("Domains"), make_shared<string>("json")));
+  }
+  shared_ptr<map<string, boost::any>> query = make_shared<map<string, boost::any>>(map<string, boost::any>());
+  if (!Darabonba_Util::Client::isUnset<string>(request->domainsShrink)) {
+    query->insert(pair<string, string>("Domains", *request->domainsShrink));
+  }
+  shared_ptr<Alibabacloud_OpenApi::OpenApiRequest> req = make_shared<Alibabacloud_OpenApi::OpenApiRequest>(map<string, boost::any>({
+    {"query", boost::any(Alibabacloud_OpenApiUtil::Client::query(query))}
+  }));
+  shared_ptr<Alibabacloud_OpenApi::Params> params = make_shared<Alibabacloud_OpenApi::Params>(map<string, boost::any>({
+    {"action", boost::any(string("RefreshResolveCache"))},
+    {"version", boost::any(string("2016-02-01"))},
+    {"protocol", boost::any(string("HTTPS"))},
+    {"pathname", boost::any(string("/"))},
+    {"method", boost::any(string("POST"))},
+    {"authType", boost::any(string("AK"))},
+    {"style", boost::any(string("RPC"))},
+    {"reqBodyType", boost::any(string("formData"))},
+    {"bodyType", boost::any(string("json"))}
+  }));
+  return RefreshResolveCacheResponse(callApi(params, req, runtime));
+}
+
+RefreshResolveCacheResponse Alibabacloud_Httpdns20160201::Client::refreshResolveCache(shared_ptr<RefreshResolveCacheRequest> request) {
+  shared_ptr<Darabonba_Util::RuntimeOptions> runtime = make_shared<Darabonba_Util::RuntimeOptions>();
+  return refreshResolveCacheWithOptions(request, runtime);
+}
+
