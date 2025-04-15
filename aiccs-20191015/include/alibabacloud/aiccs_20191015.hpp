61c03d6738198a85baa3cf11b65ddcb834736248
sdk-team <sdk-team@alibabacloud.com>
2025-04-15 17:28:31 +0000

    Support API LlmSmartCallEncrypt.

--- aiccs-20191015/include/alibabacloud/aiccs_20191015.hpp
+++ aiccs-20191015/include/alibabacloud/aiccs_20191015.hpp
@@ -27941,6 +27941,335 @@ public:
 
   virtual ~LlmSmartCallResponse() = default;
 };
+class LlmSmartCallEncryptRequest : public Darabonba::Model {
+public:
+  shared_ptr<string> applicationCode{};
+  shared_ptr<string> callerNumber{};
+  shared_ptr<string> encryptCalledNumber{};
+  shared_ptr<string> outId{};
+  shared_ptr<long> ownerId{};
+  shared_ptr<map<string, boost::any>> promptParam{};
+  shared_ptr<string> resourceOwnerAccount{};
+  shared_ptr<long> resourceOwnerId{};
+  shared_ptr<map<string, boost::any>> startWordParam{};
+
+  LlmSmartCallEncryptRequest() {}
+
+  explicit LlmSmartCallEncryptRequest(const std::map<string, boost::any> &config) : Darabonba::Model(config) {
+    fromMap(config);
+  };
+
+  void validate() override {}
+
+  map<string, boost::any> toMap() override {
+    map<string, boost::any> res;
+    if (applicationCode) {
+      res["ApplicationCode"] = boost::any(*applicationCode);
+    }
+    if (callerNumber) {
+      res["CallerNumber"] = boost::any(*callerNumber);
+    }
+    if (encryptCalledNumber) {
+      res["EncryptCalledNumber"] = boost::any(*encryptCalledNumber);
+    }
+    if (outId) {
+      res["OutId"] = boost::any(*outId);
+    }
+    if (ownerId) {
+      res["OwnerId"] = boost::any(*ownerId);
+    }
+    if (promptParam) {
+      res["PromptParam"] = boost::any(*promptParam);
+    }
+    if (resourceOwnerAccount) {
+      res["ResourceOwnerAccount"] = boost::any(*resourceOwnerAccount);
+    }
+    if (resourceOwnerId) {
+      res["ResourceOwnerId"] = boost::any(*resourceOwnerId);
+    }
+    if (startWordParam) {
+      res["StartWordParam"] = boost::any(*startWordParam);
+    }
+    return res;
+  }
+
+  void fromMap(map<string, boost::any> m) override {
+    if (m.find("ApplicationCode") != m.end() && !m["ApplicationCode"].empty()) {
+      applicationCode = make_shared<string>(boost::any_cast<string>(m["ApplicationCode"]));
+    }
+    if (m.find("CallerNumber") != m.end() && !m["CallerNumber"].empty()) {
+      callerNumber = make_shared<string>(boost::any_cast<string>(m["CallerNumber"]));
+    }
+    if (m.find("EncryptCalledNumber") != m.end() && !m["EncryptCalledNumber"].empty()) {
+      encryptCalledNumber = make_shared<string>(boost::any_cast<string>(m["EncryptCalledNumber"]));
+    }
+    if (m.find("OutId") != m.end() && !m["OutId"].empty()) {
+      outId = make_shared<string>(boost::any_cast<string>(m["OutId"]));
+    }
+    if (m.find("OwnerId") != m.end() && !m["OwnerId"].empty()) {
+      ownerId = make_shared<long>(boost::any_cast<long>(m["OwnerId"]));
+    }
+    if (m.find("PromptParam") != m.end() && !m["PromptParam"].empty()) {
+      map<string, boost::any> map1 = boost::any_cast<map<string, boost::any>>(m["PromptParam"]);
+      map<string, boost::any> toMap1;
+      for (auto item:map1) {
+         toMap1[item.first] = item.second;
+      }
+      promptParam = make_shared<map<string, boost::any>>(toMap1);
+    }
+    if (m.find("ResourceOwnerAccount") != m.end() && !m["ResourceOwnerAccount"].empty()) {
+      resourceOwnerAccount = make_shared<string>(boost::any_cast<string>(m["ResourceOwnerAccount"]));
+    }
+    if (m.find("ResourceOwnerId") != m.end() && !m["ResourceOwnerId"].empty()) {
+      resourceOwnerId = make_shared<long>(boost::any_cast<long>(m["ResourceOwnerId"]));
+    }
+    if (m.find("StartWordParam") != m.end() && !m["StartWordParam"].empty()) {
+      map<string, boost::any> map1 = boost::any_cast<map<string, boost::any>>(m["StartWordParam"]);
+      map<string, boost::any> toMap1;
+      for (auto item:map1) {
+         toMap1[item.first] = item.second;
+      }
+      startWordParam = make_shared<map<string, boost::any>>(toMap1);
+    }
+  }
+
+
+  virtual ~LlmSmartCallEncryptRequest() = default;
+};
+class LlmSmartCallEncryptShrinkRequest : public Darabonba::Model {
+public:
+  shared_ptr<string> applicationCode{};
+  shared_ptr<string> callerNumber{};
+  shared_ptr<string> encryptCalledNumber{};
+  shared_ptr<string> outId{};
+  shared_ptr<long> ownerId{};
+  shared_ptr<string> promptParamShrink{};
+  shared_ptr<string> resourceOwnerAccount{};
+  shared_ptr<long> resourceOwnerId{};
+  shared_ptr<string> startWordParamShrink{};
+
+  LlmSmartCallEncryptShrinkRequest() {}
+
+  explicit LlmSmartCallEncryptShrinkRequest(const std::map<string, boost::any> &config) : Darabonba::Model(config) {
+    fromMap(config);
+  };
+
+  void validate() override {}
+
+  map<string, boost::any> toMap() override {
+    map<string, boost::any> res;
+    if (applicationCode) {
+      res["ApplicationCode"] = boost::any(*applicationCode);
+    }
+    if (callerNumber) {
+      res["CallerNumber"] = boost::any(*callerNumber);
+    }
+    if (encryptCalledNumber) {
+      res["EncryptCalledNumber"] = boost::any(*encryptCalledNumber);
+    }
+    if (outId) {
+      res["OutId"] = boost::any(*outId);
+    }
+    if (ownerId) {
+      res["OwnerId"] = boost::any(*ownerId);
+    }
+    if (promptParamShrink) {
+      res["PromptParam"] = boost::any(*promptParamShrink);
+    }
+    if (resourceOwnerAccount) {
+      res["ResourceOwnerAccount"] = boost::any(*resourceOwnerAccount);
+    }
+    if (resourceOwnerId) {
+      res["ResourceOwnerId"] = boost::any(*resourceOwnerId);
+    }
+    if (startWordParamShrink) {
+      res["StartWordParam"] = boost::any(*startWordParamShrink);
+    }
+    return res;
+  }
+
+  void fromMap(map<string, boost::any> m) override {
+    if (m.find("ApplicationCode") != m.end() && !m["ApplicationCode"].empty()) {
+      applicationCode = make_shared<string>(boost::any_cast<string>(m["ApplicationCode"]));
+    }
+    if (m.find("CallerNumber") != m.end() && !m["CallerNumber"].empty()) {
+      callerNumber = make_shared<string>(boost::any_cast<string>(m["CallerNumber"]));
+    }
+    if (m.find("EncryptCalledNumber") != m.end() && !m["EncryptCalledNumber"].empty()) {
+      encryptCalledNumber = make_shared<string>(boost::any_cast<string>(m["EncryptCalledNumber"]));
+    }
+    if (m.find("OutId") != m.end() && !m["OutId"].empty()) {
+      outId = make_shared<string>(boost::any_cast<string>(m["OutId"]));
+    }
+    if (m.find("OwnerId") != m.end() && !m["OwnerId"].empty()) {
+      ownerId = make_shared<long>(boost::any_cast<long>(m["OwnerId"]));
+    }
+    if (m.find("PromptParam") != m.end() && !m["PromptParam"].empty()) {
+      promptParamShrink = make_shared<string>(boost::any_cast<string>(m["PromptParam"]));
+    }
+    if (m.find("ResourceOwnerAccount") != m.end() && !m["ResourceOwnerAccount"].empty()) {
+      resourceOwnerAccount = make_shared<string>(boost::any_cast<string>(m["ResourceOwnerAccount"]));
+    }
+    if (m.find("ResourceOwnerId") != m.end() && !m["ResourceOwnerId"].empty()) {
+      resourceOwnerId = make_shared<long>(boost::any_cast<long>(m["ResourceOwnerId"]));
+    }
+    if (m.find("StartWordParam") != m.end() && !m["StartWordParam"].empty()) {
+      startWordParamShrink = make_shared<string>(boost::any_cast<string>(m["StartWordParam"]));
+    }
+  }
+
+
+  virtual ~LlmSmartCallEncryptShrinkRequest() = default;
+};
+class LlmSmartCallEncryptResponseBodyData : public Darabonba::Model {
+public:
+  shared_ptr<string> callId{};
+
+  LlmSmartCallEncryptResponseBodyData() {}
+
+  explicit LlmSmartCallEncryptResponseBodyData(const std::map<string, boost::any> &config) : Darabonba::Model(config) {
+    fromMap(config);
+  };
+
+  void validate() override {}
+
+  map<string, boost::any> toMap() override {
+    map<string, boost::any> res;
+    if (callId) {
+      res["CallId"] = boost::any(*callId);
+    }
+    return res;
+  }
+
+  void fromMap(map<string, boost::any> m) override {
+    if (m.find("CallId") != m.end() && !m["CallId"].empty()) {
+      callId = make_shared<string>(boost::any_cast<string>(m["CallId"]));
+    }
+  }
+
+
+  virtual ~LlmSmartCallEncryptResponseBodyData() = default;
+};
+class LlmSmartCallEncryptResponseBody : public Darabonba::Model {
+public:
+  shared_ptr<string> accessDeniedDetail{};
+  shared_ptr<string> code{};
+  shared_ptr<LlmSmartCallEncryptResponseBodyData> data{};
+  shared_ptr<string> message{};
+  shared_ptr<string> requestId{};
+  shared_ptr<bool> success{};
+
+  LlmSmartCallEncryptResponseBody() {}
+
+  explicit LlmSmartCallEncryptResponseBody(const std::map<string, boost::any> &config) : Darabonba::Model(config) {
+    fromMap(config);
+  };
+
+  void validate() override {}
+
+  map<string, boost::any> toMap() override {
+    map<string, boost::any> res;
+    if (accessDeniedDetail) {
+      res["AccessDeniedDetail"] = boost::any(*accessDeniedDetail);
+    }
+    if (code) {
+      res["Code"] = boost::any(*code);
+    }
+    if (data) {
+      res["Data"] = data ? boost::any(data->toMap()) : boost::any(map<string,boost::any>({}));
+    }
+    if (message) {
+      res["Message"] = boost::any(*message);
+    }
+    if (requestId) {
+      res["RequestId"] = boost::any(*requestId);
+    }
+    if (success) {
+      res["Success"] = boost::any(*success);
+    }
+    return res;
+  }
+
+  void fromMap(map<string, boost::any> m) override {
+    if (m.find("AccessDeniedDetail") != m.end() && !m["AccessDeniedDetail"].empty()) {
+      accessDeniedDetail = make_shared<string>(boost::any_cast<string>(m["AccessDeniedDetail"]));
+    }
+    if (m.find("Code") != m.end() && !m["Code"].empty()) {
+      code = make_shared<string>(boost::any_cast<string>(m["Code"]));
+    }
+    if (m.find("Data") != m.end() && !m["Data"].empty()) {
+      if (typeid(map<string, boost::any>) == m["Data"].type()) {
+        LlmSmartCallEncryptResponseBodyData model1;
+        model1.fromMap(boost::any_cast<map<string, boost::any>>(m["Data"]));
+        data = make_shared<LlmSmartCallEncryptResponseBodyData>(model1);
+      }
+    }
+    if (m.find("Message") != m.end() && !m["Message"].empty()) {
+      message = make_shared<string>(boost::any_cast<string>(m["Message"]));
+    }
+    if (m.find("RequestId") != m.end() && !m["RequestId"].empty()) {
+      requestId = make_shared<string>(boost::any_cast<string>(m["RequestId"]));
+    }
+    if (m.find("Success") != m.end() && !m["Success"].empty()) {
+      success = make_shared<bool>(boost::any_cast<bool>(m["Success"]));
+    }
+  }
+
+
+  virtual ~LlmSmartCallEncryptResponseBody() = default;
+};
+class LlmSmartCallEncryptResponse : public Darabonba::Model {
+public:
+  shared_ptr<map<string, string>> headers{};
+  shared_ptr<long> statusCode{};
+  shared_ptr<LlmSmartCallEncryptResponseBody> body{};
+
+  LlmSmartCallEncryptResponse() {}
+
+  explicit LlmSmartCallEncryptResponse(const std::map<string, boost::any> &config) : Darabonba::Model(config) {
+    fromMap(config);
+  };
+
+  void validate() override {}
+
+  map<string, boost::any> toMap() override {
+    map<string, boost::any> res;
+    if (headers) {
+      res["headers"] = boost::any(*headers);
+    }
+    if (statusCode) {
+      res["statusCode"] = boost::any(*statusCode);
+    }
+    if (body) {
+      res["body"] = body ? boost::any(body->toMap()) : boost::any(map<string,boost::any>({}));
+    }
+    return res;
+  }
+
+  void fromMap(map<string, boost::any> m) override {
+    if (m.find("headers") != m.end() && !m["headers"].empty()) {
+      map<string, string> map1 = boost::any_cast<map<string, string>>(m["headers"]);
+      map<string, string> toMap1;
+      for (auto item:map1) {
+         toMap1[item.first] = item.second;
+      }
+      headers = make_shared<map<string, string>>(toMap1);
+    }
+    if (m.find("statusCode") != m.end() && !m["statusCode"].empty()) {
+      statusCode = make_shared<long>(boost::any_cast<long>(m["statusCode"]));
+    }
+    if (m.find("body") != m.end() && !m["body"].empty()) {
+      if (typeid(map<string, boost::any>) == m["body"].type()) {
+        LlmSmartCallEncryptResponseBody model1;
+        model1.fromMap(boost::any_cast<map<string, boost::any>>(m["body"]));
+        body = make_shared<LlmSmartCallEncryptResponseBody>(model1);
+      }
+    }
+  }
+
+
+  virtual ~LlmSmartCallEncryptResponse() = default;
+};
 class MakeCallRequest : public Darabonba::Model {
 public:
   shared_ptr<string> calledNumber{};
@@ -36418,6 +36747,8 @@ public:
   ListTaskDetailResponse listTaskDetail(shared_ptr<ListTaskDetailRequest> request);
   LlmSmartCallResponse llmSmartCallWithOptions(shared_ptr<LlmSmartCallRequest> tmpReq, shared_ptr<Darabonba_Util::RuntimeOptions> runtime);
   LlmSmartCallResponse llmSmartCall(shared_ptr<LlmSmartCallRequest> request);
+  LlmSmartCallEncryptResponse llmSmartCallEncryptWithOptions(shared_ptr<LlmSmartCallEncryptRequest> tmpReq, shared_ptr<Darabonba_Util::RuntimeOptions> runtime);
+  LlmSmartCallEncryptResponse llmSmartCallEncrypt(shared_ptr<LlmSmartCallEncryptRequest> request);
   MakeCallResponse makeCallWithOptions(shared_ptr<MakeCallRequest> request, shared_ptr<Darabonba_Util::RuntimeOptions> runtime);
   MakeCallResponse makeCall(shared_ptr<MakeCallRequest> request);
   MakeDoubleCallResponse makeDoubleCallWithOptions(shared_ptr<MakeDoubleCallRequest> request, shared_ptr<Darabonba_Util::RuntimeOptions> runtime);
