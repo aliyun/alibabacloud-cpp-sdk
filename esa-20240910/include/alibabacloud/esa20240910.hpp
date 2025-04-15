fab72789672c0a5f5de7eeee23791d791ccd240f
sdk-team <sdk-team@alibabacloud.com>
2025-04-15 03:56:30 +0000

    Support API GetErService.

--- esa-20240910/include/alibabacloud/esa20240910.hpp
+++ esa-20240910/include/alibabacloud/esa20240910.hpp
@@ -30214,6 +30214,137 @@ public:
 
   virtual ~GetEdgeContainerTerminalResponse() = default;
 };
+class GetErServiceRequest : public Darabonba::Model {
+public:
+  shared_ptr<long> ownerId{};
+  shared_ptr<string> securityToken{};
+
+  GetErServiceRequest() {}
+
+  explicit GetErServiceRequest(const std::map<string, boost::any> &config) : Darabonba::Model(config) {
+    fromMap(config);
+  };
+
+  void validate() override {}
+
+  map<string, boost::any> toMap() override {
+    map<string, boost::any> res;
+    if (ownerId) {
+      res["OwnerId"] = boost::any(*ownerId);
+    }
+    if (securityToken) {
+      res["SecurityToken"] = boost::any(*securityToken);
+    }
+    return res;
+  }
+
+  void fromMap(map<string, boost::any> m) override {
+    if (m.find("OwnerId") != m.end() && !m["OwnerId"].empty()) {
+      ownerId = make_shared<long>(boost::any_cast<long>(m["OwnerId"]));
+    }
+    if (m.find("SecurityToken") != m.end() && !m["SecurityToken"].empty()) {
+      securityToken = make_shared<string>(boost::any_cast<string>(m["SecurityToken"]));
+    }
+  }
+
+
+  virtual ~GetErServiceRequest() = default;
+};
+class GetErServiceResponseBody : public Darabonba::Model {
+public:
+  shared_ptr<string> planName{};
+  shared_ptr<string> requestId{};
+  shared_ptr<string> status{};
+
+  GetErServiceResponseBody() {}
+
+  explicit GetErServiceResponseBody(const std::map<string, boost::any> &config) : Darabonba::Model(config) {
+    fromMap(config);
+  };
+
+  void validate() override {}
+
+  map<string, boost::any> toMap() override {
+    map<string, boost::any> res;
+    if (planName) {
+      res["PlanName"] = boost::any(*planName);
+    }
+    if (requestId) {
+      res["RequestId"] = boost::any(*requestId);
+    }
+    if (status) {
+      res["Status"] = boost::any(*status);
+    }
+    return res;
+  }
+
+  void fromMap(map<string, boost::any> m) override {
+    if (m.find("PlanName") != m.end() && !m["PlanName"].empty()) {
+      planName = make_shared<string>(boost::any_cast<string>(m["PlanName"]));
+    }
+    if (m.find("RequestId") != m.end() && !m["RequestId"].empty()) {
+      requestId = make_shared<string>(boost::any_cast<string>(m["RequestId"]));
+    }
+    if (m.find("Status") != m.end() && !m["Status"].empty()) {
+      status = make_shared<string>(boost::any_cast<string>(m["Status"]));
+    }
+  }
+
+
+  virtual ~GetErServiceResponseBody() = default;
+};
+class GetErServiceResponse : public Darabonba::Model {
+public:
+  shared_ptr<map<string, string>> headers{};
+  shared_ptr<long> statusCode{};
+  shared_ptr<GetErServiceResponseBody> body{};
+
+  GetErServiceResponse() {}
+
+  explicit GetErServiceResponse(const std::map<string, boost::any> &config) : Darabonba::Model(config) {
+    fromMap(config);
+  };
+
+  void validate() override {}
+
+  map<string, boost::any> toMap() override {
+    map<string, boost::any> res;
+    if (headers) {
+      res["headers"] = boost::any(*headers);
+    }
+    if (statusCode) {
+      res["statusCode"] = boost::any(*statusCode);
+    }
+    if (body) {
+      res["body"] = body ? boost::any(body->toMap()) : boost::any(map<string,boost::any>({}));
+    }
+    return res;
+  }
+
+  void fromMap(map<string, boost::any> m) override {
+    if (m.find("headers") != m.end() && !m["headers"].empty()) {
+      map<string, string> map1 = boost::any_cast<map<string, string>>(m["headers"]);
+      map<string, string> toMap1;
+      for (auto item:map1) {
+         toMap1[item.first] = item.second;
+      }
+      headers = make_shared<map<string, string>>(toMap1);
+    }
+    if (m.find("statusCode") != m.end() && !m["statusCode"].empty()) {
+      statusCode = make_shared<long>(boost::any_cast<long>(m["statusCode"]));
+    }
+    if (m.find("body") != m.end() && !m["body"].empty()) {
+      if (typeid(map<string, boost::any>) == m["body"].type()) {
+        GetErServiceResponseBody model1;
+        model1.fromMap(boost::any_cast<map<string, boost::any>>(m["body"]));
+        body = make_shared<GetErServiceResponseBody>(model1);
+      }
+    }
+  }
+
+
+  virtual ~GetErServiceResponse() = default;
+};
 class GetHttpRequestHeaderModificationRuleRequest : public Darabonba::Model {
 public:
   shared_ptr<long> configId{};
@@ -73302,6 +73433,8 @@ public:
   GetEdgeContainerStagingDeployStatusResponse getEdgeContainerStagingDeployStatus(shared_ptr<GetEdgeContainerStagingDeployStatusRequest> request);
   GetEdgeContainerTerminalResponse getEdgeContainerTerminalWithOptions(shared_ptr<GetEdgeContainerTerminalRequest> request, shared_ptr<Darabonba_Util::RuntimeOptions> runtime);
   GetEdgeContainerTerminalResponse getEdgeContainerTerminal(shared_ptr<GetEdgeContainerTerminalRequest> request);
+  GetErServiceResponse getErServiceWithOptions(shared_ptr<GetErServiceRequest> request, shared_ptr<Darabonba_Util::RuntimeOptions> runtime);
+  GetErServiceResponse getErService(shared_ptr<GetErServiceRequest> request);
   GetHttpRequestHeaderModificationRuleResponse getHttpRequestHeaderModificationRuleWithOptions(shared_ptr<GetHttpRequestHeaderModificationRuleRequest> request, shared_ptr<Darabonba_Util::RuntimeOptions> runtime);
   GetHttpRequestHeaderModificationRuleResponse getHttpRequestHeaderModificationRule(shared_ptr<GetHttpRequestHeaderModificationRuleRequest> request);
   GetHttpResponseHeaderModificationRuleResponse getHttpResponseHeaderModificationRuleWithOptions(shared_ptr<GetHttpResponseHeaderModificationRuleRequest> request, shared_ptr<Darabonba_Util::RuntimeOptions> runtime);
--- esa-20240910/src/esa20240910.cpp
+++ esa-20240910/src/esa20240910.cpp
@@ -5935,6 +5935,36 @@ GetEdgeContainerTerminalResponse Alibabacloud_ESA20240910::Client::getEdgeContai
   return getEdgeContainerTerminalWithOptions(request, runtime);
 }
 
+GetErServiceResponse Alibabacloud_ESA20240910::Client::getErServiceWithOptions(shared_ptr<GetErServiceRequest> request, shared_ptr<Darabonba_Util::RuntimeOptions> runtime) {
+  Darabonba_Util::Client::validateModel(request);
+  shared_ptr<map<string, string>> query = make_shared<map<string, string>>(Alibabacloud_OpenApiUtil::Client::query(make_shared<map<string, boost::any>>(Darabonba_Util::Client::toMap(request))));
+  shared_ptr<Alibabacloud_OpenApi::OpenApiRequest> req = make_shared<Alibabacloud_OpenApi::OpenApiRequest>(map<string, boost::any>({
+    {"query", boost::any(Alibabacloud_OpenApiUtil::Client::query(query))}
+  }));
+  shared_ptr<Alibabacloud_OpenApi::Params> params = make_shared<Alibabacloud_OpenApi::Params>(map<string, boost::any>({
+    {"action", boost::any(string("GetErService"))},
+    {"version", boost::any(string("2024-09-10"))},
+    {"protocol", boost::any(string("HTTPS"))},
+    {"pathname", boost::any(string("/"))},
+    {"method", boost::any(string("GET"))},
+    {"authType", boost::any(string("AK"))},
+    {"style", boost::any(string("RPC"))},
+    {"reqBodyType", boost::any(string("formData"))},
+    {"bodyType", boost::any(string("json"))}
+  }));
+  if (Darabonba_Util::Client::isUnset<string>(_signatureVersion) || !Darabonba_Util::Client::equalString(_signatureVersion, make_shared<string>("v4"))) {
+    return GetErServiceResponse(callApi(params, req, runtime));
+  }
+  else {
+    return GetErServiceResponse(execute(params, req, runtime));
+  }
+}
+
+GetErServiceResponse Alibabacloud_ESA20240910::Client::getErService(shared_ptr<GetErServiceRequest> request) {
+  shared_ptr<Darabonba_Util::RuntimeOptions> runtime = make_shared<Darabonba_Util::RuntimeOptions>();
+  return getErServiceWithOptions(request, runtime);
+}
+
 GetHttpRequestHeaderModificationRuleResponse Alibabacloud_ESA20240910::Client::getHttpRequestHeaderModificationRuleWithOptions(shared_ptr<GetHttpRequestHeaderModificationRuleRequest> request, shared_ptr<Darabonba_Util::RuntimeOptions> runtime) {
   Darabonba_Util::Client::validateModel(request);
   shared_ptr<map<string, string>> query = make_shared<map<string, string>>(Alibabacloud_OpenApiUtil::Client::query(make_shared<map<string, boost::any>>(Darabonba_Util::Client::toMap(request))));
