994262d82f8336293e57875f5f53e43d3eada8b9
sdk-team <sdk-team@alibabacloud.com>
2025-05-20 09:10:46 +0000

    Support API CheckServiceLinkedRoleEfloCnpForDeleting.

--- eflo-cnp-20230828/include/alibabacloud/eflo_cnp_20230828.hpp
+++ eflo-cnp-20230828/include/alibabacloud/eflo_cnp_20230828.hpp
@@ -244,6 +244,215 @@ public:
 
   virtual ~ChangeResourceGroupResponse() = default;
 };
+class CheckServiceLinkedRoleEfloCnpForDeletingRequest : public Darabonba::Model {
+public:
+  shared_ptr<string> accountId{};
+  shared_ptr<string> deletionTaskId{};
+  shared_ptr<string> roleArn{};
+  shared_ptr<string> SPIRegionId{};
+  shared_ptr<string> serviceName{};
+
+  CheckServiceLinkedRoleEfloCnpForDeletingRequest() {}
+
+  explicit CheckServiceLinkedRoleEfloCnpForDeletingRequest(const std::map<string, boost::any> &config) : Darabonba::Model(config) {
+    fromMap(config);
+  };
+
+  void validate() override {}
+
+  map<string, boost::any> toMap() override {
+    map<string, boost::any> res;
+    if (accountId) {
+      res["AccountId"] = boost::any(*accountId);
+    }
+    if (deletionTaskId) {
+      res["DeletionTaskId"] = boost::any(*deletionTaskId);
+    }
+    if (roleArn) {
+      res["RoleArn"] = boost::any(*roleArn);
+    }
+    if (SPIRegionId) {
+      res["SPIRegionId"] = boost::any(*SPIRegionId);
+    }
+    if (serviceName) {
+      res["ServiceName"] = boost::any(*serviceName);
+    }
+    return res;
+  }
+
+  void fromMap(map<string, boost::any> m) override {
+    if (m.find("AccountId") != m.end() && !m["AccountId"].empty()) {
+      accountId = make_shared<string>(boost::any_cast<string>(m["AccountId"]));
+    }
+    if (m.find("DeletionTaskId") != m.end() && !m["DeletionTaskId"].empty()) {
+      deletionTaskId = make_shared<string>(boost::any_cast<string>(m["DeletionTaskId"]));
+    }
+    if (m.find("RoleArn") != m.end() && !m["RoleArn"].empty()) {
+      roleArn = make_shared<string>(boost::any_cast<string>(m["RoleArn"]));
+    }
+    if (m.find("SPIRegionId") != m.end() && !m["SPIRegionId"].empty()) {
+      SPIRegionId = make_shared<string>(boost::any_cast<string>(m["SPIRegionId"]));
+    }
+    if (m.find("ServiceName") != m.end() && !m["ServiceName"].empty()) {
+      serviceName = make_shared<string>(boost::any_cast<string>(m["ServiceName"]));
+    }
+  }
+
+
+  virtual ~CheckServiceLinkedRoleEfloCnpForDeletingRequest() = default;
+};
+class CheckServiceLinkedRoleEfloCnpForDeletingResponseBodyResources : public Darabonba::Model {
+public:
+  shared_ptr<string> region{};
+  shared_ptr<vector<string>> resources{};
+
+  CheckServiceLinkedRoleEfloCnpForDeletingResponseBodyResources() {}
+
+  explicit CheckServiceLinkedRoleEfloCnpForDeletingResponseBodyResources(const std::map<string, boost::any> &config) : Darabonba::Model(config) {
+    fromMap(config);
+  };
+
+  void validate() override {}
+
+  map<string, boost::any> toMap() override {
+    map<string, boost::any> res;
+    if (region) {
+      res["Region"] = boost::any(*region);
+    }
+    if (resources) {
+      res["Resources"] = boost::any(*resources);
+    }
+    return res;
+  }
+
+  void fromMap(map<string, boost::any> m) override {
+    if (m.find("Region") != m.end() && !m["Region"].empty()) {
+      region = make_shared<string>(boost::any_cast<string>(m["Region"]));
+    }
+    if (m.find("Resources") != m.end() && !m["Resources"].empty()) {
+      vector<string> toVec1;
+      if (typeid(vector<boost::any>) == m["Resources"].type()) {
+        vector<boost::any> vec1 = boost::any_cast<vector<boost::any>>(m["Resources"]);
+        for (auto item:vec1) {
+           toVec1.push_back(boost::any_cast<string>(item));
+        }
+      }
+      resources = make_shared<vector<string>>(toVec1);
+    }
+  }
+
+
+  virtual ~CheckServiceLinkedRoleEfloCnpForDeletingResponseBodyResources() = default;
+};
+class CheckServiceLinkedRoleEfloCnpForDeletingResponseBody : public Darabonba::Model {
+public:
+  shared_ptr<bool> deletable{};
+  shared_ptr<string> requestId{};
+  shared_ptr<vector<CheckServiceLinkedRoleEfloCnpForDeletingResponseBodyResources>> resources{};
+
+  CheckServiceLinkedRoleEfloCnpForDeletingResponseBody() {}
+
+  explicit CheckServiceLinkedRoleEfloCnpForDeletingResponseBody(const std::map<string, boost::any> &config) : Darabonba::Model(config) {
+    fromMap(config);
+  };
+
+  void validate() override {}
+
+  map<string, boost::any> toMap() override {
+    map<string, boost::any> res;
+    if (deletable) {
+      res["Deletable"] = boost::any(*deletable);
+    }
+    if (requestId) {
+      res["RequestId"] = boost::any(*requestId);
+    }
+    if (resources) {
+      vector<boost::any> temp1;
+      for(auto item1:*resources){
+        temp1.push_back(boost::any(item1.toMap()));
+      }
+      res["Resources"] = boost::any(temp1);
+    }
+    return res;
+  }
+
+  void fromMap(map<string, boost::any> m) override {
+    if (m.find("Deletable") != m.end() && !m["Deletable"].empty()) {
+      deletable = make_shared<bool>(boost::any_cast<bool>(m["Deletable"]));
+    }
+    if (m.find("RequestId") != m.end() && !m["RequestId"].empty()) {
+      requestId = make_shared<string>(boost::any_cast<string>(m["RequestId"]));
+    }
+    if (m.find("Resources") != m.end() && !m["Resources"].empty()) {
+      if (typeid(vector<boost::any>) == m["Resources"].type()) {
+        vector<CheckServiceLinkedRoleEfloCnpForDeletingResponseBodyResources> expect1;
+        for(auto item1:boost::any_cast<vector<boost::any>>(m["Resources"])){
+          if (typeid(map<string, boost::any>) == item1.type()) {
+            CheckServiceLinkedRoleEfloCnpForDeletingResponseBodyResources model2;
+            model2.fromMap(boost::any_cast<map<string, boost::any>>(item1));
+            expect1.push_back(model2);
+          }
+        }
+        resources = make_shared<vector<CheckServiceLinkedRoleEfloCnpForDeletingResponseBodyResources>>(expect1);
+      }
+    }
+  }
+
+
+  virtual ~CheckServiceLinkedRoleEfloCnpForDeletingResponseBody() = default;
+};
+class CheckServiceLinkedRoleEfloCnpForDeletingResponse : public Darabonba::Model {
+public:
+  shared_ptr<map<string, string>> headers{};
+  shared_ptr<long> statusCode{};
+  shared_ptr<CheckServiceLinkedRoleEfloCnpForDeletingResponseBody> body{};
+
+  CheckServiceLinkedRoleEfloCnpForDeletingResponse() {}
+
+  explicit CheckServiceLinkedRoleEfloCnpForDeletingResponse(const std::map<string, boost::any> &config) : Darabonba::Model(config) {
+    fromMap(config);
+  };
+
+  void validate() override {}
+
+  map<string, boost::any> toMap() override {
+    map<string, boost::any> res;
+    if (headers) {
+      res["headers"] = boost::any(*headers);
+    }
+    if (statusCode) {
+      res["statusCode"] = boost::any(*statusCode);
+    }
+    if (body) {
+      res["body"] = body ? boost::any(body->toMap()) : boost::any(map<string,boost::any>({}));
+    }
+    return res;
+  }
+
+  void fromMap(map<string, boost::any> m) override {
+    if (m.find("headers") != m.end() && !m["headers"].empty()) {
+      map<string, string> map1 = boost::any_cast<map<string, string>>(m["headers"]);
+      map<string, string> toMap1;
+      for (auto item:map1) {
+         toMap1[item.first] = item.second;
+      }
+      headers = make_shared<map<string, string>>(toMap1);
+    }
+    if (m.find("statusCode") != m.end() && !m["statusCode"].empty()) {
+      statusCode = make_shared<long>(boost::any_cast<long>(m["statusCode"]));
+    }
+    if (m.find("body") != m.end() && !m["body"].empty()) {
+      if (typeid(map<string, boost::any>) == m["body"].type()) {
+        CheckServiceLinkedRoleEfloCnpForDeletingResponseBody model1;
+        model1.fromMap(boost::any_cast<map<string, boost::any>>(m["body"]));
+        body = make_shared<CheckServiceLinkedRoleEfloCnpForDeletingResponseBody>(model1);
+      }
+    }
+  }
+
+
+  virtual ~CheckServiceLinkedRoleEfloCnpForDeletingResponse() = default;
+};
 class CreateExperimentPlanRequestTag : public Darabonba::Model {
 public:
   shared_ptr<string> key{};
@@ -9982,6 +10191,8 @@ public:
                      shared_ptr<string> endpoint);
   ChangeResourceGroupResponse changeResourceGroupWithOptions(shared_ptr<ChangeResourceGroupRequest> request, shared_ptr<Darabonba_Util::RuntimeOptions> runtime);
   ChangeResourceGroupResponse changeResourceGroup(shared_ptr<ChangeResourceGroupRequest> request);
+  CheckServiceLinkedRoleEfloCnpForDeletingResponse checkServiceLinkedRoleEfloCnpForDeletingWithOptions(shared_ptr<CheckServiceLinkedRoleEfloCnpForDeletingRequest> request, shared_ptr<Darabonba_Util::RuntimeOptions> runtime);
+  CheckServiceLinkedRoleEfloCnpForDeletingResponse checkServiceLinkedRoleEfloCnpForDeleting(shared_ptr<CheckServiceLinkedRoleEfloCnpForDeletingRequest> request);
   CreateExperimentPlanResponse createExperimentPlanWithOptions(shared_ptr<CreateExperimentPlanRequest> tmpReq, shared_ptr<Darabonba_Util::RuntimeOptions> runtime);
   CreateExperimentPlanResponse createExperimentPlan(shared_ptr<CreateExperimentPlanRequest> request);
   CreateExperimentPlanTemplateResponse createExperimentPlanTemplateWithOptions(shared_ptr<CreateExperimentPlanTemplateRequest> tmpReq, shared_ptr<Darabonba_Util::RuntimeOptions> runtime);
--- eflo-cnp-20230828/src/eflo_cnp_20230828.cpp
+++ eflo-cnp-20230828/src/eflo_cnp_20230828.cpp
@@ -74,6 +74,46 @@ ChangeResourceGroupResponse Alibabacloud_Eflo-cnp20230828::Client::changeResourc
   return changeResourceGroupWithOptions(request, runtime);
 }
 
+CheckServiceLinkedRoleEfloCnpForDeletingResponse Alibabacloud_Eflo-cnp20230828::Client::checkServiceLinkedRoleEfloCnpForDeletingWithOptions(shared_ptr<CheckServiceLinkedRoleEfloCnpForDeletingRequest> request, shared_ptr<Darabonba_Util::RuntimeOptions> runtime) {
+  Darabonba_Util::Client::validateModel(request);
+  shared_ptr<map<string, boost::any>> query = make_shared<map<string, boost::any>>(map<string, boost::any>());
+  if (!Darabonba_Util::Client::isUnset<string>(request->accountId)) {
+    query->insert(pair<string, string>("AccountId", *request->accountId));
+  }
+  if (!Darabonba_Util::Client::isUnset<string>(request->deletionTaskId)) {
+    query->insert(pair<string, string>("DeletionTaskId", *request->deletionTaskId));
+  }
+  if (!Darabonba_Util::Client::isUnset<string>(request->roleArn)) {
+    query->insert(pair<string, string>("RoleArn", *request->roleArn));
+  }
+  if (!Darabonba_Util::Client::isUnset<string>(request->SPIRegionId)) {
+    query->insert(pair<string, string>("SPIRegionId", *request->SPIRegionId));
+  }
+  if (!Darabonba_Util::Client::isUnset<string>(request->serviceName)) {
+    query->insert(pair<string, string>("ServiceName", *request->serviceName));
+  }
+  shared_ptr<Alibabacloud_OpenApi::OpenApiRequest> req = make_shared<Alibabacloud_OpenApi::OpenApiRequest>(map<string, boost::any>({
+    {"query", boost::any(Alibabacloud_OpenApiUtil::Client::query(query))}
+  }));
+  shared_ptr<Alibabacloud_OpenApi::Params> params = make_shared<Alibabacloud_OpenApi::Params>(map<string, boost::any>({
+    {"action", boost::any(string("CheckServiceLinkedRoleEfloCnpForDeleting"))},
+    {"version", boost::any(string("2023-08-28"))},
+    {"protocol", boost::any(string("HTTPS"))},
+    {"pathname", boost::any(string("/"))},
+    {"method", boost::any(string("POST"))},
+    {"authType", boost::any(string("AK"))},
+    {"style", boost::any(string("RPC"))},
+    {"reqBodyType", boost::any(string("formData"))},
+    {"bodyType", boost::any(string("json"))}
+  }));
+  return CheckServiceLinkedRoleEfloCnpForDeletingResponse(callApi(params, req, runtime));
+}
+
+CheckServiceLinkedRoleEfloCnpForDeletingResponse Alibabacloud_Eflo-cnp20230828::Client::checkServiceLinkedRoleEfloCnpForDeleting(shared_ptr<CheckServiceLinkedRoleEfloCnpForDeletingRequest> request) {
+  shared_ptr<Darabonba_Util::RuntimeOptions> runtime = make_shared<Darabonba_Util::RuntimeOptions>();
+  return checkServiceLinkedRoleEfloCnpForDeletingWithOptions(request, runtime);
+}
+
 CreateExperimentPlanResponse Alibabacloud_Eflo-cnp20230828::Client::createExperimentPlanWithOptions(shared_ptr<CreateExperimentPlanRequest> tmpReq, shared_ptr<Darabonba_Util::RuntimeOptions> runtime) {
   Darabonba_Util::Client::validateModel(tmpReq);
   shared_ptr<CreateExperimentPlanShrinkRequest> request = make_shared<CreateExperimentPlanShrinkRequest>();
