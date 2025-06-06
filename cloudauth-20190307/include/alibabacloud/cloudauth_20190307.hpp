62c5860bffae19e5fbc296592f123d87e87acf9c
sdk-team <sdk-team@alibabacloud.com>
2025-06-06 06:11:42 +0000

    Support API Mobile2MetaVerify.

--- cloudauth-20190307/include/alibabacloud/cloudauth_20190307.hpp
+++ cloudauth-20190307/include/alibabacloud/cloudauth_20190307.hpp
@@ -7644,6 +7644,191 @@ public:
 
   virtual ~LivenessFaceVerifyResponse() = default;
 };
+class Mobile2MetaVerifyRequest : public Darabonba::Model {
+public:
+  shared_ptr<string> mobile{};
+  shared_ptr<string> paramType{};
+  shared_ptr<string> userName{};
+
+  Mobile2MetaVerifyRequest() {}
+
+  explicit Mobile2MetaVerifyRequest(const std::map<string, boost::any> &config) : Darabonba::Model(config) {
+    fromMap(config);
+  };
+
+  void validate() override {}
+
+  map<string, boost::any> toMap() override {
+    map<string, boost::any> res;
+    if (mobile) {
+      res["Mobile"] = boost::any(*mobile);
+    }
+    if (paramType) {
+      res["ParamType"] = boost::any(*paramType);
+    }
+    if (userName) {
+      res["UserName"] = boost::any(*userName);
+    }
+    return res;
+  }
+
+  void fromMap(map<string, boost::any> m) override {
+    if (m.find("Mobile") != m.end() && !m["Mobile"].empty()) {
+      mobile = make_shared<string>(boost::any_cast<string>(m["Mobile"]));
+    }
+    if (m.find("ParamType") != m.end() && !m["ParamType"].empty()) {
+      paramType = make_shared<string>(boost::any_cast<string>(m["ParamType"]));
+    }
+    if (m.find("UserName") != m.end() && !m["UserName"].empty()) {
+      userName = make_shared<string>(boost::any_cast<string>(m["UserName"]));
+    }
+  }
+
+
+  virtual ~Mobile2MetaVerifyRequest() = default;
+};
+class Mobile2MetaVerifyResponseBodyResultObject : public Darabonba::Model {
+public:
+  shared_ptr<string> bizCode{};
+  shared_ptr<string> ispName{};
+
+  Mobile2MetaVerifyResponseBodyResultObject() {}
+
+  explicit Mobile2MetaVerifyResponseBodyResultObject(const std::map<string, boost::any> &config) : Darabonba::Model(config) {
+    fromMap(config);
+  };
+
+  void validate() override {}
+
+  map<string, boost::any> toMap() override {
+    map<string, boost::any> res;
+    if (bizCode) {
+      res["BizCode"] = boost::any(*bizCode);
+    }
+    if (ispName) {
+      res["IspName"] = boost::any(*ispName);
+    }
+    return res;
+  }
+
+  void fromMap(map<string, boost::any> m) override {
+    if (m.find("BizCode") != m.end() && !m["BizCode"].empty()) {
+      bizCode = make_shared<string>(boost::any_cast<string>(m["BizCode"]));
+    }
+    if (m.find("IspName") != m.end() && !m["IspName"].empty()) {
+      ispName = make_shared<string>(boost::any_cast<string>(m["IspName"]));
+    }
+  }
+
+
+  virtual ~Mobile2MetaVerifyResponseBodyResultObject() = default;
+};
+class Mobile2MetaVerifyResponseBody : public Darabonba::Model {
+public:
+  shared_ptr<string> code{};
+  shared_ptr<string> message{};
+  shared_ptr<string> requestId{};
+  shared_ptr<Mobile2MetaVerifyResponseBodyResultObject> resultObject{};
+
+  Mobile2MetaVerifyResponseBody() {}
+
+  explicit Mobile2MetaVerifyResponseBody(const std::map<string, boost::any> &config) : Darabonba::Model(config) {
+    fromMap(config);
+  };
+
+  void validate() override {}
+
+  map<string, boost::any> toMap() override {
+    map<string, boost::any> res;
+    if (code) {
+      res["Code"] = boost::any(*code);
+    }
+    if (message) {
+      res["Message"] = boost::any(*message);
+    }
+    if (requestId) {
+      res["RequestId"] = boost::any(*requestId);
+    }
+    if (resultObject) {
+      res["ResultObject"] = resultObject ? boost::any(resultObject->toMap()) : boost::any(map<string,boost::any>({}));
+    }
+    return res;
+  }
+
+  void fromMap(map<string, boost::any> m) override {
+    if (m.find("Code") != m.end() && !m["Code"].empty()) {
+      code = make_shared<string>(boost::any_cast<string>(m["Code"]));
+    }
+    if (m.find("Message") != m.end() && !m["Message"].empty()) {
+      message = make_shared<string>(boost::any_cast<string>(m["Message"]));
+    }
+    if (m.find("RequestId") != m.end() && !m["RequestId"].empty()) {
+      requestId = make_shared<string>(boost::any_cast<string>(m["RequestId"]));
+    }
+    if (m.find("ResultObject") != m.end() && !m["ResultObject"].empty()) {
+      if (typeid(map<string, boost::any>) == m["ResultObject"].type()) {
+        Mobile2MetaVerifyResponseBodyResultObject model1;
+        model1.fromMap(boost::any_cast<map<string, boost::any>>(m["ResultObject"]));
+        resultObject = make_shared<Mobile2MetaVerifyResponseBodyResultObject>(model1);
+      }
+    }
+  }
+
+
+  virtual ~Mobile2MetaVerifyResponseBody() = default;
+};
+class Mobile2MetaVerifyResponse : public Darabonba::Model {
+public:
+  shared_ptr<map<string, string>> headers{};
+  shared_ptr<long> statusCode{};
+  shared_ptr<Mobile2MetaVerifyResponseBody> body{};
+
+  Mobile2MetaVerifyResponse() {}
+
+  explicit Mobile2MetaVerifyResponse(const std::map<string, boost::any> &config) : Darabonba::Model(config) {
+    fromMap(config);
+  };
+
+  void validate() override {}
+
+  map<string, boost::any> toMap() override {
+    map<string, boost::any> res;
+    if (headers) {
+      res["headers"] = boost::any(*headers);
+    }
+    if (statusCode) {
+      res["statusCode"] = boost::any(*statusCode);
+    }
+    if (body) {
+      res["body"] = body ? boost::any(body->toMap()) : boost::any(map<string,boost::any>({}));
+    }
+    return res;
+  }
+
+  void fromMap(map<string, boost::any> m) override {
+    if (m.find("headers") != m.end() && !m["headers"].empty()) {
+      map<string, string> map1 = boost::any_cast<map<string, string>>(m["headers"]);
+      map<string, string> toMap1;
+      for (auto item:map1) {
+         toMap1[item.first] = item.second;
+      }
+      headers = make_shared<map<string, string>>(toMap1);
+    }
+    if (m.find("statusCode") != m.end() && !m["statusCode"].empty()) {
+      statusCode = make_shared<long>(boost::any_cast<long>(m["statusCode"]));
+    }
+    if (m.find("body") != m.end() && !m["body"].empty()) {
+      if (typeid(map<string, boost::any>) == m["body"].type()) {
+        Mobile2MetaVerifyResponseBody model1;
+        model1.fromMap(boost::any_cast<map<string, boost::any>>(m["body"]));
+        body = make_shared<Mobile2MetaVerifyResponseBody>(model1);
+      }
+    }
+  }
+
+
+  virtual ~Mobile2MetaVerifyResponse() = default;
+};
 class Mobile3MetaDetailStandardVerifyRequest : public Darabonba::Model {
 public:
   shared_ptr<string> identifyNum{};
@@ -11142,6 +11327,8 @@ public:
   InsertWhiteListSettingResponse insertWhiteListSetting(shared_ptr<InsertWhiteListSettingRequest> request);
   LivenessFaceVerifyResponse livenessFaceVerifyWithOptions(shared_ptr<LivenessFaceVerifyRequest> request, shared_ptr<Darabonba_Util::RuntimeOptions> runtime);
   LivenessFaceVerifyResponse livenessFaceVerify(shared_ptr<LivenessFaceVerifyRequest> request);
+  Mobile2MetaVerifyResponse mobile2MetaVerifyWithOptions(shared_ptr<Mobile2MetaVerifyRequest> request, shared_ptr<Darabonba_Util::RuntimeOptions> runtime);
+  Mobile2MetaVerifyResponse mobile2MetaVerify(shared_ptr<Mobile2MetaVerifyRequest> request);
   Mobile3MetaDetailStandardVerifyResponse mobile3MetaDetailStandardVerifyWithOptions(shared_ptr<Mobile3MetaDetailStandardVerifyRequest> request, shared_ptr<Darabonba_Util::RuntimeOptions> runtime);
   Mobile3MetaDetailStandardVerifyResponse mobile3MetaDetailStandardVerify(shared_ptr<Mobile3MetaDetailStandardVerifyRequest> request);
   Mobile3MetaDetailVerifyResponse mobile3MetaDetailVerifyWithOptions(shared_ptr<Mobile3MetaDetailVerifyRequest> request, shared_ptr<Darabonba_Util::RuntimeOptions> runtime);
--- cloudauth-20190307/src/cloudauth_20190307.cpp
+++ cloudauth-20190307/src/cloudauth_20190307.cpp
@@ -1644,6 +1644,40 @@ LivenessFaceVerifyResponse Alibabacloud_Cloudauth20190307::Client::livenessFaceV
   return livenessFaceVerifyWithOptions(request, runtime);
 }
 
+Mobile2MetaVerifyResponse Alibabacloud_Cloudauth20190307::Client::mobile2MetaVerifyWithOptions(shared_ptr<Mobile2MetaVerifyRequest> request, shared_ptr<Darabonba_Util::RuntimeOptions> runtime) {
+  Darabonba_Util::Client::validateModel(request);
+  shared_ptr<map<string, boost::any>> body = make_shared<map<string, boost::any>>(map<string, boost::any>());
+  if (!Darabonba_Util::Client::isUnset<string>(request->mobile)) {
+    body->insert(pair<string, string>("Mobile", *request->mobile));
+  }
+  if (!Darabonba_Util::Client::isUnset<string>(request->paramType)) {
+    body->insert(pair<string, string>("ParamType", *request->paramType));
+  }
+  if (!Darabonba_Util::Client::isUnset<string>(request->userName)) {
+    body->insert(pair<string, string>("UserName", *request->userName));
+  }
+  shared_ptr<Alibabacloud_OpenApi::OpenApiRequest> req = make_shared<Alibabacloud_OpenApi::OpenApiRequest>(map<string, boost::any>({
+    {"body", boost::any(Alibabacloud_OpenApiUtil::Client::parseToMap(body))}
+  }));
+  shared_ptr<Alibabacloud_OpenApi::Params> params = make_shared<Alibabacloud_OpenApi::Params>(map<string, boost::any>({
+    {"action", boost::any(string("Mobile2MetaVerify"))},
+    {"version", boost::any(string("2019-03-07"))},
+    {"protocol", boost::any(string("HTTPS"))},
+    {"pathname", boost::any(string("/"))},
+    {"method", boost::any(string("POST"))},
+    {"authType", boost::any(string("AK"))},
+    {"style", boost::any(string("RPC"))},
+    {"reqBodyType", boost::any(string("formData"))},
+    {"bodyType", boost::any(string("json"))}
+  }));
+  return Mobile2MetaVerifyResponse(callApi(params, req, runtime));
+}
+
+Mobile2MetaVerifyResponse Alibabacloud_Cloudauth20190307::Client::mobile2MetaVerify(shared_ptr<Mobile2MetaVerifyRequest> request) {
+  shared_ptr<Darabonba_Util::RuntimeOptions> runtime = make_shared<Darabonba_Util::RuntimeOptions>();
+  return mobile2MetaVerifyWithOptions(request, runtime);
+}
+
 Mobile3MetaDetailStandardVerifyResponse Alibabacloud_Cloudauth20190307::Client::mobile3MetaDetailStandardVerifyWithOptions(shared_ptr<Mobile3MetaDetailStandardVerifyRequest> request, shared_ptr<Darabonba_Util::RuntimeOptions> runtime) {
   Darabonba_Util::Client::validateModel(request);
   shared_ptr<map<string, boost::any>> body = make_shared<map<string, boost::any>>(map<string, boost::any>());
