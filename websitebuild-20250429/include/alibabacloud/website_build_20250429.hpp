c970c77aff217cb9acee0c617e9b68419ed6f577
sdk-team <sdk-team@alibabacloud.com>
2025-06-12 08:43:50 +0000

    Support API OperateAppInstanceForPartner.

--- websitebuild-20250429/include/alibabacloud/website_build_20250429.hpp
+++ websitebuild-20250429/include/alibabacloud/website_build_20250429.hpp
@@ -12,6 +12,144 @@
 using namespace std;
 
 namespace Alibabacloud_WebsiteBuild20250429 {
+class OperateAppInstanceForPartnerRequest : public Darabonba::Model {
+public:
+  shared_ptr<string> extend{};
+  shared_ptr<string> operateEvent{};
+
+  OperateAppInstanceForPartnerRequest() {}
+
+  explicit OperateAppInstanceForPartnerRequest(const std::map<string, boost::any> &config) : Darabonba::Model(config) {
+    fromMap(config);
+  };
+
+  void validate() override {}
+
+  map<string, boost::any> toMap() override {
+    map<string, boost::any> res;
+    if (extend) {
+      res["Extend"] = boost::any(*extend);
+    }
+    if (operateEvent) {
+      res["OperateEvent"] = boost::any(*operateEvent);
+    }
+    return res;
+  }
+
+  void fromMap(map<string, boost::any> m) override {
+    if (m.find("Extend") != m.end() && !m["Extend"].empty()) {
+      extend = make_shared<string>(boost::any_cast<string>(m["Extend"]));
+    }
+    if (m.find("OperateEvent") != m.end() && !m["OperateEvent"].empty()) {
+      operateEvent = make_shared<string>(boost::any_cast<string>(m["OperateEvent"]));
+    }
+  }
+
+
+  virtual ~OperateAppInstanceForPartnerRequest() = default;
+};
+class OperateAppInstanceForPartnerResponseBody : public Darabonba::Model {
+public:
+  shared_ptr<string> errorCode{};
+  shared_ptr<string> errorMsg{};
+  shared_ptr<string> requestId{};
+  shared_ptr<string> success{};
+
+  OperateAppInstanceForPartnerResponseBody() {}
+
+  explicit OperateAppInstanceForPartnerResponseBody(const std::map<string, boost::any> &config) : Darabonba::Model(config) {
+    fromMap(config);
+  };
+
+  void validate() override {}
+
+  map<string, boost::any> toMap() override {
+    map<string, boost::any> res;
+    if (errorCode) {
+      res["ErrorCode"] = boost::any(*errorCode);
+    }
+    if (errorMsg) {
+      res["ErrorMsg"] = boost::any(*errorMsg);
+    }
+    if (requestId) {
+      res["RequestId"] = boost::any(*requestId);
+    }
+    if (success) {
+      res["Success"] = boost::any(*success);
+    }
+    return res;
+  }
+
+  void fromMap(map<string, boost::any> m) override {
+    if (m.find("ErrorCode") != m.end() && !m["ErrorCode"].empty()) {
+      errorCode = make_shared<string>(boost::any_cast<string>(m["ErrorCode"]));
+    }
+    if (m.find("ErrorMsg") != m.end() && !m["ErrorMsg"].empty()) {
+      errorMsg = make_shared<string>(boost::any_cast<string>(m["ErrorMsg"]));
+    }
+    if (m.find("RequestId") != m.end() && !m["RequestId"].empty()) {
+      requestId = make_shared<string>(boost::any_cast<string>(m["RequestId"]));
+    }
+    if (m.find("Success") != m.end() && !m["Success"].empty()) {
+      success = make_shared<string>(boost::any_cast<string>(m["Success"]));
+    }
+  }
+
+
+  virtual ~OperateAppInstanceForPartnerResponseBody() = default;
+};
+class OperateAppInstanceForPartnerResponse : public Darabonba::Model {
+public:
+  shared_ptr<map<string, string>> headers{};
+  shared_ptr<long> statusCode{};
+  shared_ptr<OperateAppInstanceForPartnerResponseBody> body{};
+
+  OperateAppInstanceForPartnerResponse() {}
+
+  explicit OperateAppInstanceForPartnerResponse(const std::map<string, boost::any> &config) : Darabonba::Model(config) {
+    fromMap(config);
+  };
+
+  void validate() override {}
+
+  map<string, boost::any> toMap() override {
+    map<string, boost::any> res;
+    if (headers) {
+      res["headers"] = boost::any(*headers);
+    }
+    if (statusCode) {
+      res["statusCode"] = boost::any(*statusCode);
+    }
+    if (body) {
+      res["body"] = body ? boost::any(body->toMap()) : boost::any(map<string,boost::any>({}));
+    }
+    return res;
+  }
+
+  void fromMap(map<string, boost::any> m) override {
+    if (m.find("headers") != m.end() && !m["headers"].empty()) {
+      map<string, string> map1 = boost::any_cast<map<string, string>>(m["headers"]);
+      map<string, string> toMap1;
+      for (auto item:map1) {
+         toMap1[item.first] = item.second;
+      }
+      headers = make_shared<map<string, string>>(toMap1);
+    }
+    if (m.find("statusCode") != m.end() && !m["statusCode"].empty()) {
+      statusCode = make_shared<long>(boost::any_cast<long>(m["statusCode"]));
+    }
+    if (m.find("body") != m.end() && !m["body"].empty()) {
+      if (typeid(map<string, boost::any>) == m["body"].type()) {
+        OperateAppInstanceForPartnerResponseBody model1;
+        model1.fromMap(boost::any_cast<map<string, boost::any>>(m["body"]));
+        body = make_shared<OperateAppInstanceForPartnerResponseBody>(model1);
+      }
+    }
+  }
+
+
+  virtual ~OperateAppInstanceForPartnerResponse() = default;
+};
 class OperateAppServiceForPartnerRequest : public Darabonba::Model {
 public:
   shared_ptr<string> bizId{};
@@ -174,6 +312,8 @@ public:
                      shared_ptr<string> suffix,
                      shared_ptr<map<string, string>> endpointMap,
                      shared_ptr<string> endpoint);
+  OperateAppInstanceForPartnerResponse operateAppInstanceForPartnerWithOptions(shared_ptr<OperateAppInstanceForPartnerRequest> request, shared_ptr<Darabonba_Util::RuntimeOptions> runtime);
+  OperateAppInstanceForPartnerResponse operateAppInstanceForPartner(shared_ptr<OperateAppInstanceForPartnerRequest> request);
   OperateAppServiceForPartnerResponse operateAppServiceForPartnerWithOptions(shared_ptr<OperateAppServiceForPartnerRequest> request, shared_ptr<Darabonba_Util::RuntimeOptions> runtime);
   OperateAppServiceForPartnerResponse operateAppServiceForPartner(shared_ptr<OperateAppServiceForPartnerRequest> request);
 
--- websitebuild-20250429/src/website_build_20250429.cpp
+++ websitebuild-20250429/src/website_build_20250429.cpp
@@ -36,6 +36,37 @@ string Alibabacloud_WebsiteBuild20250429::Client::getEndpoint(shared_ptr<string>
   return Alibabacloud_EndpointUtil::Client::getEndpointRules(productId, regionId, endpointRule, network, suffix);
 }
 
+OperateAppInstanceForPartnerResponse Alibabacloud_WebsiteBuild20250429::Client::operateAppInstanceForPartnerWithOptions(shared_ptr<OperateAppInstanceForPartnerRequest> request, shared_ptr<Darabonba_Util::RuntimeOptions> runtime) {
+  Darabonba_Util::Client::validateModel(request);
+  shared_ptr<map<string, boost::any>> query = make_shared<map<string, boost::any>>(map<string, boost::any>());
+  if (!Darabonba_Util::Client::isUnset<string>(request->extend)) {
+    query->insert(pair<string, string>("Extend", *request->extend));
+  }
+  if (!Darabonba_Util::Client::isUnset<string>(request->operateEvent)) {
+    query->insert(pair<string, string>("OperateEvent", *request->operateEvent));
+  }
+  shared_ptr<Alibabacloud_OpenApi::OpenApiRequest> req = make_shared<Alibabacloud_OpenApi::OpenApiRequest>(map<string, boost::any>({
+    {"query", boost::any(Alibabacloud_OpenApiUtil::Client::query(query))}
+  }));
+  shared_ptr<Alibabacloud_OpenApi::Params> params = make_shared<Alibabacloud_OpenApi::Params>(map<string, boost::any>({
+    {"action", boost::any(string("OperateAppInstanceForPartner"))},
+    {"version", boost::any(string("2025-04-29"))},
+    {"protocol", boost::any(string("HTTPS"))},
+    {"pathname", boost::any(string("/"))},
+    {"method", boost::any(string("POST"))},
+    {"authType", boost::any(string("AK"))},
+    {"style", boost::any(string("RPC"))},
+    {"reqBodyType", boost::any(string("formData"))},
+    {"bodyType", boost::any(string("json"))}
+  }));
+  return OperateAppInstanceForPartnerResponse(callApi(params, req, runtime));
+}
+
+OperateAppInstanceForPartnerResponse Alibabacloud_WebsiteBuild20250429::Client::operateAppInstanceForPartner(shared_ptr<OperateAppInstanceForPartnerRequest> request) {
+  shared_ptr<Darabonba_Util::RuntimeOptions> runtime = make_shared<Darabonba_Util::RuntimeOptions>();
+  return operateAppInstanceForPartnerWithOptions(request, runtime);
+}
+
 OperateAppServiceForPartnerResponse Alibabacloud_WebsiteBuild20250429::Client::operateAppServiceForPartnerWithOptions(shared_ptr<OperateAppServiceForPartnerRequest> request, shared_ptr<Darabonba_Util::RuntimeOptions> runtime) {
   Darabonba_Util::Client::validateModel(request);
   shared_ptr<map<string, boost::any>> query = make_shared<map<string, boost::any>>(map<string, boost::any>());
