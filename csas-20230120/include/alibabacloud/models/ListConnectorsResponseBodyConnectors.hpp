c865857716c31a4996323a73062fbd033144b1c4
sdk-team <sdk-team@alibabacloud.com>
2025-09-09 17:43:48 +0000

    Update API ListConnectors: add response parameters Body.Connectors.$.ClusterIP.

--- csas-20230120/include/alibabacloud/models/ListConnectorsResponseBodyConnectors.hpp
+++ csas-20230120/include/alibabacloud/models/ListConnectorsResponseBodyConnectors.hpp
@@ -18,6 +18,8 @@ namespace Models
   public:
     friend void to_json(Darabonba::Json& j, const ListConnectorsResponseBodyConnectors& obj) { 
       DARABONBA_PTR_TO_JSON(Applications, applications_);
+      DARABONBA_PTR_TO_JSON(ClusterIP, clusterIP_);
+      DARABONBA_PTR_TO_JSON(ClusterPort, clusterPort_);
       DARABONBA_PTR_TO_JSON(ConnectorClients, connectorClients_);
       DARABONBA_PTR_TO_JSON(ConnectorId, connectorId_);
       DARABONBA_PTR_TO_JSON(CreateTime, createTime_);
@@ -29,6 +31,8 @@ namespace Models
     };
     friend void from_json(const Darabonba::Json& j, ListConnectorsResponseBodyConnectors& obj) { 
       DARABONBA_PTR_FROM_JSON(Applications, applications_);
+      DARABONBA_PTR_FROM_JSON(ClusterIP, clusterIP_);
+      DARABONBA_PTR_FROM_JSON(ClusterPort, clusterPort_);
       DARABONBA_PTR_FROM_JSON(ConnectorClients, connectorClients_);
       DARABONBA_PTR_FROM_JSON(ConnectorId, connectorId_);
       DARABONBA_PTR_FROM_JSON(CreateTime, createTime_);
@@ -50,8 +54,8 @@ namespace Models
     virtual void fromMap(const Darabonba::Json &obj) override { from_json(obj, *this); validate(); };
     virtual Darabonba::Json toMap() const override { Darabonba::Json obj; to_json(obj, *this); return obj; };
     virtual bool empty() const override { this->applications_ != nullptr
-        && this->connectorClients_ != nullptr && this->connectorId_ != nullptr && this->createTime_ != nullptr && this->name_ != nullptr && this->regionId_ != nullptr
-        && this->status_ != nullptr && this->switchStatus_ != nullptr && this->upgradeTime_ != nullptr; };
+        && this->clusterIP_ != nullptr && this->clusterPort_ != nullptr && this->connectorClients_ != nullptr && this->connectorId_ != nullptr && this->createTime_ != nullptr
+        && this->name_ != nullptr && this->regionId_ != nullptr && this->status_ != nullptr && this->switchStatus_ != nullptr && this->upgradeTime_ != nullptr; };
     // applications Field Functions 
     bool hasApplications() const { return this->applications_ != nullptr;};
     void deleteApplications() { this->applications_ = nullptr;};
@@ -59,6 +63,20 @@ namespace Models
     inline vector<Models::ListConnectorsResponseBodyConnectorsApplications> applications() { DARABONBA_PTR_GET(applications_, vector<Models::ListConnectorsResponseBodyConnectorsApplications>) };
     inline ListConnectorsResponseBodyConnectors& setApplications(const vector<Models::ListConnectorsResponseBodyConnectorsApplications> & applications) { DARABONBA_PTR_SET_VALUE(applications_, applications) };
     inline ListConnectorsResponseBodyConnectors& setApplications(vector<Models::ListConnectorsResponseBodyConnectorsApplications> && applications) { DARABONBA_PTR_SET_RVALUE(applications_, applications) };
+
+
+    // clusterIP Field Functions 
+    bool hasClusterIP() const { return this->clusterIP_ != nullptr;};
+    void deleteClusterIP() { this->clusterIP_ = nullptr;};
+    inline string clusterIP() const { DARABONBA_PTR_GET_DEFAULT(clusterIP_, "") };
+    inline ListConnectorsResponseBodyConnectors& setClusterIP(string clusterIP) { DARABONBA_PTR_SET_VALUE(clusterIP_, clusterIP) };
+
+
+    // clusterPort Field Functions 
+    bool hasClusterPort() const { return this->clusterPort_ != nullptr;};
+    void deleteClusterPort() { this->clusterPort_ = nullptr;};
+    inline string clusterPort() const { DARABONBA_PTR_GET_DEFAULT(clusterPort_, "") };
+    inline ListConnectorsResponseBodyConnectors& setClusterPort(string clusterPort) { DARABONBA_PTR_SET_VALUE(clusterPort_, clusterPort) };
 
 
     // connectorClients Field Functions 
@@ -123,6 +141,8 @@ namespace Models
 
   protected:
     std::shared_ptr<vector<Models::ListConnectorsResponseBodyConnectorsApplications>> applications_ = nullptr;
+    std::shared_ptr<string> clusterIP_ = nullptr;
+    std::shared_ptr<string> clusterPort_ = nullptr;
     std::shared_ptr<vector<Models::ListConnectorsResponseBodyConnectorsConnectorClients>> connectorClients_ = nullptr;
     // ConnectorID。
     std::shared_ptr<string> connectorId_ = nullptr;
