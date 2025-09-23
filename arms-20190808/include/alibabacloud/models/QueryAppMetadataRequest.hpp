617b538b7294d600849cfb4f2a23b0efd7268640
sdk-team <sdk-team@alibabacloud.com>
2025-09-23 17:39:51 +0000

    Update API QueryAppMetadata: add request parameters EndTimeMs.

--- arms-20190808/include/alibabacloud/models/QueryAppMetadataRequest.hpp
+++ arms-20190808/include/alibabacloud/models/QueryAppMetadataRequest.hpp
@@ -13,16 +13,20 @@ namespace Models
   class QueryAppMetadataRequest : public Darabonba::Model {
   public:
     friend void to_json(Darabonba::Json& j, const QueryAppMetadataRequest& obj) { 
+      DARABONBA_PTR_TO_JSON(EndTimeMs, endTimeMs_);
       DARABONBA_PTR_TO_JSON(MetaIds, metaIds_);
       DARABONBA_PTR_TO_JSON(MetaType, metaType_);
       DARABONBA_PTR_TO_JSON(Pid, pid_);
       DARABONBA_PTR_TO_JSON(RegionId, regionId_);
+      DARABONBA_PTR_TO_JSON(StartTimeMs, startTimeMs_);
     };
     friend void from_json(const Darabonba::Json& j, QueryAppMetadataRequest& obj) { 
+      DARABONBA_PTR_FROM_JSON(EndTimeMs, endTimeMs_);
       DARABONBA_PTR_FROM_JSON(MetaIds, metaIds_);
       DARABONBA_PTR_FROM_JSON(MetaType, metaType_);
       DARABONBA_PTR_FROM_JSON(Pid, pid_);
       DARABONBA_PTR_FROM_JSON(RegionId, regionId_);
+      DARABONBA_PTR_FROM_JSON(StartTimeMs, startTimeMs_);
     };
     QueryAppMetadataRequest() = default ;
     QueryAppMetadataRequest(const QueryAppMetadataRequest &) = default ;
@@ -35,8 +39,15 @@ namespace Models
     };
     virtual void fromMap(const Darabonba::Json &obj) override { from_json(obj, *this); validate(); };
     virtual Darabonba::Json toMap() const override { Darabonba::Json obj; to_json(obj, *this); return obj; };
-    virtual bool empty() const override { this->metaIds_ != nullptr
-        && this->metaType_ != nullptr && this->pid_ != nullptr && this->regionId_ != nullptr; };
+    virtual bool empty() const override { this->endTimeMs_ != nullptr
+        && this->metaIds_ != nullptr && this->metaType_ != nullptr && this->pid_ != nullptr && this->regionId_ != nullptr && this->startTimeMs_ != nullptr; };
+    // endTimeMs Field Functions 
+    bool hasEndTimeMs() const { return this->endTimeMs_ != nullptr;};
+    void deleteEndTimeMs() { this->endTimeMs_ = nullptr;};
+    inline int64_t endTimeMs() const { DARABONBA_PTR_GET_DEFAULT(endTimeMs_, 0L) };
+    inline QueryAppMetadataRequest& setEndTimeMs(int64_t endTimeMs) { DARABONBA_PTR_SET_VALUE(endTimeMs_, endTimeMs) };
+
+
     // metaIds Field Functions 
     bool hasMetaIds() const { return this->metaIds_ != nullptr;};
     void deleteMetaIds() { this->metaIds_ = nullptr;};
@@ -65,7 +76,15 @@ namespace Models
     inline QueryAppMetadataRequest& setRegionId(string regionId) { DARABONBA_PTR_SET_VALUE(regionId_, regionId) };
 
 
+    // startTimeMs Field Functions 
+    bool hasStartTimeMs() const { return this->startTimeMs_ != nullptr;};
+    void deleteStartTimeMs() { this->startTimeMs_ = nullptr;};
+    inline int64_t startTimeMs() const { DARABONBA_PTR_GET_DEFAULT(startTimeMs_, 0L) };
+    inline QueryAppMetadataRequest& setStartTimeMs(int64_t startTimeMs) { DARABONBA_PTR_SET_VALUE(startTimeMs_, startTimeMs) };
+
+
   protected:
+    std::shared_ptr<int64_t> endTimeMs_ = nullptr;
     // The metadata IDs. Separate multiple IDs with commas (,).
     // 
     // You can obtain the exception ID on the **Exception Analysis** page of your application in the ARMS console.
@@ -87,6 +106,7 @@ namespace Models
     // 
     // This parameter is required.
     std::shared_ptr<string> regionId_ = nullptr;
+    std::shared_ptr<int64_t> startTimeMs_ = nullptr;
   };
 
   } // namespace Models
