85877d06f7aba27bf3d57f4f0060ccf65166f89f
sdk-team <sdk-team@alibabacloud.com>
2025-10-09 17:30:23 +0000

    Update API GetConsumerProgress: add response parameters Body.ConsumerProgress.TopicList.$.OffsetList.$.Accumulate.

--- alikafka-20190916/include/alibabacloud/models/GetConsumerProgressResponseBodyConsumerProgressTopicListTopicListOffsetListOffsetList.hpp
+++ alikafka-20190916/include/alibabacloud/models/GetConsumerProgressResponseBodyConsumerProgressTopicListTopicListOffsetListOffsetList.hpp
@@ -13,6 +13,7 @@ namespace Models
   class GetConsumerProgressResponseBodyConsumerProgressTopicListTopicListOffsetListOffsetList : public Darabonba::Model {
   public:
     friend void to_json(Darabonba::Json& j, const GetConsumerProgressResponseBodyConsumerProgressTopicListTopicListOffsetListOffsetList& obj) { 
+      DARABONBA_PTR_TO_JSON(Accumulate, accumulate_);
       DARABONBA_PTR_TO_JSON(BrokerOffset, brokerOffset_);
       DARABONBA_PTR_TO_JSON(ClientId, clientId_);
       DARABONBA_PTR_TO_JSON(ClientIp, clientIp_);
@@ -22,6 +23,7 @@ namespace Models
       DARABONBA_PTR_TO_JSON(Partition, partition_);
     };
     friend void from_json(const Darabonba::Json& j, GetConsumerProgressResponseBodyConsumerProgressTopicListTopicListOffsetListOffsetList& obj) { 
+      DARABONBA_PTR_FROM_JSON(Accumulate, accumulate_);
       DARABONBA_PTR_FROM_JSON(BrokerOffset, brokerOffset_);
       DARABONBA_PTR_FROM_JSON(ClientId, clientId_);
       DARABONBA_PTR_FROM_JSON(ClientIp, clientIp_);
@@ -41,9 +43,16 @@ namespace Models
     };
     virtual void fromMap(const Darabonba::Json &obj) override { from_json(obj, *this); validate(); };
     virtual Darabonba::Json toMap() const override { Darabonba::Json obj; to_json(obj, *this); return obj; };
-    virtual bool empty() const override { this->brokerOffset_ != nullptr
-        && this->clientId_ != nullptr && this->clientIp_ != nullptr && this->consumerOffset_ != nullptr && this->lastTimestamp_ != nullptr && this->memberId_ != nullptr
-        && this->partition_ != nullptr; };
+    virtual bool empty() const override { this->accumulate_ != nullptr
+        && this->brokerOffset_ != nullptr && this->clientId_ != nullptr && this->clientIp_ != nullptr && this->consumerOffset_ != nullptr && this->lastTimestamp_ != nullptr
+        && this->memberId_ != nullptr && this->partition_ != nullptr; };
+    // accumulate Field Functions 
+    bool hasAccumulate() const { return this->accumulate_ != nullptr;};
+    void deleteAccumulate() { this->accumulate_ = nullptr;};
+    inline int64_t accumulate() const { DARABONBA_PTR_GET_DEFAULT(accumulate_, 0L) };
+    inline GetConsumerProgressResponseBodyConsumerProgressTopicListTopicListOffsetListOffsetList& setAccumulate(int64_t accumulate) { DARABONBA_PTR_SET_VALUE(accumulate_, accumulate) };
+
+
     // brokerOffset Field Functions 
     bool hasBrokerOffset() const { return this->brokerOffset_ != nullptr;};
     void deleteBrokerOffset() { this->brokerOffset_ = nullptr;};
@@ -94,6 +103,7 @@ namespace Models
 
 
   protected:
+    std::shared_ptr<int64_t> accumulate_ = nullptr;
     // The latest offset in the partition of the topic.
     std::shared_ptr<int64_t> brokerOffset_ = nullptr;
     // Client ID of the application.
