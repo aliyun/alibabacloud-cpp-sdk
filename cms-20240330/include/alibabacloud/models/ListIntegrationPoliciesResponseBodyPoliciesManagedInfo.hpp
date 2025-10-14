1470bbc37db616732cbdb8a77ab50df60bd14fd5
sdk-team <sdk-team@alibabacloud.com>
2025-10-14 17:34:18 +0000

    Update API ListIntegrationPolicies: add response parameters Body.policies.$.managedInfo.eniId.

--- cms-20240330/include/alibabacloud/models/ListIntegrationPoliciesResponseBodyPoliciesManagedInfo.hpp
+++ cms-20240330/include/alibabacloud/models/ListIntegrationPoliciesResponseBodyPoliciesManagedInfo.hpp
@@ -13,10 +13,12 @@ namespace Models
   class ListIntegrationPoliciesResponseBodyPoliciesManagedInfo : public Darabonba::Model {
   public:
     friend void to_json(Darabonba::Json& j, const ListIntegrationPoliciesResponseBodyPoliciesManagedInfo& obj) { 
+      DARABONBA_PTR_TO_JSON(eniId, eniId_);
       DARABONBA_PTR_TO_JSON(securityGroupId, securityGroupId_);
       DARABONBA_PTR_TO_JSON(vswitchId, vswitchId_);
     };
     friend void from_json(const Darabonba::Json& j, ListIntegrationPoliciesResponseBodyPoliciesManagedInfo& obj) { 
+      DARABONBA_PTR_FROM_JSON(eniId, eniId_);
       DARABONBA_PTR_FROM_JSON(securityGroupId, securityGroupId_);
       DARABONBA_PTR_FROM_JSON(vswitchId, vswitchId_);
     };
@@ -31,8 +33,15 @@ namespace Models
     };
     virtual void fromMap(const Darabonba::Json &obj) override { from_json(obj, *this); validate(); };
     virtual Darabonba::Json toMap() const override { Darabonba::Json obj; to_json(obj, *this); return obj; };
-    virtual bool empty() const override { return this->securityGroupId_ == nullptr
-        && return this->vswitchId_ == nullptr; };
+    virtual bool empty() const override { return this->eniId_ == nullptr
+        && return this->securityGroupId_ == nullptr && return this->vswitchId_ == nullptr; };
+    // eniId Field Functions 
+    bool hasEniId() const { return this->eniId_ != nullptr;};
+    void deleteEniId() { this->eniId_ = nullptr;};
+    inline string eniId() const { DARABONBA_PTR_GET_DEFAULT(eniId_, "") };
+    inline ListIntegrationPoliciesResponseBodyPoliciesManagedInfo& setEniId(string eniId) { DARABONBA_PTR_SET_VALUE(eniId_, eniId) };
+
+
     // securityGroupId Field Functions 
     bool hasSecurityGroupId() const { return this->securityGroupId_ != nullptr;};
     void deleteSecurityGroupId() { this->securityGroupId_ = nullptr;};
@@ -48,6 +57,7 @@ namespace Models
 
 
   protected:
+    std::shared_ptr<string> eniId_ = nullptr;
     std::shared_ptr<string> securityGroupId_ = nullptr;
     std::shared_ptr<string> vswitchId_ = nullptr;
   };
