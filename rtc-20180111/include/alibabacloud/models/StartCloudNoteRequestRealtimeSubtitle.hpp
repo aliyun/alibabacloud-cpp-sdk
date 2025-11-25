4eff8f56e6b6bb54b86fc4265e8c566db7fd59cc
sdk-team <sdk-team@alibabacloud.com>
2025-11-25 09:01:57 +0000

    Update API StartCloudNote: add request parameters RealtimeSubtitle.AsrCallback.

--- rtc-20180111/include/alibabacloud/models/StartCloudNoteRequestRealtimeSubtitle.hpp
+++ rtc-20180111/include/alibabacloud/models/StartCloudNoteRequestRealtimeSubtitle.hpp
@@ -14,10 +14,12 @@ namespace Models
   class StartCloudNoteRequestRealtimeSubtitle : public Darabonba::Model {
   public:
     friend void to_json(Darabonba::Json& j, const StartCloudNoteRequestRealtimeSubtitle& obj) { 
+      DARABONBA_PTR_TO_JSON(AsrCallback, asrCallback_);
       DARABONBA_PTR_TO_JSON(Enabled, enabled_);
       DARABONBA_PTR_TO_JSON(Translation, translation_);
     };
     friend void from_json(const Darabonba::Json& j, StartCloudNoteRequestRealtimeSubtitle& obj) { 
+      DARABONBA_PTR_FROM_JSON(AsrCallback, asrCallback_);
       DARABONBA_PTR_FROM_JSON(Enabled, enabled_);
       DARABONBA_PTR_FROM_JSON(Translation, translation_);
     };
@@ -32,8 +34,15 @@ namespace Models
     };
     virtual void fromMap(const Darabonba::Json &obj) override { from_json(obj, *this); validate(); };
     virtual Darabonba::Json toMap() const override { Darabonba::Json obj; to_json(obj, *this); return obj; };
-    virtual bool empty() const override { return this->enabled_ == nullptr
-        && return this->translation_ == nullptr; };
+    virtual bool empty() const override { return this->asrCallback_ == nullptr
+        && return this->enabled_ == nullptr && return this->translation_ == nullptr; };
+    // asrCallback Field Functions 
+    bool hasAsrCallback() const { return this->asrCallback_ != nullptr;};
+    void deleteAsrCallback() { this->asrCallback_ = nullptr;};
+    inline bool asrCallback() const { DARABONBA_PTR_GET_DEFAULT(asrCallback_, false) };
+    inline StartCloudNoteRequestRealtimeSubtitle& setAsrCallback(bool asrCallback) { DARABONBA_PTR_SET_VALUE(asrCallback_, asrCallback) };
+
+
     // enabled Field Functions 
     bool hasEnabled() const { return this->enabled_ != nullptr;};
     void deleteEnabled() { this->enabled_ = nullptr;};
@@ -51,6 +60,7 @@ namespace Models
 
 
   protected:
+    std::shared_ptr<bool> asrCallback_ = nullptr;
     std::shared_ptr<bool> enabled_ = nullptr;
     std::shared_ptr<Models::StartCloudNoteRequestRealtimeSubtitleTranslation> translation_ = nullptr;
   };
