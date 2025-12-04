37e0ec4dc104ac0e2d845420bb0baac19bd9e5d8
sdk-team <sdk-team@alibabacloud.com>
2025-12-04 19:00:43 +0000

    Update API DescribeNetworkInterfaces: add response parameters Body.NetworkInterfaceSets.$.PrivateIpSets.$.PrivateIpStatus.

--- ens-20171110/include/alibabacloud/models/DescribeNetworkInterfacesResponseBodyNetworkInterfaceSetsNetworkInterfaceSetPrivateIpSetsPrivateIpSet.hpp
+++ ens-20171110/include/alibabacloud/models/DescribeNetworkInterfacesResponseBodyNetworkInterfaceSetsNetworkInterfaceSetPrivateIpSetsPrivateIpSet.hpp
@@ -15,10 +15,12 @@ namespace Models
     friend void to_json(Darabonba::Json& j, const DescribeNetworkInterfacesResponseBodyNetworkInterfaceSetsNetworkInterfaceSetPrivateIpSetsPrivateIpSet& obj) { 
       DARABONBA_PTR_TO_JSON(Primary, primary_);
       DARABONBA_PTR_TO_JSON(PrivateIpAddress, privateIpAddress_);
+      DARABONBA_PTR_TO_JSON(PrivateIpStatus, privateIpStatus_);
     };
     friend void from_json(const Darabonba::Json& j, DescribeNetworkInterfacesResponseBodyNetworkInterfaceSetsNetworkInterfaceSetPrivateIpSetsPrivateIpSet& obj) { 
       DARABONBA_PTR_FROM_JSON(Primary, primary_);
       DARABONBA_PTR_FROM_JSON(PrivateIpAddress, privateIpAddress_);
+      DARABONBA_PTR_FROM_JSON(PrivateIpStatus, privateIpStatus_);
     };
     DescribeNetworkInterfacesResponseBodyNetworkInterfaceSetsNetworkInterfaceSetPrivateIpSetsPrivateIpSet() = default ;
     DescribeNetworkInterfacesResponseBodyNetworkInterfaceSetsNetworkInterfaceSetPrivateIpSetsPrivateIpSet(const DescribeNetworkInterfacesResponseBodyNetworkInterfaceSetsNetworkInterfaceSetPrivateIpSetsPrivateIpSet &) = default ;
@@ -32,7 +34,7 @@ namespace Models
     virtual void fromMap(const Darabonba::Json &obj) override { from_json(obj, *this); validate(); };
     virtual Darabonba::Json toMap() const override { Darabonba::Json obj; to_json(obj, *this); return obj; };
     virtual bool empty() const override { return this->primary_ == nullptr
-        && return this->privateIpAddress_ == nullptr; };
+        && return this->privateIpAddress_ == nullptr && return this->privateIpStatus_ == nullptr; };
     // primary Field Functions 
     bool hasPrimary() const { return this->primary_ != nullptr;};
     void deletePrimary() { this->primary_ = nullptr;};
@@ -47,6 +49,13 @@ namespace Models
     inline DescribeNetworkInterfacesResponseBodyNetworkInterfaceSetsNetworkInterfaceSetPrivateIpSetsPrivateIpSet& setPrivateIpAddress(string privateIpAddress) { DARABONBA_PTR_SET_VALUE(privateIpAddress_, privateIpAddress) };
 
 
+    // privateIpStatus Field Functions 
+    bool hasPrivateIpStatus() const { return this->privateIpStatus_ != nullptr;};
+    void deletePrivateIpStatus() { this->privateIpStatus_ = nullptr;};
+    inline string privateIpStatus() const { DARABONBA_PTR_GET_DEFAULT(privateIpStatus_, "") };
+    inline DescribeNetworkInterfacesResponseBodyNetworkInterfaceSetsNetworkInterfaceSetPrivateIpSetsPrivateIpSet& setPrivateIpStatus(string privateIpStatus) { DARABONBA_PTR_SET_VALUE(privateIpStatus_, privateIpStatus) };
+
+
   protected:
     // Specifies whether the private IP address is the primary private IP address. Valid values:
     // 
@@ -55,6 +64,7 @@ namespace Models
     std::shared_ptr<bool> primary_ = nullptr;
     // The private IP address.
     std::shared_ptr<string> privateIpAddress_ = nullptr;
+    std::shared_ptr<string> privateIpStatus_ = nullptr;
   };
 
   } // namespace Models
