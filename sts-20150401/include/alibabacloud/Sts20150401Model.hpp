6b9f1866502b9a37bb97f0e5c8af59e072bee24c
sdk-team <sdk-team@alibabacloud.com>
2026-01-12 10:20:07 +0000

    Generated cpp 2015-04-01 for Sts.

--- sts-20150401/include/alibabacloud/Sts20150401Model.hpp
+++ sts-20150401/include/alibabacloud/Sts20150401Model.hpp
@@ -5,12 +5,12 @@
 #include <alibabacloud/models/AssumeRoleRequest.hpp>
 #include <alibabacloud/models/AssumeRoleResponseBody.hpp>
 #include <alibabacloud/models/AssumeRoleResponse.hpp>
-#include <alibabacloud/models/AssumeRoleWithOidcRequest.hpp>
-#include <alibabacloud/models/AssumeRoleWithOidcResponseBody.hpp>
-#include <alibabacloud/models/AssumeRoleWithOidcResponse.hpp>
-#include <alibabacloud/models/AssumeRoleWithSamlRequest.hpp>
-#include <alibabacloud/models/AssumeRoleWithSamlResponseBody.hpp>
-#include <alibabacloud/models/AssumeRoleWithSamlResponse.hpp>
+#include <alibabacloud/models/AssumeRoleWithOIDCRequest.hpp>
+#include <alibabacloud/models/AssumeRoleWithOIDCResponseBody.hpp>
+#include <alibabacloud/models/AssumeRoleWithOIDCResponse.hpp>
+#include <alibabacloud/models/AssumeRoleWithSAMLRequest.hpp>
+#include <alibabacloud/models/AssumeRoleWithSAMLResponseBody.hpp>
+#include <alibabacloud/models/AssumeRoleWithSAMLResponse.hpp>
 #include <alibabacloud/models/GetCallerIdentityResponseBody.hpp>
 #include <alibabacloud/models/GetCallerIdentityResponse.hpp>
 
