9ef4922c4cb8077a1c83f3edbcd827e3bf23df15
sdk-team <sdk-team@alibabacloud.com>
2026-01-27 12:06:35 +0000

    Update API ListInstance: add response parameters Body.Result.$.zoneCount.

--- elasticsearch-20170613/include/alibabacloud/models/ListInstanceResponseBody.hpp
+++ elasticsearch-20170613/include/alibabacloud/models/ListInstanceResponseBody.hpp
@@ -69,6 +69,8 @@ namespace Models
         DARABONBA_PTR_TO_JSON(tags, tags_);
         DARABONBA_PTR_TO_JSON(updatedAt, updatedAt_);
         DARABONBA_PTR_TO_JSON(vpcInstanceId, vpcInstanceId_);
+        DARABONBA_PTR_TO_JSON(zoneCount, zoneCount_);
+        DARABONBA_PTR_TO_JSON(zoneInfos, zoneInfos_);
       };
       friend void from_json(const Darabonba::Json& j, Result& obj) { 
         DARABONBA_PTR_FROM_JSON(advancedDedicateMaster, advancedDedicateMaster_);
@@ -103,6 +105,8 @@ namespace Models
         DARABONBA_PTR_FROM_JSON(tags, tags_);
         DARABONBA_PTR_FROM_JSON(updatedAt, updatedAt_);
         DARABONBA_PTR_FROM_JSON(vpcInstanceId, vpcInstanceId_);
+        DARABONBA_PTR_FROM_JSON(zoneCount, zoneCount_);
+        DARABONBA_PTR_FROM_JSON(zoneInfos, zoneInfos_);
       };
       Result() = default ;
       Result(const Result &) = default ;
@@ -115,6 +119,48 @@ namespace Models
       };
       virtual void fromMap(const Darabonba::Json &obj) override { from_json(obj, *this); validate(); };
       virtual Darabonba::Json toMap() const override { Darabonba::Json obj; to_json(obj, *this); return obj; };
+      class ZoneInfos : public Darabonba::Model {
+      public:
+        friend void to_json(Darabonba::Json& j, const ZoneInfos& obj) { 
+          DARABONBA_PTR_TO_JSON(status, status_);
+          DARABONBA_PTR_TO_JSON(zoneId, zoneId_);
+        };
+        friend void from_json(const Darabonba::Json& j, ZoneInfos& obj) { 
+          DARABONBA_PTR_FROM_JSON(status, status_);
+          DARABONBA_PTR_FROM_JSON(zoneId, zoneId_);
+        };
+        ZoneInfos() = default ;
+        ZoneInfos(const ZoneInfos &) = default ;
+        ZoneInfos(ZoneInfos &&) = default ;
+        ZoneInfos(const Darabonba::Json & obj) { from_json(obj, *this); };
+        virtual ~ZoneInfos() = default ;
+        ZoneInfos& operator=(const ZoneInfos &) = default ;
+        ZoneInfos& operator=(ZoneInfos &&) = default ;
+        virtual void validate() const override {
+        };
+        virtual void fromMap(const Darabonba::Json &obj) override { from_json(obj, *this); validate(); };
+        virtual Darabonba::Json toMap() const override { Darabonba::Json obj; to_json(obj, *this); return obj; };
+        virtual bool empty() const override { return this->status_ == nullptr
+        && this->zoneId_ == nullptr; };
+        // status Field Functions 
+        bool hasStatus() const { return this->status_ != nullptr;};
+        void deleteStatus() { this->status_ = nullptr;};
+        inline string getStatus() const { DARABONBA_PTR_GET_DEFAULT(status_, "") };
+        inline ZoneInfos& setStatus(string status) { DARABONBA_PTR_SET_VALUE(status_, status) };
+
+
+        // zoneId Field Functions 
+        bool hasZoneId() const { return this->zoneId_ != nullptr;};
+        void deleteZoneId() { this->zoneId_ = nullptr;};
+        inline string getZoneId() const { DARABONBA_PTR_GET_DEFAULT(zoneId_, "") };
+        inline ZoneInfos& setZoneId(string zoneId) { DARABONBA_PTR_SET_VALUE(zoneId_, zoneId) };
+
+
+      protected:
+        shared_ptr<string> status_ {};
+        shared_ptr<string> zoneId_ {};
+      };
+
       class Tags : public Darabonba::Model {
       public:
         friend void to_json(Darabonba::Json& j, const Tags& obj) { 
@@ -708,7 +754,7 @@ namespace Models
         && this->masterConfiguration_ == nullptr && this->networkConfig_ == nullptr && this->nodeAmount_ == nullptr && this->nodeSpec_ == nullptr && this->paymentType_ == nullptr
         && this->port_ == nullptr && this->postpaidServiceStatus_ == nullptr && this->privateNetworkIpWhiteList_ == nullptr && this->protocol_ == nullptr && this->publicIpWhitelist_ == nullptr
         && this->resourceGroupId_ == nullptr && this->serviceVpc_ == nullptr && this->status_ == nullptr && this->tags_ == nullptr && this->updatedAt_ == nullptr
-        && this->vpcInstanceId_ == nullptr; };
+        && this->vpcInstanceId_ == nullptr && this->zoneCount_ == nullptr && this->zoneInfos_ == nullptr; };
       // advancedDedicateMaster Field Functions 
       bool hasAdvancedDedicateMaster() const { return this->advancedDedicateMaster_ != nullptr;};
       void deleteAdvancedDedicateMaster() { this->advancedDedicateMaster_ = nullptr;};
@@ -957,6 +1003,22 @@ namespace Models
       inline Result& setVpcInstanceId(string vpcInstanceId) { DARABONBA_PTR_SET_VALUE(vpcInstanceId_, vpcInstanceId) };
 
 
+      // zoneCount Field Functions 
+      bool hasZoneCount() const { return this->zoneCount_ != nullptr;};
+      void deleteZoneCount() { this->zoneCount_ = nullptr;};
+      inline int32_t getZoneCount() const { DARABONBA_PTR_GET_DEFAULT(zoneCount_, 0) };
+      inline Result& setZoneCount(int32_t zoneCount) { DARABONBA_PTR_SET_VALUE(zoneCount_, zoneCount) };
+
+
+      // zoneInfos Field Functions 
+      bool hasZoneInfos() const { return this->zoneInfos_ != nullptr;};
+      void deleteZoneInfos() { this->zoneInfos_ = nullptr;};
+      inline const vector<Result::ZoneInfos> & getZoneInfos() const { DARABONBA_PTR_GET_CONST(zoneInfos_, vector<Result::ZoneInfos>) };
+      inline vector<Result::ZoneInfos> getZoneInfos() { DARABONBA_PTR_GET(zoneInfos_, vector<Result::ZoneInfos>) };
+      inline Result& setZoneInfos(const vector<Result::ZoneInfos> & zoneInfos) { DARABONBA_PTR_SET_VALUE(zoneInfos_, zoneInfos) };
+      inline Result& setZoneInfos(vector<Result::ZoneInfos> && zoneInfos) { DARABONBA_PTR_SET_RVALUE(zoneInfos_, zoneInfos) };
+
+
     protected:
       // The billing method of the instance. Valid values:
       // 
@@ -1019,6 +1081,8 @@ namespace Models
       // Coordination node configuration.
       shared_ptr<string> updatedAt_ {};
       shared_ptr<string> vpcInstanceId_ {};
+      shared_ptr<int32_t> zoneCount_ {};
+      shared_ptr<vector<Result::ZoneInfos>> zoneInfos_ {};
     };
 
     class Headers : public Darabonba::Model {
